5d864e2274864d24ac37497a4b5449d37d6bdfde
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2010-11-30 06:58:44 +0000

    git-svn-id: http://svn.shamangrad.net/zcad/trunk@34 d67b0292-7a95-df11-8fcc-003048937916

--- cad/rtl/savedvar.pas
+++ cad/rtl/savedvar.pas
@@ -20,14 +20,14 @@ begin
   VIEW_CommandLineH:=102;
   VIEW_ObjInspV:=206;
   VIEW_ObjInspSubV:=90;
-  PATH_File0:='C:\zcad\cad\sample\test_dxf\em.dxf';
+  PATH_File0:='E:\CAD_XDXF\test2000\pozar.dxf';
   PATH_File1:='C:\zcad\cad\sample\test_dxf\ops.dxf';
-  PATH_File2:='/mnt/win_c/zcad/cad/autosave/autosave.dxf';
-  PATH_File3:='/mnt/win_c/zcad/cad/sample/test_dxf/ops.dxf';
-  PATH_File4:='/mnt/win_c/zcad/cad/sample/test_dxf/em.dxf';
-  PATH_File5:='E:\CAD_XDXF\test2000\text.dxf';
-  PATH_File6:='E:\CAD_XDXF\test2000\shema_Poly_Line_Text_Circle_Arc.dxf';
-  PATH_File7:='E:\CAD_XDXF\test2000\shema.dxf';
-  PATH_File8:='E:\CAD_XDXF\test2000\shema_Poly_Line_Text_Circle_Arc.bak';
-  PATH_File9:='C:\zcad\cad\sample\test_dxf\teapot.dxf';
+  PATH_File2:='C:\zcad\cad\sample\test_dxf\em.dxf';
+  PATH_File3:='/mnt/win_c/zcad/cad/autosave/autosave.dxf';
+  PATH_File4:='/mnt/win_c/zcad/cad/sample/test_dxf/ops.dxf';
+  PATH_File5:='/mnt/win_c/zcad/cad/sample/test_dxf/em.dxf';
+  PATH_File6:='E:\CAD_XDXF\test2000\text.dxf';
+  PATH_File7:='E:\CAD_XDXF\test2000\shema_Poly_Line_Text_Circle_Arc.dxf';
+  PATH_File8:='E:\CAD_XDXF\test2000\shema.dxf';
+  PATH_File9:='E:\CAD_XDXF\test2000\shema_Poly_Line_Text_Circle_Arc.bak';
 end.

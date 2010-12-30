de84d97637242f9bcbfdfc78adf9ab542da700c7
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2010-12-30 20:06:30 +0000

    Починен перспективный вид. курсор трассировки глючит в перспективе
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@52 d67b0292-7a95-df11-8fcc-003048937916

--- cad/rtl/savedvar.pas
+++ cad/rtl/savedvar.pas
@@ -17,15 +17,15 @@ var
   PATH_File9:GDBString;
 implementation
 begin
-  VIEW_CommandLineH:=46;
-  VIEW_ObjInspV:=185;
+  VIEW_CommandLineH:=145;
+  VIEW_ObjInspV:=281;
   VIEW_ObjInspSubV:=90;
-  PATH_File0:='C:\zcad\cad\autosave\autosave.dxf';
-  PATH_File1:='J:\nh\Федосеева\ИТП ПОЛИКЛИНИКА\1.dxf';
-  PATH_File2:='J:\nh\Федосеева\ИТП ПОЛИКЛИНИКА\proj.dxf';
-  PATH_File3:='Z:\1.dxf';
-  PATH_File4:='C:\zcad\cad\sample\test_dxf\ops.dxf';
-  PATH_File5:='C:\zcad\cad\sample\test_dxf\em.dxf';
+  PATH_File0:='Z:\1.dxf';
+  PATH_File1:='C:\zcad\cad\sample\test_dxf\ops.dxf';
+  PATH_File2:='C:\zcad\cad\autosave\autosave.dxf';
+  PATH_File3:='C:\zcad\cad\sample\test_dxf\em.dxf';
+  PATH_File4:='J:\nh\Федосеева\ИТП ПОЛИКЛИНИКА\1.dxf';
+  PATH_File5:='J:\nh\Федосеева\ИТП ПОЛИКЛИНИКА\proj.dxf';
   PATH_File6:='/mnt/win_c/zcad/cad/sample/test_dxf/ops.dxf';
   PATH_File7:='c:\zcad\errors\4.dxf';
   PATH_File8:='c:\zcad\errors\3.dxf';

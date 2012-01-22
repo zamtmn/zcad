3e53096672248eb82a5e09744e63913df36bedcd
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2012-01-22 20:20:39 +0000

    git-svn-id: http://svn.shamangrad.net/zcad/trunk@125 d67b0292-7a95-df11-8fcc-003048937916

--- deltmp.bat
+++ deltmp.bat
@@ -1,38 +1,37 @@
-del /S /F /Q *.~*;*.dcu;*.identcache;*.local;*.or;*.s;*.o;*.ppu;*.bak;*.tgs;*.tgw;*.a;*.prjconfig;*.txaPackage;*.txvpck
-del /S /F /Q cad\*.ppu;cad\*.o;cad\*.or;cad\*.a;cad\*.res;*.rst
-del /S /F /Q cad_source\dcu\*.res;cad_source\dcu\*.compiled;
-del /S /F /Q cad\log\*.log;
-del /S /F /Q cad\log\*.loghard;
-del /S /F /Q cad\autosave\*.dxf;
-del /S /F /Q cad\autosave\*.dbpas;
-del /S /F /Q cad\autosave\*.pas;
-rd cad_source\__history
-rd /S /Q cad_source\dcu\*.sl
-rd cad_source\backup
-rd cad_source\electroteh\backup\
-rd cad_source\devicebase\backup
-rd cad_source\utils\backup
-rd cad\rtl\backup
-rd cad_source\LCLmod\__history
-rd cad_source\LCLmod\backup
-rd cad_source\gui\backup
-rd cad_source\u\backup
-rd cad_source\commands\backup
-rd cad_source\languade\backup
-rd cad_source\zwin\backup
-rd cad_source\GDB\backup
-rd cad_source\languade\__history
-rd cad_source\commands\__history
-rd cad_source\gdb\__history
-rd cad_source\gui\__history
-rd cad_source\iolow\__history
-rd cad_source\OpenGL\__history
-rd cad_source\plugins\__history
-rd cad_source\u\__history
-rd cad_source\utils\__history
-rd cad_source\zwin\__history
-rd cad_source\DeviceBase\__history
-rd cad_source\Utils\__history;Utils\ModelSupport
-rd cad_source\ModelSupport
-rd cad_source\ModelSupport_cad
-rd cad_source\ModelSupport_zcad
+#!/bin/sh
+rm cad_source/dcu/*.ppu cad_source/dcu/*.o cad_source/dcu/*.or cad_source/dcu/*.a cad_source/dcu/*.rst
+rm cad_source/dcu/*.res cad_source/dcu/*.compiled cad_source/dcu/*.lrs
+rm cad/log/*.log;
+rm cad/log/*.loghard;
+rm cad/autosave/*.dxf;
+rm cad/autosave/*.dbpas;
+rm cad/autosave/*.pas;
+rm -rf cad_source/__history
+rm -rf cad_source/backup
+rm -rf cad_source/electroteh/backup
+rm -rf cad_source/devicebase/backup
+rm -rf cad_source/utils/backup
+rm -rf cad/rtl/backup
+rm -rf cad_source/LCLmod/__history
+rm -rf cad_source/LCLmod/backup
+rm -rf cad_source/gui/backup
+rm -rf cad_source/u/backup
+rm -rf cad_source/commands/backup
+rm -rf cad_source/languade/backup
+rm -rf cad_source/zwin/backup
+rm -rf cad_source/GDB/backup
+rm -rf cad_source/languade/__history
+rm -rf cad_source/commands/__history
+rm -rf cad_source/gdb/__history
+rm -rf cad_source/gui/__history
+rm -rf cad_source/iolow/__history
+rm -rf cad_source/OpenGL/__history
+rm -rf cad_source/plugins/__history
+rm -rf cad_source/u/__history
+rm -rf cad_source/utils/__history
+rm -rf cad_source/zwin/__history
+rm -rf cad_source/DeviceBase/__history
+rm -rf cad_source/Utils/__history Utils/ModelSupport
+rm -rf cad_source/ModelSupport
+rm -rf cad_source/ModelSupport_cad
+rm -rf cad_source/ModelSupport_zcad
--- run7z.bat
+++ run7z.bat
@@ -1,5 +1,6 @@
-call s3p
-call deltmp
-del c:\cad.7z;c:\cad_source.7z
-7z a c:\cad.7z cad\ -ms=off  -xr!*.svn
-7z a c:\cad_source.7z cad_source\ -ms=off -xr!*.svn
+#!/bin/sh
+call deltmp.bat
+deltmp.bat
+rm cad.7z cad_source.7z
+7z a cad.7z cad/ -ms=off  -xr!*.svn
+7z a cad_source.7z cad_source/ -ms=off -xr!*.svn

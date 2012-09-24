4638614ece2d30a9c796dba3ed0dd9c09873e2c4
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2012-09-24 10:23:28 +0000

    Add testing LineTypes to lines
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@306 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/u/ugdbshxfont.pas
+++ cad_source/u/ugdbshxfont.pas
@@ -19,7 +19,7 @@
 unit UGDBSHXFont;
 {$INCLUDE def.inc}
 interface
-uses strproc,UGDBOpenArrayOfByte{,UGDBPoint3DArray},gdbasetypes,UGDBOpenArrayOfData,sysutils,gdbase,{UGDBVisibleOpenArray,}geometry{,gdbEntity,UGDBOpenArrayOfPV};
+uses UGDBPolyPoint3DArray,gdbobjectsconstdef,UGDBPoint3DArray,strproc,UGDBOpenArrayOfByte{,UGDBPoint3DArray},gdbasetypes,UGDBOpenArrayOfData,sysutils,gdbase,{UGDBVisibleOpenArray,}geometry{,gdbEntity,UGDBOpenArrayOfPV};
 type
 {EXPORT+}
 PGDBsymdolinfo=^GDBsymdolinfo;
@@ -53,12 +53,171 @@ GDBfont=object(GDBNamedObject)
     function GetOrCreateSymbolInfo(symbol:GDBInteger):PGDBsymdolinfo;
     function GetOrReplaceSymbolInfo(symbol:GDBInteger):PGDBsymdolinfo;
     function findunisymbolinfo(symbol:GDBInteger):PGDBsymdolinfo;
+    procedure CreateSymbol(var Vertex3D_in_WCS_Array:GDBPolyPoint3DArray;_symbol:GDBInteger;const objmatrix:DMatrix4D;matr:DMatrix4D;var minx,miny,maxx,maxy:GDBDouble;ln:GDBInteger);
   end;
 {EXPORT-}
 var
    pbasefont: PGDBfont;
 implementation
 uses {math,}log;
+procedure GDBfont.CreateSymbol(var Vertex3D_in_WCS_Array:GDBPolyPoint3DArray;_symbol:GDBInteger;const objmatrix:DMatrix4D;matr:DMatrix4D;var minx,miny,maxx,maxy:GDBDouble;ln:GDBInteger);
+var
+  psymbol: GDBPointer;
+  i, j, k: GDBInteger;
+  len: GDBWord;
+  //matr,m1: DMatrix4D;
+  v:GDBvertex4D;
+  pv:GDBPolyVertex2D;
+  pv3:GDBPolyVertex3D;
+
+  plp,plp2:pgdbvertex;
+  lp,tv:gdbvertex;
+  pl:GDBPoint3DArray;
+  ispl:gdbboolean;
+  ir:itrec;
+  psyminfo:PGDBsymdolinfo;
+  deb:GDBsymdolinfo;
+begin
+  if _symbol=100 then
+                      _symbol:=_symbol;
+  {if _symbol<256 then
+                    _symbol:=ach2uch(_symbol);}
+  if _symbol=32 then
+                      _symbol:=_symbol;
+
+  psyminfo:=self.GetOrReplaceSymbolInfo(integer(_symbol));
+  deb:=psyminfo^;
+  psymbol := self.SHXdata.getelement({pgdbfont(pfont).symbo linfo[GDBByte(_symbol)]}psyminfo.addr);// GDBPointer(GDBPlatformint(pfont)+ pgdbfont(pfont).symbo linfo[GDBByte(_symbol)].addr);
+  if {pgdbfont(pfont)^.symbo linfo[GDBByte(_symbol)]}psyminfo.size <> 0 then
+    for j := 1 to {pgdbfont(pfont)^.symbo linfo[GDBByte(_symbol)]}psyminfo.size do
+    begin
+      case GDBByte(psymbol^) of
+        2:
+          begin
+            inc(pGDBByte(psymbol), sizeof(GDBLineID));
+            PGDBvertex2D(@v)^.x:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            PGDBvertex2D(@v)^.y:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            v.z:=0;
+            v.w:=1;
+            v:=VectorTransform(v,matr);
+            pv.coord:=PGDBvertex2D(@v)^;
+            pv.count:=0;
+
+            if v.x<minx then minx:=v.x;
+            if v.y<miny then miny:=v.y;
+            if v.x>maxx then maxx:=v.x;
+            if v.y>maxy then maxy:=v.y;
+
+            v:=VectorTransform(v,objmatrix);
+
+            pv3.coord:=PGDBvertex(@v)^;
+
+            tv:=pv3.coord;
+            pv3.LineNumber:=ln;
+
+            pv3.count:=0;
+            Vertex3D_in_WCS_Array.add(@pv3);
+
+            //inc(pGDBByte(psymbol), 2 * sizeof(GDBDouble));
+            PGDBvertex2D(@v)^.x:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            PGDBvertex2D(@v)^.y:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            v.z:=0;
+            v.w:=1;
+            v:=VectorTransform(v,matr);
+
+            if v.x<minx then minx:=v.x;
+            if v.y<miny then miny:=v.y;
+            if v.x>maxx then maxx:=v.x;
+            if v.y>maxy then maxy:=v.y;
+
+
+            v:=VectorTransform(v,objmatrix);
+            pv3.coord:=PGDBvertex(@v)^;
+            pv3.count:=0;
+
+            pv3.LineNumber:=ln;
+
+            Vertex3D_in_WCS_Array.add(@pv3);
+
+
+            pv.coord:=PGDBvertex2D(@v)^;
+            pv.count:=0;
+            //inc(pGDBByte(psymbol), 2 * sizeof(GDBDouble));
+          end;
+        4:
+          begin
+            inc(pGDBByte(psymbol), sizeof(GDBPolylineID));
+            len := GDBWord(psymbol^);
+            inc(pGDBByte(psymbol), sizeof(GDBWord));
+            PGDBvertex2D(@v)^.x:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            PGDBvertex2D(@v)^.y:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            v.z:=0;
+            v.w:=1;
+            v:=VectorTransform(v,matr);
+            pv.coord:=PGDBvertex2D(@v)^;
+            pv.count:=len;
+
+            if v.x<minx then minx:=v.x;
+            if v.y<miny then miny:=v.y;
+            if v.x>maxx then maxx:=v.x;
+            if v.y>maxy then maxy:=v.y;
+
+
+            v:=VectorTransform(v,objmatrix);
+            pv3.coord:=PGDBvertex(@v)^;
+            pv3.count:=len;
+
+            tv:=pv3.coord;
+            pv3.LineNumber:=ln;
+
+            Vertex3D_in_WCS_Array.add(@pv3);
+
+
+            //inc(pGDBByte(psymbol), 2 * sizeof(GDBDouble));
+            k := 1;
+            while k < len do //for k:=1 to len-1 do
+            begin
+            PGDBvertex2D(@v)^.x:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            PGDBvertex2D(@v)^.y:=pfontfloat(psymbol)^;
+            inc(pfontfloat(psymbol));
+            v.z:=0;
+            v.w:=1;
+
+            v:=VectorTransform(v,matr);
+
+            if v.x<minx then minx:=v.x;
+            if v.y<miny then miny:=v.y;
+            if v.x>maxx then maxx:=v.x;
+            if v.y>maxy then maxy:=v.y;
+
+
+            v:=VectorTransform(v,objmatrix);
+            pv.coord:=PGDBvertex2D(@v)^;
+            pv.count:=-1;
+
+            pv3.coord:=PGDBvertex(@v)^;
+            pv3.count:={-1}k-len+1;
+
+            pv3.LineNumber:=ln;
+            tv:=pv3.coord;
+
+            Vertex3D_in_WCS_Array.add(@pv3);
+
+
+            //inc(pGDBByte(psymbol), 2 * sizeof(GDBDouble));
+            inc(k);
+            end;
+          end;
+      end;
+    end;
+  end;
 constructor GDBfont.initnul;
 begin
      inherited;

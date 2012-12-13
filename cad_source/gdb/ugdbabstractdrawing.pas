e9c1c7242371f39d5c77453c3b3106a9eaedbd51
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2012-12-13 20:43:26 +0000

    Add forgot ugdbabstractdrawing
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@441 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/gdb/ugdbabstractdrawing.pas
@@ -0,0 +1,44 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit ugdbabstractdrawing;
+interface
+uses gdbase,gdbasetypes,GDBCamera,GDBGenericSubEntry,GDBRoot,UGDBSelectedObjArray,UGDBLayerArray,UGDBOpenArrayOfPV;
+type
+{EXPORT+}
+PTAbstractDrawing=^TAbstractDrawing;
+TAbstractDrawing=object(GDBaseobject)
+                       //function CreateBlockDef(name:GDBString):GDBPointer;virtual;abstract;
+                       function myGluProject2(objcoord:GDBVertex; out wincoord:GDBVertex):Integer;virtual;abstract;
+                       function myGluUnProject(win:GDBVertex;out obj:GDBvertex):Integer;virtual;abstract;
+                       function GetPcamera:PGDBObjCamera;virtual;abstract;
+                       function GetCurrentROOT:PGDBObjGenericSubEntry;virtual;abstract;
+                       function GetConstructObjRoot:PGDBObjRoot;virtual;abstract;
+                       function GetSelObjArray:PGDBSelectedObjArray;virtual;abstract;
+                       function GetOnMouseObj:PGDBObjOpenArrayOfPV;virtual;abstract;
+                       procedure RotateCameraInLocalCSXY(ux,uy:GDBDouble);virtual;abstract;
+                       procedure MoveCameraInLocalCSXY(oldx,oldy:GDBDouble;ax:gdbvertex);virtual;abstract;
+                       procedure SetCurrentDWG;virtual;abstract;
+                       function GetLayerTable:PGDBLayerArray;virtual;abstract;
+                       function GetChangeStampt:GDBBoolean;virtual;abstract;
+                       function StoreOldCamerapPos:Pointer;virtual;abstract;
+                       procedure StoreNewCamerapPos(command:Pointer);virtual;abstract;
+                 end;
+{EXPORT-}
+implementation
+end.

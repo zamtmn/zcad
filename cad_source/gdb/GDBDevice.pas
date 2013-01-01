bf22650a7e1a09d88c253edc59f9bae92aec15a9
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2013-01-01 21:24:38 +0000

    Fix compilation in linux
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@458 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/gdb/GDBDevice.pas
+++ cad_source/gdb/GDBDevice.pas
@@ -20,7 +20,7 @@ unit GDBDevice;
 {$INCLUDE def.inc}
 
 interface
-uses ugdbdrawingdef,GDBCamera,zcadsysvars,sysutils,devices,UGDBOpenArrayOfByte,UGDBOpenArrayOfPObjects,
+uses UGDBDrawingdef,GDBCamera,zcadsysvars,sysutils,devices,UGDBOpenArrayOfByte,UGDBOpenArrayOfPObjects,
 OGLSpecFunc,uunitmanager{,shared},
 memman{,strmy,varman},geometry,gdbobjectsconstdef,GDBEntity,GDBSubordinated,varmandef,{UGDBOpenArrayOfPV,}gdbasetypes,GDBBlockInsert,GDBase,UGDBVisibleOpenArray,UGDBObjBlockdefArray{,UGDBDescriptor}{,UGDBLayerArray,oglwindowdef};
 

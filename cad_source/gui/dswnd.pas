d81772ffb45d5c31760cbd20a1bc29c6f563d4c2
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2013-12-05 20:05:06 +0000

    Add forgot files
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@821 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/gui/dswnd.pas
@@ -0,0 +1,30 @@
+unit dswnd;
+
+{$mode delphi}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TDSWindow }
+
+  TDSWindow = class(TForm)
+    Label1: TLabel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  DSWindow: TDSWindow;
+
+implementation
+
+{$R *.lfm}
+
+end.
+

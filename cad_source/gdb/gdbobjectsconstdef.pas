7c4386740888f2862fd0fd9454922066aceeb6c2
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2013-12-18 19:15:23 +0000

    Add matchprop command; Some improvements
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@845 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/gdb/gdbobjectsconstdef.pas
+++ cad_source/gdb/gdbobjectsconstdef.pas
@@ -91,6 +91,35 @@ const {as_normal=0;
   DXFjsbtc = 11;
   DXFjsbtr = 12;
 
+
+  LnWt000 = 0;
+  LnWt005 = 5;
+  LnWt009 = 9;
+  LnWt013 = 13;
+  LnWt015 = 15;
+  LnWt018 = 18;
+  LnWt020 = 20;
+  LnWt025 = 25;
+  LnWt030 = 30;
+  LnWt035 = 35;
+  LnWt040 = 40;
+  LnWt050 = 50;
+  LnWt053 = 53;
+  LnWt060 = 60;
+  LnWt070 = 70;
+  LnWt080 = 80;
+  LnWt090 = 90;
+  LnWt100 = 100;
+  LnWt106 = 106;
+  LnWt120 = 120;
+  LnWt140 = 140;
+  LnWt158 = 158;
+  LnWt200 = 200;
+  LnWt211 = 211;
+  LnWtByLayer = -1;
+  LnWtByBlock = -2;
+  LnWtByLwDefault = -3;
+
   CGDBWhile = 7;
   CGDBGreen = 3;
   lwgdbdefault = -3;

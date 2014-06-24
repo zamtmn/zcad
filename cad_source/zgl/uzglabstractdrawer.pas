5bdd536a40d3556896f66a23860b56f4b1367cbc
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2014-06-24 22:22:07 +0000

    Continue drawers
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@918 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zgl/uzglabstractdrawer.pas
+++ cad_source/zgl/uzglabstractdrawer.pas
@@ -28,6 +28,23 @@ TZGLAbstractDrawer=class
                         procedure DrawPoint(const i:TLLVertexIndex);virtual;abstract;
                         procedure startrender;virtual;
                         procedure endrender;virtual;
+                        procedure SetLineWidth(const w:single);virtual;abstract;
+                        procedure SetPointSize(const s:single);virtual;abstract;
+                        procedure SetColor(const red, green, blue: byte);overload;virtual;abstract;
+                        procedure SetClearColor(const red, green, blue, alpha: byte);overload;virtual;abstract;
+                        procedure SetColor(const color: TRGB);overload;virtual;abstract;
+                        procedure ClearScreen(stencil:boolean);virtual;abstract;
+                        procedure TranslateCoord2D(const tx,ty:single);virtual;abstract;
+                        procedure ScaleCoord2D(const sx,sy:single);virtual;abstract;
+                        procedure SetLineSmooth(const smoth:boolean);virtual;abstract;
+                        procedure SetPointSmooth(const smoth:boolean);virtual;abstract;
+                        procedure ClearStatesMachine;virtual;abstract;
+                        procedure SetFillStencilMode;virtual;abstract;
+                        procedure SetDrawWithStencilMode;virtual;abstract;
+                        procedure DisableStencil;virtual;abstract;
+                        procedure SetZTest(Z:boolean);virtual;abstract;
+
+
 
                         procedure DrawLine2DInDCS(const x1,y1,x2,y2:integer);overload;virtual;abstract;
                         procedure DrawLine2DInDCS(const x1,y1,x2,y2:single);overload;virtual;abstract;

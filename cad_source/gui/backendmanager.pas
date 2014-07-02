5ce3535a19e4452f924c407ac37c343805f02b60
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2014-07-02 16:11:04 +0000

    Add forgot unit
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@941 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/gui/backendmanager.pas
@@ -0,0 +1,48 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit backendmanager;
+{$INCLUDE def.inc}
+
+interface
+uses gdbasetypes,
+     gdbase,UGDBStringArray,zcadsysvars,abstractviewarea,UGDBOpenArrayOfPointer;
+const test:GDBSTRING='asdasd';
+type
+    TVA=class of TAbstractViewArea;
+var
+    Backends:GDBOpenArrayOfGDBPointer;
+procedure RegisterBackend(BackEndClass:TVA;Name:string);
+function GetCurrentBackEnd:TVA;
+implementation
+procedure RegisterBackend(BackEndClass:TVA;Name:string);
+begin
+     sysvar.RD.RD_RendererBackEnd.Enums.add(@name);
+     Backends.Add(@BackEndClass);
+end;
+function GetCurrentBackEnd:TVA;
+begin
+     result:=ppointer(Backends.getelement(sysvar.RD.RD_RendererBackEnd.Selected))^;
+end;
+
+begin
+  sysvar.RD.RD_RendererBackEnd.Enums.init(10);
+  sysvar.RD.RD_RendererBackEnd.Selected:=0;
+  Backends.init(10);
+end.
+

8bdb7cad251f234a9abcf51809cc8c72ae670e23
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2014-09-21 19:32:45 +0000

    Improve object inspector
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@978 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/gui/objinsp.pas
+++ cad_source/gui/objinsp.pas
@@ -930,13 +930,14 @@ begin
                                       self.VertScrollBar.Smooth:=false;
                                       UpdateScrollbars;
                                  end;
-     {$IFNDEF LCLWIN32}
-     if ((VertScrollBar.Position>0) and (VertScrollBar.Position<contentheigth-height))or changed then
-     begin
-     VertScrollBar.Position:=VertScrollBar.Position-1;
-     VertScrollBar.Position:=VertScrollBar.Position+1;
-     end;
-     {$ENDIF}
+     //Нихуя не понял нахуя это сделано... пока уберу
+     //{$IFNDEF LCLWIN32}
+     //if ((VertScrollBar.Position>0) and (VertScrollBar.Position<contentheigth-height))or changed then
+     //begin
+     //VertScrollBar.Position:=VertScrollBar.Position-1;
+     //VertScrollBar.Position:=VertScrollBar.Position+1;
+     //end;
+     //{$ENDIF}
      UpdateScrollbars;
 end;
 function TGDBobjinsp.IsMouseOnSpliter(pp:PPropertyDeskriptor; X:Integer):GDBBoolean;

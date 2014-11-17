1801b04083f797c5486f1caaff962e71db7b336c
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2014-11-17 23:11:24 +0000

    Add BlockInsert form and ArrayInsert form from ur_naz
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1084 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/gui/blockinsertwnd.pas
@@ -0,0 +1,59 @@
+unit blockinsertwnd;
+
+{$mode objfpc}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, EditBtn, ButtonPanel, FileCtrl;
+
+type
+
+  { TBlockInsertFRM }
+
+  TBlockInsertFRM = class(TForm)
+    Button1: TButton;
+    ButtonPanel1: TButtonPanel;
+    CheckBox1: TCheckBox;
+    CheckBox2: TCheckBox;
+    ComboBox1: TComboBox;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    Edit7: TEdit;
+    Edit8: TEdit;
+    Edit9: TEdit;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
+    GroupBox4: TGroupBox;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  BlockInsertFRM: TBlockInsertFRM;
+
+implementation
+{$R *.lfm}
+
+initialization
+
+end.
+

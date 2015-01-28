8d4d59cd4e0d4c1715f1bf76ca9c12ae1fda9394
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2015-01-28 21:19:50 +0000

    Separate project
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1195 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/gdb/gdbfieldprocessor.pas
+++ cad_source/gdb/gdbfieldprocessor.pas
@@ -19,12 +19,17 @@ unit gdbfieldprocessor;
 {$INCLUDE def.inc}
 
 interface
-uses languade,strproc,sysutils,gdbasetypes,varmandef,GDBase;
+uses sysutils,gdbasetypes,usimplegenerics;
+type
+TStrProcessFunc=procedure(var str:gdbstring;startpos:integer;pobj:pointer);
+TPrefix2ProcessFunc=GKey2DataMap<GDBString,TStrProcessFunc,LessGDBString>;
+var
+    Prefix2ProcessFunc:TPrefix2ProcessFunc;
 function textformat(s:GDBString;pobj:GDBPointer):GDBString;
 function convertfromunicode(s:GDBString):GDBString;
 implementation
 uses
-   log,GDBSubordinated;
+   log;
 function convertfromunicode(s:GDBString):GDBString;
 var //i,i2:GDBInteger;
     ps{,varname}:GDBString;
@@ -48,12 +53,11 @@ begin
 end;
 function textformat;
 var i,i2,counter:GDBInteger;
-    ps,varname:GDBString;
-    pv:pvardesk;
-    //num,code:integer;
-    vd:vardesk;
+    ps,s2:GDBString;
+    iterator:Prefix2ProcessFunc.TIterator;
+const
+    maxitertations=100;
 begin
-     //ps:=s;
      ps:=convertfromunicode(s);
      repeat
           i:=pos('%%DATE',uppercase(ps));
@@ -62,101 +66,32 @@ begin
                           ps:=copy(ps,1,i-1)+datetostr(date)+copy(ps,i+6,length(ps)-i-5)
                      end;
      until i<=0;
-     {repeat
-          i:=pos('\U+',uppercase(ps));
-          if i>0 then
-                     begin
-                          varname:='$'+copy(ps,i+3,4);
-                          val(varname,num,code);
-                          if code=0 then
-                                        ps:=copy(ps,1,i-1)+Chr(uch2ach(num))+copy(ps,i+7,length(ps)-i-6)
-                     end;
-     until i<=0;}
-     {repeat
-          i:=pos('%%D',uppercase(ps));
-          if i>0 then
-                     begin
-                          ps:=copy(ps,1,i-1)+#35+copy(ps,i+3,length(ps)-i-2)
-                     end;
-     until i<=0;
-     repeat
-          i:=pos('%%P',uppercase(ps));
-          if i>0 then
-                     begin
-                          ps:=copy(ps,1,i-1)+#96+copy(ps,i+3,length(ps)-i-2)
-                     end;
-     until i<=0;
-     repeat
-          i:=pos('%%C',uppercase(ps));
-          if i>0 then
-                     begin
-                          ps:=copy(ps,1,i-1)+#143+copy(ps,i+3,length(ps)-i-2)
-                     end;
-     until i<=0;
-     repeat
-          i:=pos('%%U',uppercase(ps));
-          if i>0 then
-                     begin
-                          ps:=copy(ps,1,i-1)+#1+copy(ps,i+3,length(ps)-i-2)
-                     end;
-     until i<=0;}
-{     repeat
-          i:=pos('\L',uppercase(ps));
-          if i>0 then
-                     begin
-                          ps:=copy(ps,1,i-1)+#1+copy(ps,i+2,length(ps)-i-1)
-                     end;
-     until i<=0;
-     repeat
-          i:=pos('\l',uppercase(ps));
-          if i>0 then
-                     begin
-                          ps:=copy(ps,1,i-1)+#1+copy(ps,i+2,length(ps)-i-1)
-                     end;
-     until i<=0; }
      counter:=0;
+     iterator:=Prefix2ProcessFunc.Min;
+     if assigned(iterator) then
      repeat
-          inc(counter);
-          i:=pos('@@[',ps);
-          if i>0 then
-                     begin
-                          i2:=pos(']',ps);
-                          if i2<i then system.break;
-                          varname:=copy(ps,i+3,i2-i-3);
-                          pv:=nil;
-                          if pobj<>nil then
-                                           pv:=PGDBObjGenericWithSubordinated(pobj).FindVariable(varname);
-                                           //pv:=gdb.GetCurrentDWG.DWGUnits.findunit('DrawingVars').FindVariable(varname);
-                          //pv:=SysUnit.InterfaceVariables.findvardesc(varname);
-                          if pv<>nil then
-                                         begin
-                                              //ps:=copy(ps,1,i-1)+ varman.valuetoGDBString(pv^.pvalue,pv.ptd) +copy(ps,i2+1,length(ps)-i2)
-                                              ps:=copy(ps,1,i-1)+pv.data.ptd^.GetValueAsString(pv^.data.Instance)+copy(ps,i2+1,length(ps)-i2)
-                                         end
-                                     else
-                                         ps:=copy(ps,1,i-1)+'!!ERR('+varname+')!!'+copy(ps,i2+1,length(ps)-i2)
-                     end
-          else
-          begin
-          i:=pos('##[',ps);
-          if i>0 then
-                     begin
-                          i2:=pos(']',ps);
-                          if i2<i then system.break;
-                          varname:=copy(ps,i+3,i2-i-3);
-                          vd:=evaluate(varname,@PGDBObjGenericWithSubordinated(pobj).OU);
-                          if (assigned(vd.data.ptd))and(assigned(vd.data.Instance)) then
-                                                                                        ps:=copy(ps,1,i-1)+vd.data.ptd^.GetValueAsString(vd.data.Instance)+copy(ps,i2+1,length(ps)-i2)
-                                                                                    else
-                                                                                        ps:=copy(ps,1,i-1)+'!!ERR('+varname+')!!'+copy(ps,i2+1,length(ps)-i2)
-                     end;
-          end;
-     until (i<=0)or(counter>100);
-     if counter>100 then
+       s2:=iterator.key;
+       if assigned(iterator.value)then
+       begin
+         repeat
+           i:=pos(iterator.key,ps);
+           if i>0 then
+           begin
+             iterator.value(ps,i,pobj);
+             inc(counter);
+           end;
+         until (i<=0)or(counter>maxitertations);
+       end;
+     until (not iterator.Next)or(counter>maxitertations);
+
+     if counter>maxitertations then
                         result:='!!ERR(Loop detected)'
                     else
                         result:=ps;
 end;
-begin
+initialization
   {$IFDEF DEBUGINITSECTION}LogOut('gdbfieldprocessor.initialization');{$ENDIF}
+  Prefix2ProcessFunc:=TPrefix2ProcessFunc.Create;
+finalization
+  Prefix2ProcessFunc.Destroy;
 end.

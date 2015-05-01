c1f7ae7992d4350ea1aafcf1bcabc802b604e477
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2015-05-01 23:10:50 +0000

    Continue zemathutils unit
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1339 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zengine/misc/zemathutils.pas
+++ cad_source/zengine/misc/zemathutils.pas
@@ -25,13 +25,6 @@ function zeDoubleToString(const value:Double; const f:TzeUnitsFormat):GDBString;
 implementation
 const
   fractions:array [TUPrec] of integer=(1,2,4,8,16,32,64,128,256);
-function zeDoubleToString(const value:Double; const f:TzeUnitsFormat):GDBString;
-var
-   _ft:double;{1}
-   absvalue,_in:double;{12*_ft}
-   _fts,_ins:GDBString;
-   divide:integer;
-   simplifieduprec:TUPrec;
 procedure rtz(var _value:GDBString);
 var
   Q:Integer;
@@ -47,7 +40,13 @@ begin
   else
     SetLength(_value,Q);
 end;
-
+function zeDoubleToString(const value:Double; const f:TzeUnitsFormat):GDBString;
+var
+   _ft,_dft:double;{1}
+   absvalue,_in:double;{12*_ft}
+   _fts,_ins,_dfts:GDBString;
+   divide:integer;
+   simplifieduprec:TUPrec;
 begin
   //result:='fuckoff';exit;
   //';;Pattern length %f'#13#10'%s';
@@ -83,7 +82,75 @@ begin
                                              result:='-'+result;
                   end;
   LUArchitectural:begin
-                       result:=floattostr(value);
+                       simplifieduprec:=f.uprec;
+                       absvalue:=abs(value);
+                       _in:=floor(absvalue/12);
+                       _ft:=absvalue-12*_in;
+                       _dft:=floor(_ft);
+                       _ft:=_ft-_dft;
+                       divide:=round(_ft*fractions[f.uprec]);
+                       if {divide=1}divide=fractions[f.uprec] then
+                                     begin
+                                          _dft:=_dft+1;
+                                          divide:=0;
+                                     end;
+                       if _in<>0 then
+                         begin
+                           str(_in:0:ord(f.uprec),_ins);
+                           rtz(_ins);
+                         end;
+                       if _dft<>0 then
+                         begin
+                           str(_dft:0:ord(f.uprec),_dfts);
+                           rtz(_dfts);
+                         end;
+                       if divide<>0 then
+                         begin
+                            while (divide and 1)=0 do
+                              begin
+                                   divide:=divide shr 1;
+                                   simplifieduprec:=pred(simplifieduprec);
+                              end;
+                           _fts:=inttostr(divide)+'/'+inttostr(fractions[simplifieduprec])+'"';
+                         end;
+                       if (_in=0)and(_dft=0)and(divide=0) then
+                                                   exit('0"')
+                  else if (_in<>0)and(_dft=0)and(divide=0)then
+                                                   result:=format('%s''',[_ins])
+                  else if (_in=0)and(_dft<>0)and(divide=0)then
+                                                   result:=format('%s"',[_dfts])
+                  else if (_in=0)and(_dft=0)and(divide<>0)then
+                                                   result:=_fts
+                  else if (_in<>0)and(_dft<>0)and(divide=0)then
+                                                               begin
+                                                                 if f.umode=UMWithSpaces then
+                                                                                             result:=format('%s''%s"',[_ins,_dfts])
+                                                                                         else
+                                                                                             result:=format('%s'' %s"',[_ins,_dfts]);
+                                                               end
+                  else if (_in<>0)and(_dft=0)and(divide<>0)then
+                                                               begin
+                                                                 if f.umode=UMWithSpaces then
+                                                                                             result:=format('%s'' %s"',[_ins,_fts])
+                                                                                         else
+                                                                                             result:=format('%s''-%s"',[_ins,_fts]);
+                                                               end
+                  else if (_in=0)and(_dft<>0)and(divide<>0)then
+                                                               begin
+                                                                 if f.umode=UMWithSpaces then
+                                                                                             result:=format('%s %s',[_dfts,_fts])
+                                                                                         else
+                                                                                             result:=format('%s-%s',[_dfts,_fts]);
+                                                               end
+                  else if (_in<>0)and(_dft<>0)and(divide<>0)then
+                                                            begin
+                                                   if f.umode=UMWithSpaces then
+                                                                   result:=format('%s''-%s %s',[_ins,_dfts,_fts])
+                                                               else
+                                                                   result:=format('%s''%s-%s',[_ins,_dfts,_fts]);
+                                                            end;
+                       if sign(value)=-1 then
+                                             result:='-'+result;
                   end;
      LUFractional:begin
                         simplifieduprec:=f.uprec;
@@ -91,10 +158,10 @@ begin
                         _in:=floor(absvalue);
                         _ft:=absvalue-_in;
                         divide:=round(_ft*fractions[f.uprec]);
-                        if divide=1 then
+                        if {divide=1}divide=fractions[f.uprec] then
                                       begin
                                            _in:=_in+1;
-                                           dec(divide);
+                                           divide:=0;
                                       end;
                         if _in<>0 then
                           begin

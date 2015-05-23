de71953a88475e64a33e965fa3c39ffd431af484
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2015-05-23 13:03:03 +0000

    Improve Units form
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1367 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zcad/gui/unitswnd.pas
+++ cad_source/zcad/gui/unitswnd.pas
@@ -7,7 +7,103 @@ interface
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
   StdCtrls, Buttons, ButtonPanel,
-  gdbase,zemathutils;
+  gdbase,zemathutils, Types;
+
+const
+     UPrecByLUnits:array[TLUnits,0..8]of string=(
+                                               ('0E+01',
+                                                '0.0E+01',
+                                                '0.00E+01',
+                                                '0.000E+01',
+                                                '0.0000E+01',
+                                                '0.00000E+01',
+                                                '0.000000E+01',
+                                                '0.0000000E+01',
+                                                '0.00000000E+01'),
+                                               ('0',
+                                                '0.0',
+                                                '0.00',
+                                                '0.000',
+                                                '0.0000',
+                                                '0.00000',
+                                                '0.000000',
+                                                '0.0000000',
+                                                '0.00000000'),
+                                               ('0''-0"',
+                                                '0''-0.0"',
+                                                '0''-0.00"',
+                                                '0''-0.000"',
+                                                '0''-0.0000"',
+                                                '0''-0.00000"',
+                                                '0''-0.000000"',
+                                                '0''-0.0000000"',
+                                                '0''-0.00000000"'),
+                                               ('0''-0"',
+                                                '0''-0 1/2"',
+                                                '0''-0 1/4"',
+                                                '0''-0 1/8"',
+                                                '0''-0 1/16"',
+                                                '0''-0 1/32"',
+                                                '0''-0 1/64"',
+                                                '0''-0 1/128"',
+                                                '0''-0 1/256"'),
+                                               ('0',
+                                                '0 1/2',
+                                                '0 1/4',
+                                                '0 1/8',
+                                                '0 1/16',
+                                                '0 1/32',
+                                                '0 1/64',
+                                                '0 1/128',
+                                                '0 1/256')
+                                                );
+     UPrecByAUnits:array[TAUnits,0..8]of string=(
+                                               ('0',
+                                                '0.0',
+                                                '0.00',
+                                                '0.000',
+                                                '0.0000',
+                                                '0.00000',
+                                                '0.000000',
+                                                '0.0000000',
+                                                '0.00000000'),
+                                               ('0d',
+                                                '0d00''',
+                                                '0d00''',
+                                                '0d00''00"',
+                                                '0d00''00"',
+                                                '0d00''00.0"',
+                                                '0d00''00.00"',
+                                                '0d00''00.000"',
+                                                '0d00''00.0000"'),
+                                               ('0g',
+                                                '0.0g',
+                                                '0.00g',
+                                                '0.000g',
+                                                '0.0000g',
+                                                '0.00000g',
+                                                '0.000000g',
+                                                '0.0000000g',
+                                                '0.00000000g'),
+                                               ('0r',
+                                                '0.0r',
+                                                '0.00r',
+                                                '0.000r',
+                                                '0.0000r',
+                                                '0.00000r',
+                                                '0.000000r',
+                                                '0.0000000r',
+                                                '0.00000000r'),
+                                               ('N 0d E',
+                                                'N 0d00'' E',
+                                                'N 0d00'' E',
+                                                'N 0d00''00" E',
+                                                'N 0d00''00" E',
+                                                'N 0d00''00.0" E',
+                                                'N 0d00''00.00" E',
+                                                'N 0d00''00.000" E',
+                                                'N 0d00''00.0000" E')
+                                                );
 
 type
 
@@ -46,8 +142,10 @@ type
     { public declarations }
     function RunModal(var _UnitsFormat:TzeUnitsFormat; var _InsUnits:TInsUnits):Integer; virtual;
     procedure UpdateSample; //реализация с меня
+    procedure SetLUprecFromLUnits;
+    procedure SetAUprecFromAUnits;
     procedure LocalUnitsFormat2Intterface; //загоняем всё из LocalUnitsFormat в комбобоксы
-    procedure Intterface2LocalUnitsFormat; //собираем всё в LocalUnitsFormat из комбобоксов
+    procedure Intterface2LocalUnitsFormat(Sender: TObject); //собираем всё в LocalUnitsFormat из комбобоксов
   end;
 
 var
@@ -58,29 +156,59 @@ implementation
 {$R *.lfm}
 
 { TUnitsWindow }
-procedure TUnitsWindow.Intterface2LocalUnitsFormat;
+procedure TUnitsWindow.Intterface2LocalUnitsFormat(Sender: TObject);
 begin
      //собираем всё в LocalUnitsFormat из комбобоксов
+     if (sender=LUnitsComboBox)or(sender=nil)then
      if LUnitsComboBox.ItemIndex<>-1 then
-       LocalUnitsFormat.uformat:=TLUnits(LUnitsComboBox.ItemIndex);
+                                         begin
+                                              LocalUnitsFormat.uformat:=TLUnits(LUnitsComboBox.ItemIndex);
+                                              SetLUprecFromLunits;
+                                         end;
+     if (sender=LUPrecComboBox)or(sender=nil)then
      if LUPrecComboBox.ItemIndex<>-1 then
        LocalUnitsFormat.uprec:=TUPrec(LUPrecComboBox.ItemIndex);
+     if (sender=AUnitsComboBox)or(sender=nil)then
      if AUnitsComboBox.ItemIndex<>-1 then
-       LocalUnitsFormat.aformat:=TAUnits(AUnitsComboBox.ItemIndex);
+                                         begin
+                                              LocalUnitsFormat.aformat:=TAUnits(AUnitsComboBox.ItemIndex);
+                                              SetAUprecFromAunits;
+                                         end;
+     if (sender=AUPrecComboBox)or(sender=nil)then
      if AUPrecComboBox.ItemIndex<>-1 then
        LocalUnitsFormat.aprec:=TUPrec(AUPrecComboBox.ItemIndex);
+     if (sender=UnitModeCheckBox)or(sender=nil)then
      if UnitModeCheckBox.Checked then
                                      LocalUnitsFormat.umode:=UMWithSpaces
                                  else
                                      LocalUnitsFormat.umode:=UMWithoutSpaces;
+     if (sender=AngDirCheckBox)or(sender=nil)then
      if AngDirCheckBox.Checked then
                                    LocalUnitsFormat.adir:=ADClockwise
                                else
                                    LocalUnitsFormat.adir:=ADCounterClockwise;
+     if (sender=ComboBox5)or(sender=nil)then
      if ComboBox5.ItemIndex<>-1 then
        LocalInsUnits:=TInsUnits(ComboBox5.ItemIndex);
 end;
-
+procedure TUnitsWindow.SetLUprecFromLUnits;
+var
+  i,curr:integer;
+begin
+     curr:=LUPrecComboBox.ItemIndex;
+     for i:=0 to LUPrecComboBox.Items.Count-1 do
+      LUPrecComboBox.Items[i]:=UPrecByLUnits[LocalUnitsFormat.uformat,i];
+     LUPrecComboBox.ItemIndex:=curr;
+end;
+procedure TUnitsWindow.SetAUprecFromAUnits;
+var
+  i,curr:integer;
+begin
+     curr:=AUPrecComboBox.ItemIndex;
+     for i:=0 to AUPrecComboBox.Items.Count-1 do
+      AUPrecComboBox.Items[i]:=UPrecByAUnits[LocalUnitsFormat.aformat,i];
+     AUPrecComboBox.ItemIndex:=curr;
+end;
 procedure TUnitsWindow.LocalUnitsFormat2Intterface;
 begin
      //загоняем всё из LocalUnitsFormat в комбобоксы
@@ -112,7 +240,7 @@ end;
 procedure TUnitsWindow.ChangeInInterface(Sender: TObject);
 begin
      //Обработчик onChange комбиков
-     Intterface2LocalUnitsFormat;
+     Intterface2LocalUnitsFormat(sender);
      UpdateSample;
 end;
 

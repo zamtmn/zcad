7f5b7b91318f829e9033e604727a0164611251dc
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2015-07-20 21:00:01 +0000

    Add forgot indexes unit
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1435 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/zengine/zgl/uzgindexsarray.pas
@@ -0,0 +1,45 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit uzgindexsarray;
+{$INCLUDE def.inc}
+interface
+uses gdbasetypes,UGDBOpenArrayOfData,sysutils,gdbase,memman,
+geometry;
+type
+{Export+}
+ZGLIndexsArray={$IFNDEF DELPHI}packed{$ENDIF} object(GDBOpenArrayOfData)(*OpenArrayOfData=TArrayIndex*)
+                constructor init({$IFDEF DEBUGBUILD}ErrGuid:pansichar;{$ENDIF}m:GDBInteger);
+                constructor initnul;
+             end;
+{Export-}
+implementation
+uses glstatemanager,log;
+constructor ZGLIndexsArray.init;
+begin
+  inherited init({$IFDEF DEBUGBUILD}ErrGuid,{$ENDIF}m,sizeof(TArrayIndex));
+end;
+constructor ZGLIndexsArray.initnul;
+begin
+  inherited initnul;
+  size:=sizeof(TArrayIndex);
+end;
+begin
+  {$IFDEF DEBUGINITSECTION}LogOut('uzgindexsarray.initialization');{$ENDIF}
+end.
+

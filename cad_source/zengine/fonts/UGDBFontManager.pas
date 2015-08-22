87acbc6c7a8f01a4147ccc9b1090fb67acd8308f
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2015-08-22 05:34:25 +0000

    fix linux compilation
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1470 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zengine/fonts/UGDBFontManager.pas
+++ cad_source/zengine/fonts/UGDBFontManager.pas
@@ -19,7 +19,7 @@
 unit UGDBFontManager;
 {$INCLUDE def.inc}
 interface
-uses windows,zcadstrconsts,shared,zcadsysvars,strproc,ugdbfont,gdbasetypes,SysInfo,memman,
+uses {$IFDEF WINDOWS}windows,{$ENDIF}zcadstrconsts,shared,zcadsysvars,strproc,ugdbfont,gdbasetypes,SysInfo,memman,
      sysutils,gdbase, geometry,usimplegenerics,
      UGDBNamedObjectsArray,classes;
 type
@@ -49,7 +49,7 @@ GDBFontManager={$IFNDEF DELPHI}packed{$ENDIF} object({GDBOpenArrayOfData}GDBName
                     {procedure freeelement(p:GDBPointer);virtual;}
               end;
 {Export-}
-function AddFontResourceEx(_para1:LPCSTR; flags:DWORD; reserved:Pointer) : integer; stdcall; external 'gdi32' name 'AddFontResourceExA';
+{$IFDEF WINDOWS}function AddFontResourceEx(_para1:LPCSTR; flags:DWORD; reserved:Pointer) : integer; stdcall; external 'gdi32' name 'AddFontResourceExA';{$ENDIF}
 var
    FontManager:GDBFontManager;
    FontExt2LoadProc:TFontExt2LoadProcMap;
@@ -71,8 +71,10 @@ procedure GDBFontManager.EnumerateTTFFontFile(filename:GDBString);
 var
    r:longint;
 begin
+     {$IFDEF WINDOWS}
+     if AddFontResourceEx(@filename[1],$10,0)>0 then
+     {$ENDIF}
      ttffontfiles.Add(filename);
-     r:=AddFontResourceEx(@filename[1],$10,0);
 end;
 procedure GDBFontManager.EnumerateSHXFontFile(filename:GDBString);
 begin

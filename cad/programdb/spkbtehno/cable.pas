d38869c011c794755071ab8ef08b2383ed1b3d19
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2016-01-07 09:47:29 +0000

    Add ТехноКИПнг(А)-FRLS cable to db
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1642 d67b0292-7a95-df11-8fcc-003048937916

--- cad/programdb/spkbtehno/cable.pas
+++ cad/programdb/spkbtehno/cable.pas
@@ -16,6 +16,68 @@ type
                            _2_2_1_0(*'2х2х1.00'*),
                            _2_2_1_5(*'2х2х1.50'*),
                            _2_2_2_5(*'2х2х2.50'*));
+
+TSPKBTEHNO_TEHNOKIPNGAFRLS_WCS=(_1_2_0_2(*'1х2х0.20'*),
+                                _1_2_0_35(*'1х2х0.35'*),
+                                _1_2_0_5(*'1х2х0.50'*),
+                                _1_2_0_75(*'1х2х0.75'*),
+                                _1_2_1_0(*'1х2х1.00'*),
+                                _1_2_1_5(*'1х2х1.50'*),
+                                _2_2_0_2(*'2х2х0.20'*),
+                                _2_2_0_35(*'2х2х0.35'*),
+                                _2_2_0_5(*'2х2х0.50'*),
+                                _2_2_0_75(*'2х2х0.75'*),
+                                _2_2_1_0(*'2х2х1.00'*),
+                                _2_2_1_5(*'2х2х1.50'*),
+                                _3_2_0_2(*'3х2х0.20'*),
+                                _3_2_0_35(*'3х2х0.35'*),
+                                _3_2_0_5(*'3х2х0.50'*),
+                                _3_2_0_75(*'3х2х0.75'*),
+                                _3_2_1_0(*'3х2х1.00'*),
+                                _3_2_1_5(*'3х2х1.50'*),
+                                _4_2_0_2(*'4х2х0.20'*),
+                                _4_2_0_35(*'4х2х0.35'*),
+                                _4_2_0_5(*'4х2х0.50'*),
+                                _4_2_0_75(*'4х2х0.75'*),
+                                _4_2_1_0(*'4х2х1.00'*),
+                                _4_2_1_5(*'4х2х1.50'*),
+                                _5_2_0_2(*'5х2х0.20'*),
+                                _5_2_0_35(*'5х2х0.35'*),
+                                _5_2_0_5(*'5х2х0.50'*),
+                                _5_2_0_75(*'5х2х0.75'*),
+                                _5_2_1_0(*'5х2х1.00'*),
+                                _5_2_1_5(*'5х2х1.50'*),
+                                _6_2_0_2(*'6х2х0.20'*),
+                                _6_2_0_35(*'6х2х0.35'*),
+                                _6_2_0_5(*'6х2х0.50'*),
+                                _6_2_0_75(*'6х2х0.75'*),
+                                _6_2_1_0(*'6х2х1.00'*),
+                                _6_2_1_5(*'6х2х1.50'*),
+                                _7_2_0_2(*'7х2х0.20'*),
+                                _7_2_0_35(*'7х2х0.35'*),
+                                _7_2_0_5(*'7х2х0.50'*),
+                                _7_2_0_75(*'7х2х0.75'*),
+                                _7_2_1_0(*'7х2х1.00'*),
+                                _7_2_1_5(*'7х2х1.50'*),
+                                _8_2_0_2(*'8х2х0.20'*),
+                                _8_2_0_35(*'8х2х0.35'*),
+                                _8_2_0_5(*'8х2х0.50'*),
+                                _8_2_0_75(*'8х2х0.75'*),
+                                _8_2_1_0(*'8х2х1.00'*),
+                                _8_2_1_5(*'8х2х1.50'*),
+                                _9_2_0_2(*'9х2х0.20'*),
+                                _9_2_0_35(*'9х2х0.35'*),
+                                _9_2_0_5(*'9х2х0.50'*),
+                                _9_2_0_75(*'9х2х0.75'*),
+                                _9_2_1_0(*'9х2х1.00'*),
+                                _9_2_1_5(*'9х2х1.50'*),
+                                _10_2_0_2(*'10х2х0.20'*),
+                                _10_2_0_35(*'10х2х0.35'*),
+                                _10_2_0_5(*'10х2х0.50'*),
+                                _10_2_0_75(*'10х2х0.75'*),
+                                _10_2_1_0(*'10х2х1.00'*),
+                                _10_2_1_5(*'10х2х1.50'*));
+
      TSPKBTEHNO_PPGNGA_FRHF_WCS=(_01_01_50(*'1х1.5'*),
                         _01_02_50(*'1х2.5'*),
                         _01_04_00(*'1х4'*),
@@ -49,12 +111,32 @@ type
     TSPKBTEHNO_PPGNGA_FRHF=packed object(CableDeviceBaseObject)
                 Wire_Count_Section_DESC:TSPKBTEHNO_PPGNGA_FRHF_WCS;
            end;
+    TSPKBTEHNO_TEHNOKIPNGAFRLS=packed object(CableDeviceBaseObject)
+                Wire_Count_Section_DESC:TSPKBTEHNO_TEHNOKIPNGAFRLS_WCS;
+           end;
 
 var
    _EQ_SPKBTEHNO_KPKEV_ng_LS:TSPKBTEHNO_KPKEV_ng_LS;
    _EQ_SPKBTEHNO_PPGNGA_FRHF:TSPKBTEHNO_PPGNGA_FRHF;
+   _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS:TSPKBTEHNO_TEHNOKIPNGAFRLS;
 implementation
 begin
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.initnul;
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.Category:=_kables;
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.Group:=_cables_sv;
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.EdIzm:=_m;
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.ID:='SPKBTEHNO_TEHNOKIPNGAFRLS';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.Standard:='ТУ 3574-014-53930360-2013';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.OKP:='35 7400';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.Manufacturer:='ЗАО "СПКБ Техно" г.Подольск';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.Description:='Кабель огнестойкий, интерфейсный симметричной парной скрутки с низким значением погонной емкости предназначен для высокоскоростной передачи и приема данных в системах противопожарной защиты, построенных по промышленному интерфейсу RS-485 в соответствии со стандартами ИСО/МЭК 8482, TIA/EIA RS-485-A, на промышленных интерфейсах CAN, HART и других,а также в других системах  связи, контроля и управления, которые должны сохранять работоспособность в течение 180 минут в условиях воздействия открытого пламени.';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.NameShortTemplate:='ТехноКИПнг(А)-FRLS %%[Wire_Count_Section_DESC]';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.NameTemplate:='Кабель огнестойкий, интерфейсный симметричной парной скрутки  с низким значением погонной емкости ТехноКИПнг(А)-FRLS %%[Wire_Count_Section_DESC]';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.UIDTemplate:='%%[ID]-%%[Wire_Count_Section_DESC]';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.NameFullTemplate:='Кабель огнестойкий, интерфейсный симметричной парной скрутки  с низким значением погонной емкости ТехноКИПнг(А)-FRLS %%[Wire_Count_Section_DESC]';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.Wire_Count_Section_DESC:=_1_2_0_75;
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.TreeCoord:='BP_СПКБ Техно_Для ОПС_ТехноКИПнг(А)-FRLS|BC_Кабельная продукция_Связи_ТехноКИПнг(А)-FRLS(СПКБ Техно)';
+     _EQ_SPKBTEHNO_TEHNOKIPNGAFRLS.format;
 
      _EQ_SPKBTEHNO_KPKEV_ng_LS.initnul;
      _EQ_SPKBTEHNO_KPKEV_ng_LS.Category:=_kables;

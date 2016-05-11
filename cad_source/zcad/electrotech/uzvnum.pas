54d4d88c1e5dc920d7ffcd323bc1005ba06cb302
veb86 <veb86@d67b0292-7a95-df11-8fcc-003048937916>
2016-05-11 14:16:28 +0000

    git-svn-id: http://svn.shamangrad.net/zcad/trunk@1942 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/zcad/electrotech/uzvnum.pas
@@ -0,0 +1,72 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+unit uzvnum;
+{$INCLUDE def.inc}
+
+interface
+uses uzcenitiesvariablesextender,sysutils,UGDBOpenArrayOfPV,uzbtypesbase,uzbtypes,
+     uzeentity,varmandef,uzeentsubordinated,
+
+     uzvcom;
+function FindVariableInEnt(PEnt:PGDBObjEntity;varname:gdbstring):pvardesk;
+function FindEntityByVar(arr:GDBObjOpenArrayOfPV;objID:GDBWord;vname,vvalue:GDBString):PGDBObjSubordinated;
+implementation
+function FindVariableInEnt(PEnt:PGDBObjEntity;varname:gdbstring):pvardesk;
+var
+   pentvarext:PTVariablesExtender;
+begin
+     pentvarext:=PEnt^.GetExtension(typeof(TVariablesExtender));
+     result:=nil;
+     if pentvarext<>nil then
+     result:=pentvarext^.entityunit.FindVariable(varname);
+     if result=nil then
+     if PEnt^.bp.ListPos.Owner<>nil then
+       result:=FindVariableInEnt(pointer(PEnt^.bp.ListPos.Owner),varname);
+end;
+function FindEntityByVar(arr:GDBObjOpenArrayOfPV;objID:GDBWord;vname,vvalue:GDBString):PGDBObjSubordinated;
+var
+   pvisible:PGDBObjEntity;
+   ir:itrec;
+   pvd:pvardesk;
+   pentvarext:PTVariablesExtender;
+begin
+     result:=nil;
+     begin
+         pvisible:=arr.beginiterate(ir);
+         if pvisible<>nil then
+         repeat
+               if pvisible.GetObjType=objID then
+               begin
+                    pentvarext:=pvisible^.GetExtension(typeof(TVariablesExtender));
+                    pvd:=pentvarext^.entityunit.FindVariable(vname);
+                    if pvd<>nil then
+                    begin
+                         if pvd.data.PTD.GetValueAsString(pvd.data.Instance)=vvalue then
+                         begin
+                              result:=pvisible;
+                              exit;
+                         end;
+                    end;
+               end;
+              pvisible:=arr.iterate(ir);
+         until pvisible=nil;
+     end;
+end;
+
+end.
+

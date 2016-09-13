228f84f467dce7e883c1df9e9dac63b724136b5c
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2016-09-13 21:52:05 +0000

    Fix pudgb
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2002 d67b0292-7a95-df11-8fcc-003048937916

--- other/pudgb/uscaner.pas
+++ other/pudgb/uscaner.pas
@@ -5,7 +5,7 @@ unit uscaner;
 interface
 uses
   LazUTF8,Classes, SysUtils,
-  uoptions,uscanresult,
+  uoptions,uscanresult,ufileutils,
   PParser, PasTree;
 
 type
@@ -38,47 +38,6 @@ function TSimpleEngine.FindElement(const AName: String): TPasElement;
 begin
   { dummy implementation, see TFPDocEngine.FindElement for a real example }
   Result := nil;
-end;
-
-function GetPartOfPath(out part:String;var path:String;const separator:String):String;
-var
-   i:Integer;
-begin
-           i:=pos(separator,path);
-           if i<>0 then
-                       begin
-                            part:=copy(path,1,i-1);
-                            path:=copy(path,i+1,length(path)-i);
-                       end
-                   else
-                       begin
-                            part:=path;
-                            path:='';
-                       end;
-     result:=part;
-end;
-function FindInSupportPath(PPaths:String;FileName:String):String;
-var
-   s,ts:string;
-begin
-     if FileExists(utf8tosys(FileName)) then
-                                 begin
-                                      result:=FileName;
-                                      exit;
-                                 end;
-     begin
-     s:=PPaths;
-     repeat
-           GetPartOfPath(ts,s,';');
-           ts:=ts+FileName;
-           if FileExists(utf8tosys(ts)) then
-                                 begin
-                                      result:=ts;
-                                      exit;
-                                 end;
-     until s='';
-     end;
-     result:='';
 end;
 procedure PrepareModule(M:TPasModule;Options:TOptions;ScanResult:TScanResult;const LogWriter:TLogWriter);
 var

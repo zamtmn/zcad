16a4c6c4a079e0cd0f64f4cd97d4a4fda60459d8
arinelex <arinelex@d67b0292-7a95-df11-8fcc-003048937916>
2016-09-22 12:12:48 +0000

    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2019 d67b0292-7a95-df11-8fcc-003048937916

--- other/pudgb/uwriter.pas
+++ other/pudgb/uwriter.pas
@@ -12,7 +12,12 @@ uses
 
 procedure WriteGraph(Options:TOptions;ScanResult:TScanResult;const LogWriter:TLogWriter);
 procedure ProcessNode(Options:TOptions;var Node:TUnitInfo;const index:integer;const LogWriter:TLogWriter;ForceInclude:boolean=false);
-
+// begin arinelex
+// потом продумаю подробнее параметры
+procedure WriteGML (Options:TOptions;ScanResult:TScanResult;const LogWriter:TLogWriter);
+procedure NodeGML (Options:TOptions;var Node:TUnitInfo;const index:integer;const LogWriter:TLogWriter;ForceInclude:boolean=false);
+procedure GraphGML (Options:TOptions; var Node:TUnitInfo;); //проставление связей. (второй проход по узлам) параметры пока не ставлю специально.
+// end arinelex
 implementation
 function IncludeToGraph(Options:TOptions;var Node:TUnitInfo;const index:integer;const LogWriter:TLogWriter):boolean;
 begin
@@ -98,6 +103,85 @@ begin
 
   end;
 end;
+
+// begin arinelex
+procedure WriteGML(Options: TOptions; ScanResult: TScanResult;
+  const LogWriter: TLogWriter);
+var
+  i,j:integer;
+  s:string;
+begin
+  if assigned(LogWriter) then
+  begin
+  //шапка файла
+    LogWriter(Title);
+    LogWriter (Vers);
+    LogWriter (BracketLeft);
+    // конец шапки
+    if assigned(ScanResult) then
+    begin
+      for i:=0 to ScanResult.UnitInfoArray.Size-1 do
+       ScanResult.UnitInfoArray.mutable[i]^.NodeState:=NSNotCheced;
+
+    if Options.GraphBulding.IncludeInterfaceUses then
+    for i:=0 to ScanResult.UnitInfoArray.Size-1 do
+    begin
+     if ScanResult.UnitInfoArray[i].InterfaceUses.Size>0 then
+     begin
+     // изменить
+     ProcessNode(Options,ScanResult.UnitInfoArray.Mutable[i]^,i,LogWriter);
+       if ScanResult.UnitInfoArray[i].NodeState<>NSFiltredOut then
+       for j:=0 to ScanResult.UnitInfoArray[i].InterfaceUses.Size-1 do
+       begin
+         ProcessNode(Options,ScanResult.UnitInfoArray.Mutable[ScanResult.UnitInfoArray[i].InterfaceUses[j]]^,ScanResult.UnitInfoArray[i].InterfaceUses[j],LogWriter);
+         if ScanResult.UnitInfoArray[ScanResult.UnitInfoArray[i].InterfaceUses[j]].NodeState<>NSFiltredOut then
+         begin
+         if Options.GraphBulding.InterfaceUsesEdgeType=ETDotted then //изменить
+                                                                    LogWriter(' edge [style=dotted]');
+         LogWriter(format(' %s -> %s',[ScanResult.UnitInfoArray[i].UnitName,ScanResult.UnitInfoArray[ScanResult.UnitInfoArray[i].InterfaceUses[j]].UnitName]));
+         end;
+       end;
+     end;
+    end;
+
+    if Options.GraphBulding.IncludeImplementationUses then
+    for i:=0 to ScanResult.UnitInfoArray.Size-1 do
+    begin
+     if ScanResult.UnitInfoArray[i].NodeState<>NSFiltredOut then
+     if ScanResult.UnitInfoArray[i].ImplementationUses.Size>0 then
+     begin
+       for j:=0 to ScanResult.UnitInfoArray[i].ImplementationUses.Size-1 do
+       begin
+         ProcessNode(Options,ScanResult.UnitInfoArray.Mutable[ScanResult.UnitInfoArray[i].ImplementationUses[j]]^,ScanResult.UnitInfoArray[i].ImplementationUses[j],LogWriter);
+         if ScanResult.UnitInfoArray[ScanResult.UnitInfoArray[i].ImplementationUses[j]].NodeState<>NSFiltredOut then
+         begin
+         if Options.GraphBulding.ImplementationUsesEdgeType=ETDotted then  //изменить
+                                                                         LogWriter(' edge [style=dotted]');
+           LogWriter(format(' %s -> %s',[ScanResult.UnitInfoArray[i].UnitName,ScanResult.UnitInfoArray[ScanResult.UnitInfoArray[i].ImplementationUses[j]].UnitName]));
+         end;
+       end;
+     end;
+    end;
+    end;
+
+    LogWriter(BracketRight);      //конец файла потом проверить
+
+
+  end;
+end;
+
+procedure NodeGML(Options: TOptions; var Node: TUnitInfo; const index: integer;
+  const LogWriter: TLogWriter; ForceInclude: boolean);
+begin
+
+end;
+
+procedure GraphGML;
+begin
+
+end;
+
+// end arinelex
 
 end.
 

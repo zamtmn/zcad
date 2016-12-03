4d5efaf8fcfcf8f49a4496a71c445151696149d4
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2016-12-03 07:03:48 +0000

    Improve Navigator window
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2118 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zcad/gui/forms/uzcfnavigator.pas
+++ cad_source/zcad/gui/forms/uzcfnavigator.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
-  StdCtrls, ActnList, VirtualTrees;
+  StdCtrls, ActnList, VirtualTrees,
+  uzcimagesmanager;
 
 type
 
@@ -14,16 +15,23 @@ type
 
   TNavigator = class(TForm)
     MainToolBar: TToolBar;
+    NavTree: TVirtualStringTree;
     ToolButton1: TToolButton;
     RefreshToolButton: TToolButton;
     ToolButton3: TToolButton;
-    NavTree: TVirtualStringTree;
     ActionList1:TActionList;
     Refresh:TAction;
+    procedure RefreshTree(Sender: TObject);
+    procedure _onCreate(Sender: TObject);
+    procedure NavGetText(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
+                         TextType: TVSTTextType; var CellText: String);
+    procedure NavGetImage(Sender: TBaseVirtualTree; Node: PVirtualNode; Kind: TVTImageKind; Column: TColumnIndex;
+                          var Ghosted: Boolean; var ImageIndex: Integer);
+
   private
 
   public
-
+    PRootNode1,PRootNode2:PVirtualNode;
   end;
 
 var
@@ -32,5 +40,55 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TNavigator }
+
+procedure TNavigator._onCreate(Sender: TObject);
+begin
+   ActionList1.Images:=IconList;
+   MainToolBar.Images:=IconList;
+   Refresh.ImageIndex:=II_Refresh;
+end;
+
+procedure TNavigator.RefreshTree(Sender: TObject);
+var
+  i:integer;
+begin
+   NavTree.BeginUpdate;
+   NavTree.Clear;
+   NavTree.Images:=IconList;
+   PRootNode1:=NavTree.AddChild(nil,nil);
+   PRootNode2:=NavTree.AddChild(nil,nil);
+   NavTree.OnGetText:=NavGetText;
+   NavTree.OnGetImageIndex:=NavGetImage;
+   for i:=0 to 9 do
+      NavTree.AddChild(PRootNode1,nil);
+   for i:=0 to 9 do
+      NavTree.AddChild(PRootNode2,nil);
+   NavTree.EndUpdate;
+end;
+
+procedure TNavigator.NavGetText(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
+                         TextType: TVSTTextType; var CellText: String);
+begin
+     if Node=PRootNode1 then
+                            CellText:='Root1'
+else if Node=PRootNode2 then
+                            CellText:='Root2'
+else
+  begin
+  end;
+end;
+procedure TNavigator.NavGetImage(Sender: TBaseVirtualTree; Node: PVirtualNode; Kind: TVTImageKind; Column: TColumnIndex;
+                                 var Ghosted: Boolean; var ImageIndex: Integer);
+begin
+     if Node=PRootNode1 then
+                            ImageIndex:=1
+else if Node=PRootNode2 then
+                            ImageIndex:=2
+else
+  begin
+  end;
+end;
 end.
 

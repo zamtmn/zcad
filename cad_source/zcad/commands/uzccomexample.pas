034b1c6408575212351a3bdb9cd1d37a4b5df450
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2016-12-15 09:03:54 +0000

    Improve DrawSuperLine params
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2131 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zcad/commands/uzccomexample.pas
+++ cad_source/zcad/commands/uzccomexample.pas
@@ -96,8 +96,9 @@ uses
                       //"Менеджер" чертежей
   uzcutils,         //different functions simplify the creation entities, while there are very few
                       //разные функции упрощающие создание примитивов, пока их там очень мало
+  uzestyleslayers,
   varmandef,
-  Varman,
+  Varman,UBaseTypeDescriptor,
   {UGDBOpenArrayOfUCommands,}zcchangeundocommand,
 
   uzcstrconsts,       //resouce strings
@@ -138,7 +139,9 @@ type
 {EXPORT-}
 PTDrawSuperlineParams=^TDrawSuperlineParams;
 TDrawSuperlineParams=packed record
-                         pu:PTUnit;
+                         pu:PTUnit;                //рантайм юнит с параметрами суперлинии
+                         LayerNamePrefix:GDBString;//префикс
+                         ProcessLayer:GDBBoolean;  //выключатель
                      end;
 var
    MatchPropParam:TMatchPropParam; //**< Переменная содержащая опции команды MatchProp
@@ -837,6 +840,10 @@ var
     UndoMarcerIsPlazed:boolean;
 
 procedure createline;
+var
+    pvd:pvardesk;        //для нахождения имени суперлинии
+    layername:gdbstring; //имя слоя куда будет помещена супелиния
+    player:PGDBLayerProp;//указатель на слой куда будет помещена супелиния
 begin
     psuperline := AllocEnt(GDBSuperLineID);
     psuperline^.init(nil,nil,0,p1,p2);
@@ -847,7 +854,26 @@ begin
       if psu<>nil then
         pvarext^.entityunit.copyfrom(psu);
     end;
-    zcSetEntPropFromCurrentDrawingProp(psuperline);
+    zcSetEntPropFromCurrentDrawingProp(psuperline);           //присваиваем умолчательные значения
+    //если манипуляции со слоем включены и ранее был найден "юнит" с параметрами
+    if (DrawSuperlineParams.ProcessLayer)and(psu<>nil) then
+    begin
+      //ищем переменную 'NMO_Name'
+      pvd:=psu.FindVariable('NMO_Name');
+      //если найдена
+      if pvd<>nil then
+      begin
+        //получаем желаемое имя слоя
+        layername:=DrawSuperlineParams.LayerNamePrefix+pvd.data.PTD^.GetValueAsString(pvd.data.Instance);
+        //ищем описание слоя по имени
+        player:=drawings.GetCurrentDWG.LayerTable.getAddres(layername);
+        //если найден - присваиваем, иначе ругаемя
+        if player<>nil then
+                           psuperline.vp.Layer:=player
+                       else
+                           if assigned(HistoryOutStr) then HistoryOutStr(format('Layer "%s" not found',[layername]));
+      end;
+    end;
     zcPlaceUndoStartMarkerIfNeed(UndoMarcerIsPlazed,'DrawSuperLine');
     zcAddEntToCurrentDrawingWithUndo(psuperline);
     zcRedrawCurrentDrawing;
@@ -924,6 +950,9 @@ initialization
      CreateCommandFastObjectPlugin(@matchprop_com,       'MatchProp',  CADWG,0);
 
      SysUnit.RegisterType(TypeInfo(TDrawSuperlineParams));//регистрируем тип данных в зкадном RTTI
+     SysUnit.SetTypeDesk(TypeInfo(TDrawSuperlineParams),['SuperLineUnit','Layer name prefix','Layer change']);//даем человеческие имена параметрам
+     DrawSuperlineParams.LayerNamePrefix:='SYS_SL_';//начальное значение префикса
+     DrawSuperlineParams.ProcessLayer:=true;        //начальное значение выключателя
      CreateCommandFastObjectPlugin(@DrawSuperLine_com,   'DrawSuperLine',   CADWG,0);
      CreateCommandFastObjectPlugin(@InsertDevice_com,    'ID',   CADWG,0);
 

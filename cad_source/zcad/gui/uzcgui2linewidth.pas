126d87b5ac938a13a2222b15473149d684a88e95
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2017-01-24 05:25:36 +0000

    Separate GUI LineW combos
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2163 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/zcad/gui/uzcgui2linewidth.pas
+++ cad_source/zcad/gui/uzcgui2linewidth.pas
@@ -27,12 +27,15 @@ uses
 
 type
   TSupportLineWidthCombo = class
+                             class procedure LineWBoxDrawIVarsItem(Control: TWinControl; Index: Integer; ARect: TRect;
+                                                              State: StdCtrls.TOwnerDrawState);
                              class procedure LineWBoxDrawItem(Control: TWinControl; Index: Integer; ARect: TRect;
                                                               State: StdCtrls.TOwnerDrawState);
+
   end;
 
 implementation
-class procedure TSupportLineWidthCombo.LineWBoxDrawItem(Control: TWinControl; Index: Integer; ARect: TRect;
+class procedure TSupportLineWidthCombo.LineWBoxDrawIVarsItem(Control: TWinControl; Index: Integer; ARect: TRect;
                                                         State: StdCtrls.TOwnerDrawState);
 var
    ll:integer;
@@ -55,5 +58,20 @@ begin
     ARect.Left:=ARect.Left+2;
     drawLW(TComboBox(Control).canvas,ARect,ll,(index) div 10,s);
 end;
-
+class procedure TSupportLineWidthCombo.LineWBoxDrawItem(Control: TWinControl; Index: Integer; ARect: TRect;
+                                                        State: StdCtrls.TOwnerDrawState);
+var
+   ll:integer;
+   s:string;
+begin
+   ComboBoxDrawItem(Control,ARect,State);
+   index:=integer(tcombobox(Control).items.Objects[Index]);
+   s:=GetLWNameFromLW(index);
+   if (index<4)or(index=ClDifferent) then
+              ll:=0
+          else
+              ll:=30;
+    ARect.Left:=ARect.Left+2;
+    drawLW(TComboBox(Control).canvas,ARect,ll,(index) div 10,s);
+end;
 end.

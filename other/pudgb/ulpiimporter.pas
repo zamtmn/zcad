4515b3db68fb0007ba3a85a0466d018a46e21abf
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2017-02-09 23:38:28 +0000

    Start Lazarus IDE macros support for LPI importer
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2164 d67b0292-7a95-df11-8fcc-003048937916

--- other/pudgb/ulpiimporter.pas
+++ other/pudgb/ulpiimporter.pas
@@ -6,30 +6,133 @@ interface
 
 uses
   Classes, SysUtils,Laz2_XMLCfg,LazUTF8,
-  uoptions,ufileutils;
+  uoptions,ufileutils,LazFileUtils,Laz2_DOM,
+  MacroIntf,TransferMacros,MacroDefIntf;//From lazarus ide
+
+type
+  TMacroMethods=class
+      Options:TOptions;
+      UnitOutputDirectory:string;
+      packagefiles:TXMLConfig;
+      function MacroFuncTargetCPU(const {%H-}Param: string; const Data: PtrInt;
+                                    var {%H-}Abort: boolean): string;
+      function MacroFuncTargetOS (const {%H-}Param: string; const Data: PtrInt;
+                                    var {%H-}Abort: boolean): string;
+      function MacroFuncProjOutDir (const {%H-}Param: string; const Data: PtrInt;
+                                      var {%H-}Abort: boolean): string;
+      function MacroFuncPkgDir (const {%H-}Param: string; const Data: PtrInt;
+                                  var {%H-}Abort: boolean): string;
+      constructor create;
+  end;
 
 procedure LPIImport(var Options:TOptions;const filename:string;const LogWriter:TLogWriter);
 
 implementation
+constructor TMacroMethods.create;
+var
+ LazarusPackageFilesConfig:string;
+begin
+ inherited;
+ LazarusPackageFilesConfig:=AppendPathDelim(ExtractFilePath(ChompPathDelim(GetAppConfigDirUTF8(False)))+'lazarus')+'packagefiles.xml';
+ packagefiles:=TXMLConfig.Create(LazarusPackageFilesConfig);
+end;
+
+function TMacroMethods.MacroFuncTargetCPU(const Param: string;
+  const Data: PtrInt; var Abort: boolean): string;
+begin
+    Result:=Options.ParserOptions.TargetCPU;
+end;
+function TMacroMethods.MacroFuncTargetOS(const Param: string;
+  const Data: PtrInt; var Abort: boolean): string;
+begin
+    Result:=Options.ParserOptions.TargetOS;
+end;
+function TMacroMethods.MacroFuncProjOutDir(const Param: string;
+  const Data: PtrInt; var Abort: boolean): string;
+begin
+    Result:=UnitOutputDirectory;
+end;
+function TMacroMethods.MacroFuncPkgDir(const Param: string;
+  const Data: PtrInt; var Abort: boolean): string;
+var
+ node,subnode,namenode:TDomNode;
+ UCParam,deb:string;
+begin
+    UCParam:=uppercase(param);
+    Result:='Something wrong';
+    node:=packagefiles.FindNode('UserPkgLinks',false);
+    deb:=node.NodeValue;
+    deb:=node.NodeName;
+    deb:=node.TextContent;
+    subnode:= node.FirstChild;
+    while assigned(subnode)do
+    begin
+       deb:=subnode.NodeValue;
+       deb:=subnode.NodeName;
+       deb:=subnode.TextContent;
+       namenode:=subnode.FindNode('Name');
+       if assigned(namenode) then
+       namenode:=namenode.Attributes.GetNamedItem('Value');
+       if assigned(namenode) then
+       begin
+           deb:=namenode.NodeValue;
+           deb:=namenode.NodeName;
+           deb:=namenode.TextContent;
+         if UCParam=uppercase(namenode.NodeValue) then
+         begin
+           namenode:=subnode.FindNode('Filename');
+           if assigned(namenode) then
+           namenode:=namenode.Attributes.GetNamedItem('Value');
+           if assigned(namenode) then
+             result:=ExtractFileDir(namenode.NodeValue);
+           break;
+         end;
+       end;
+       subnode:= subnode.NextSibling;
+    end;
+end;
 
 procedure LPIImport(var Options:TOptions;const filename:string;const LogWriter:TLogWriter);
 var
  Doc:TXMLConfig;
  j:integer;
- IncludeFiles,OtherUnitFiles,UnitOutputDirectory,MainFilename:string;
+ IncludeFiles,OtherUnitFiles,MainFilename:string;
  FileVersion:integer;
  basepath:string;
  opt,s,ts:string;
+ IDEMacros: TIDEMacros;
+ tmm:TMacroMethods;
 begin
+ IDEMacros:=TLazIDEMacros.Create;
+ GlobalMacroList:=TTransferMacroList.Create;
+ tmm:=TMacroMethods.create;
+ tmm.Options:=Options;
+ GlobalMacroList.Add(TTransferMacro.Create('TargetCPU','',
+                     'Target CPU',@tmm.MacroFuncTargetCPU,[]));
+ GlobalMacroList.Add(TTransferMacro.Create('TargetOS','',
+                     'Target OS',@tmm.MacroFuncTargetOS,[]));
+ GlobalMacroList.Add(TTransferMacro.Create('ProjOutDir','',
+                     'Project out dir',@tmm.MacroFuncProjOutDir,[]));
+ GlobalMacroList.Add(TTransferMacro.Create('PkgDir','',
+                     'PkgDir',@tmm.MacroFuncPkgDir,[]));
+
  Doc:=TXMLConfig.Create(filename);
  FileVersion:=Doc.GetValue('CompilerOptions/Version/Value', 0);
  LogWriter('Version='+inttostr(FileVersion));
+
+ tmm.UnitOutputDirectory:=Doc.GetValue('CompilerOptions/SearchPaths/UnitOutputDirectory/Value','');
+ LogWriter('UnitOutputDirectory='+tmm.UnitOutputDirectory);
+ if IDEMacros.SubstituteMacros(tmm.UnitOutputDirectory) then
+                                                    LogWriter('Resolve to UnitOutputDirectory='+tmm.UnitOutputDirectory);
+
  IncludeFiles:=Doc.GetValue('CompilerOptions/SearchPaths/IncludeFiles/Value','');
  LogWriter('IncludeFiles='+IncludeFiles);
+ if IDEMacros.SubstituteMacros(IncludeFiles) then
+                                                 LogWriter('Resolve to IncludeFiles='+IncludeFiles);
  OtherUnitFiles:=Doc.GetValue('CompilerOptions/SearchPaths/OtherUnitFiles/Value','');
  LogWriter('OtherUnitFiles='+OtherUnitFiles);
- UnitOutputDirectory:=Doc.GetValue('CompilerOptions/SearchPaths/UnitOutputDirectory/Value','');
- LogWriter('UnitOutputDirectory='+UnitOutputDirectory);
+ if IDEMacros.SubstituteMacros(OtherUnitFiles) then
+                                                   LogWriter('Resolve to OtherUnitFiles='+OtherUnitFiles);
  MainFilename:=Doc.GetValue('ProjectOptions/Units/Unit0/Filename/Value','');
  LogWriter('Unit0='+MainFilename);
  Doc.Free;

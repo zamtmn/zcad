1f1a5d950db25edcd621485fc8091e80228bf17d
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2017-03-01 07:50:03 +0000

    Add forgot file
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2193 d67b0292-7a95-df11-8fcc-003048937916

---
+++ cad_source/components/zcontainers/gzctnrvectortypes.pas
@@ -0,0 +1,34 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{**
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
+}
+{**Модуль описания базового генерика обьекта-массива}
+unit gzctnrvectortypes;
+{$INCLUDE def.inc}
+interface
+uses uzbmemman,{uzbtypesbase,}sysutils,{uzbtypes,}typinfo;
+type
+{Export+}
+  itrec=packed record
+              itp:{-}PPointer{/Pointer/};
+              itc:Integer;
+        end;
+  PTArrayIndex=^TArrayIndex;
+  TArrayIndex=Integer;
+{Export-}
+implementation
+begin
+end.

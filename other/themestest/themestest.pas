0cbbf96c031b420e4c19a52c93af0cad5fb0244b
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2017-05-14 17:46:05 +0000

    Add themestest
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2262 d67b0292-7a95-df11-8fcc-003048937916

---
+++ other/themestest/themestest.pas
@@ -0,0 +1,21 @@
+program themestest;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, umainform
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource := True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+

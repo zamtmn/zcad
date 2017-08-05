e693367be45c2519c70677bf5bab00489e5fdb99
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2017-08-05 23:06:02 +0000

    Continue new docking system and toolbars
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2294 d67b0292-7a95-df11-8fcc-003048937916

--- cad_source/components/zcontrols/uztoolbarsutils.pas
+++ cad_source/components/zcontrols/uztoolbarsutils.pas
@@ -8,21 +8,31 @@ uses
   Classes, SysUtils, ComCtrls, Controls, Graphics, Menus, Forms,
   LazConfigStorage;
 
+type
+  TTBCreateFunc=function (aName: string):TToolBar of object;
+
 procedure SaveToolBarsToConfig(MainForm:TForm; Config: TConfigStorage);
+procedure RestoreToolBarsFromConfig(MainForm:TForm; Config: TConfigStorage;TBCreateFunc:TTBCreateFunc);
 
 implementation
 
-
+function IsFloatToolbar(tb:TToolBar;out tf:TCustomDockForm):boolean;
+begin
+  tf:=TCustomDockForm(tb.Parent);
+  if tf is TCustomDockForm then
+    result:=true
+  else
+    result:=false;
+end;
 
 procedure SaveToolBarsToConfig(MainForm:TForm; Config: TConfigStorage);
 var
-  i,j,fcount,ccount:integer;
+  i,j,ItemCount:integer;
   cb:TCoolBar;
   tb:TToolBar;
   tf:TCustomDockForm;
 begin
-  fcount:=0;
-  ccount:=0;
+  ItemCount:=0;
   Config.AppendBasePath('ToolBarsConfig/');
   for i:=0 to MainForm.ComponentCount-1 do
   if MainForm.Components[i] is TControl then
@@ -30,12 +40,15 @@ begin
     if MainForm.Components[i] is TCoolBar then
     begin
       cb:=MainForm.Components[i] as TCoolBar;
-      Config.AppendBasePath('CoolBar'+inttostr(ccount));
-      inc(ccount);
+      Config.AppendBasePath('Item'+inttostr(ItemCount));
+      inc(ItemCount);
+      Config.SetDeleteValue('Type','CoolBar','');
       Config.SetDeleteValue('Name',cb.Name,'');
+      Config.SetDeleteValue('ItemCount',cb.Bands.Count,-1);
       for j:=0 to cb.Bands.Count-1 do
       begin
-        Config.AppendBasePath('ToolBar'+inttostr(j));
+        Config.AppendBasePath('Item'+inttostr(j));
+        Config.SetDeleteValue('Type','ToolBar','');
         Config.SetDeleteValue('Name',cb.Bands[j].Control.Name,'');
         Config.UndoAppendBasePath;
       end;
@@ -44,16 +57,137 @@ begin
     if MainForm.Components[i] is TToolBar then
     begin
       tb:=MainForm.Components[i] as TToolBar;
-      tf:=TCustomDockForm(tb.Parent);
-      if tf is TCustomDockForm then
+      if IsFloatToolbar(tb,tf) then
       begin
-        Config.AppendBasePath('FloatToolbar'+inttostr(fcount));
-        inc(fcount);
+        Config.AppendBasePath('Item'+inttostr(ItemCount));
+        inc(ItemCount);
+        Config.SetDeleteValue('Type','FloatToolBar','');
         Config.SetDeleteValue('Name',tb.name,'');
         Config.SetDeleteValue('BoundsRect',tf.BoundsRect,Rect(0,0,0,0));
         Config.UndoAppendBasePath;
       end;
     end;
+  end;
+  Config.SetDeleteValue('ItemCount',ItemCount,0);
+  Config.UndoAppendBasePath;
+end;
+procedure FreeAllToolBars(MainForm:TForm);
+var
+  i,j:integer;
+  cb:TCoolBar;
+  tb:TToolBar;
+  tf:TCustomDockForm;
+begin
+  for i:=MainForm.ComponentCount-1 downto 0 do
+  if MainForm.Components[i] is TControl then
+  begin
+    if MainForm.Components[i] is TCoolBar then
+    begin
+      cb:=MainForm.Components[i] as TCoolBar;
+      for j:=0 to cb.Bands.Count-1 do
+      begin
+        cb.Bands[j].Control.Free;
+      end;
+    end;
+    if MainForm.Components[i] is TToolBar then
+    begin
+      tb:=MainForm.Components[i] as TToolBar;
+      if IsFloatToolbar(tb,tf) then
+      begin
+        tb.Free;
+      end;
+    end;
+  end;
+end;
+function FindCoolBar(MainForm:TForm;Name:string):TCoolBar;
+var
+  i:integer;
+begin
+  for i:=MainForm.ComponentCount-1 downto 0 do
+  if MainForm.Components[i] is TCoolBar then
+  if (MainForm.Components[i] as TCoolBar).Name=Name then
+  begin
+    result:=MainForm.Components[i] as TCoolBar;
+    exit;
+  end;
+  result:=nil;
+end;
+
+function CreateFloatingDockSite(tb:TToolBar; const Bounds: TRect): TWinControl;
+var
+  FloatingClass: TWinControlClass;
+  NewWidth: Integer;
+  NewHeight: Integer;
+  NewClientWidth: Integer;
+  NewClientHeight: Integer;
+begin
+  Result := nil;
+  FloatingClass:=tb.FloatingDockSiteClass;
+  if (FloatingClass<>nil) and (FloatingClass<>TWinControlClass(tb.ClassType)) then
+  begin
+    Result := TWinControl(FloatingClass.NewInstance);
+    Result.DisableAutoSizing{$IFDEF DebugDisableAutoSizing}('TControl.CreateFloatingDockSite'){$ENDIF};
+    Result.Create(tb);
+    // resize with minimal resizes
+    NewClientWidth:=Bounds.Right-Bounds.Left;
+    NewClientHeight:=Bounds.Bottom-Bounds.Top;
+    NewWidth:=Result.Width-Result.ClientWidth+NewClientWidth;
+    NewHeight:=Result.Height-Result.ClientHeight+NewClientHeight;
+    Result.SetBounds(Bounds.Left,Bounds.Top,NewWidth,NewHeight);
+    //Result.SetClientSize(Point(NewClientWidth,NewClientHeight));
+    {$IFDEF DebugDisableAutoSizing}
+    debugln('TControl.CreateFloatingDockSite A ',DbgSName(Self),' ',DbgSName(Result),' ',dbgs(Result.BoundsRect));
+    {$ENDIF}
+    Result.EnableAutoSizing{$IFDEF DebugDisableAutoSizing}('TControl.CreateFloatingDockSite'){$ENDIF};
+  end;
+end;
+
+procedure RestoreToolBarsFromConfig(MainForm:TForm; Config: TConfigStorage;TBCreateFunc:TTBCreateFunc);
+var
+  i,j,ItemCount:integer;
+  itemName,itemType:string;
+  cb:TCoolBar;
+  tb:TToolBar;
+  r:trect;
+  FloatHost: TWinControl;
+begin
+  FreeAllToolBars(MainForm);
+  Config.AppendBasePath('ToolBarsConfig/');
+  ItemCount:=Config.GetValue('ItemCount',0);
+  for i:=0 to ItemCount-1 do
+  begin
+    Config.AppendBasePath('Item'+IntToStr(i)+'/');
+    itemType:=Config.GetValue('Type','');
+    itemName:=Config.GetValue('Name','');
+    case itemType of
+     'CoolBar':begin
+                 cb:=FindCoolBar(MainForm,itemName);
+                 ItemCount:=Config.GetValue('ItemCount',0);
+                 if cb<>nil then
+                 for j:=0 to ItemCount-1 do
+                 begin
+                   Config.AppendBasePath('Item'+IntToStr(j)+'/');
+                   itemType:=Config.GetValue('Type','');
+                   itemName:=Config.GetValue('Name','');
+                   tb:=TBCreateFunc(itemName);
+                   cb.InsertControl(tb,j);
+                   Config.UndoAppendBasePath;
+                 end;
+               end;
+'FloatToolBar':begin
+                 Config.GetValue('BoundsRect',r,Rect(0,0,0,0));
+                 tb:=TBCreateFunc(itemName);
+
+                 FloatHost := CreateFloatingDockSite(tb,r);
+                 if FloatHost <> nil then
+                 begin
+                   tb.Parent:=FloatHost;
+                   FloatHost.Caption := FloatHost.GetDockCaption(tb);
+                   FloatHost.Show;
+                 end;
+               end;
+    end;
+    Config.UndoAppendBasePath;
   end;
   Config.UndoAppendBasePath;
 end;

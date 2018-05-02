53afd195b6fb4ec16bd8896b91766af5b4616aa6
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2018-05-02 04:58:49 +0000

    restructure
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2545 d67b0292-7a95-df11-8fcc-003048937916

--- run7z.bat
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-call deltmp.bat
-deltmp.bat
-rm cad.7z cad_source.7z
-7z a cad.7z cad/ -ms=off  -xr!*.svn
-7z a cad_source.7z cad_source/ -ms=off -xr!*.svn
--- runrar.bat
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-call deltmp
-del c:\cad.rar;c:\cad_source.rar
-rar a -r -m5 -x*\.svn\* -ed c:\cad.rar cad
-rar a -r -m5 -x*\.svn\* -ed c:\cad_source.rar cad_source

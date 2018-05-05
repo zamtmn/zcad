6c3729d1a71041cabc5fd73863cd00c192bb2ab0
zamtmn <zamtmn@d67b0292-7a95-df11-8fcc-003048937916>
2018-05-05 12:28:45 +0000

    Improve environment build scripts
    git-svn-id: http://svn.shamangrad.net/zcad/trunk@2547 d67b0292-7a95-df11-8fcc-003048937916

--- zcad.bat
+++ zcad.bat
@@ -1,2 +1,8 @@
+@echo off
 lazbuild cad_source\utils\typeexporter.lpi
+if %errorlevel% neq 0 (
+   Echo Error when running lazbuild. Check lazbuild in %%PATCH%%
+   pause
+   exit
+)
 environment\makeenv_zcad.bat

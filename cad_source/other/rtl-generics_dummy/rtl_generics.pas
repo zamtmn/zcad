6a7dd1c064e25a920a61fa3e3d5e75834649ba77
zamtmn <zamtmn@yandex.ru>
2018-10-15 23:42:47 +0500

    Add rtl-generics packages for FPC3.0.4 and rtl-generics-dummy for trunk compiler

---
+++ cad_source/other/rtl-generics_dummy/rtl_generics.pas
@@ -0,0 +1,12 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit rtl_generics;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+implementation
+
+end.

8116a8240a4f5dba3f79f9ec5c6762c98a61d79f
zamtmn <zamtmn@yandex.ru>
2019-01-11 03:31:31 +0500

    Continue stringstree entity properties

--- cad_source/zcad/electrotech/gui/forms/uzcefstringstreeselector.pas
+++ cad_source/zcad/electrotech/gui/forms/uzcefstringstreeselector.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ButtonPanel,
-  VirtualTrees, uzceltechtreeprop, uzbtypes;
+  VirtualTrees, uzceltechtreeprop, uzbtypes, math;
 
 type
 
@@ -23,11 +23,16 @@ type
     StringsTree: TVirtualStringTree;
     procedure filltree(StringTreeNode:PVirtualNode;BlobTreeNode:TBlobTree.TTreeNodeType);
     procedure fill(BlobTree:TBlobTree);
+    function FindNearestNode(value:TStringTreeType):PVirtualNode;
+    procedure FindNode(CurrentNode:PVirtualNode;value:TStringTreeType;var MatchSize:integer; var NearestNode:PVirtualNode);
     procedure clear;
     procedure setValue(value:TStringTreeType);
+    procedure VTFocuschanged(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex);
+    procedure EdChanged(Sender: TObject);
   private
 
   public
+    TreeResult:TStringTreeType;
     RootNode:PVirtualNode;
     procedure gt(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex;
                  TextType: TVSTTextType; var CellText: String);
@@ -55,12 +60,28 @@ begin
   end;
 end;
 
+procedure TStringsTreeSelector.VTFocuschanged(Sender: TBaseVirtualTree; Node: PVirtualNode; Column: TColumnIndex);
+var
+  PNodeData:PTStringsTreeNodeData;
+begin
+  PNodeData:=StringsTree.GetNodeData(Node);
+  TreeResult:=PNodeData^.data.Data.FullName;
+  ComboBox1.Text:=TreeResult;
+end;
+
+procedure TStringsTreeSelector.EdChanged(Sender: TObject);
+begin
+  TreeResult:=ComboBox1.Text;
+end;
+
 procedure TStringsTreeSelector.fill(BlobTree:TBlobTree);
 begin
   StringsTree.NodeDataSize:=sizeof(TStringsTreeNodeData);
   RootNode:=StringsTree.AddChild(nil,nil);
   filltree(RootNode,BlobTree.Root);
   StringsTree.OnGetText:=gt;
+  StringsTree.OnFocusChanged:=VTFocuschanged;
+  ComboBox1.OnChange:=EdChanged;
 end;
 
 procedure TStringsTreeSelector.clear;
@@ -69,9 +90,65 @@ begin
   StringsTree.Clear;
 end;
 
+function MyCompareStr(s1,s2:TStringTreeType):integer;
+var
+  i:integer;
+begin
+  result:=0;
+  for i:=1 to min(length(s1),length(s2)) do
+    if s1[i]=s2[i] then
+      inc(result)
+    else
+      break;
+end;
+
+procedure TStringsTreeSelector.FindNode(CurrentNode:PVirtualNode;value:TStringTreeType;var MatchSize:integer; var NearestNode:PVirtualNode);
+var
+  PNodeData:PTStringsTreeNodeData;
+  _MatchSize:integer;
+  v:TStringTreeType;
+  NewNode:PVirtualNode;
+begin
+  repeat
+    PNodeData:=StringsTree.GetNodeData(CurrentNode);
+    if PNodeData<>nil then begin
+      v:=PNodeData^.data.Data.FullName;
+      _MatchSize:=MyCompareStr(PNodeData^.data.Data.FullName,value);
+      if _MatchSize>MatchSize then begin
+        MatchSize:=_MatchSize;
+        NearestNode:=CurrentNode;
+      end;
+    end;
+    if CurrentNode.FirstChild<>nil then
+      FindNode(CurrentNode.FirstChild,value,MatchSize,NearestNode);
+    NewNode:=CurrentNode.NextSibling;
+    if NewNode<>CurrentNode then
+      CurrentNode:=NewNode
+    else
+      CurrentNode:=nil;
+  until CurrentNode=nil;
+end;
+
+
+function TStringsTreeSelector.FindNearestNode(value:TStringTreeType):PVirtualNode;
+var
+ _MatchSize:integer;
+begin
+  _MatchSize:=0;
+  result:=nil;
+  FindNode(StringsTree.RootNode,value,_MatchSize,result);
+end;
+
 procedure TStringsTreeSelector.setValue(value:TStringTreeType);
+var
+  NearestNode:PVirtualNode;
 begin
   ComboBox1.Text:=value;
+  NearestNode:=FindNearestNode(value);
+  if assigned(NearestNode) then begin
+    StringsTree.Selected[NearestNode]:=true;
+    StringsTree.FullyVisible[NearestNode]:=true;
+  end;
 end;
 
 

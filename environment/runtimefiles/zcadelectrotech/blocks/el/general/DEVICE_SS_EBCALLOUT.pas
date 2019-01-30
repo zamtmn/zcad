ab252e9921182a4018a58863cdb0bc644f27c99c
veb86 <veb86@mail.ru>
2019-01-30 15:22:03 +0300

    Add UGO device hostcall system

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_SS_EBCALLOUT.pas
@@ -0,0 +1,10 @@
+unit DEVICE_SS_BIAS;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='0BC0.0';
+   NMO_BaseName:='BC';
+   DB_link:='BC';
+   BTY_TreeCoord:='PLAN_SS_HOSTCALL_Гнездо для экстренной внешней кнопки вызова';
+end.
---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_SS_HOSTCALL_PLK.pas
@@ -0,0 +1,10 @@
+unit DEVICE_SS_BIAS;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='0PLC0.0';
+   NMO_BaseName:='PLC';
+   DB_link:='PLC';
+   BTY_TreeCoord:='PLAN_SS_HOSTCALL_Палатный контроллер';
+end.

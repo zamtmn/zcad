6a27494a0cd2b878d561c720d13a01d3cd067714
veb86 <veb86@mail.ru>
2019-01-30 15:22:03 +0300

    Add UGO device hostcall system

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_SS_HOSTCALL_KL.pas
@@ -0,0 +1,10 @@
+unit DEVICE_SS_BIAS;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='0BIAL0.0';
+   NMO_BaseName:='BIAL';
+   DB_link:='BIAL';
+   BTY_TreeCoord:='PLAN_SS_HOSTCALL_Коридорная лампа';
+end.

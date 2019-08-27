620881ee7c7b91dc6bf8d20ac583a0aaaadd2d1c
veb86 <veb86@mail.ru>
2019-08-27 17:36:16 +0300

    continued creation of the Veles method of the builder of structural single-line schemes. Not finished.

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_PS_MRO_2M.pas
@@ -0,0 +1,10 @@
+unit DEVICE_PS_MRO_2M;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='0-МРО';
+   NMO_BaseName:='МРО';
+   DB_link:='Модуль речевого оповещения';
+   BTY_TreeCoord:='PLAN_OPS_MRO-2M';
+end.

26d4c7c2fd7c7ff0e235da2e833cec620446aafc
veb86 <veb86@mail.ru>
2019-08-27 17:36:16 +0300

    continued creation of the Veles method of the builder of structural single-line schemes. Not finished.

---
+++ environment/runtimefiles/zcadelectrotech/programdb/hostcall/hostcall-NM-NP.pas
@@ -0,0 +1,22 @@
+subunit devicebase;
+interface
+uses system;
+var
+   _EQ_K_01C:DbBaseObject;
+implementation
+begin
+     _EQ_K_01C.initnul;
+     _EQ_K_01C.Category:=_ppkop;
+     _EQ_K_01C.Group:=_ppkop;
+     _EQ_K_01C.EdIzm:=_sht;
+     _EQ_K_01C.ID:='HostCall K-01C';
+     _EQ_K_01C.Standard:='';
+     _EQ_K_01C.OKP:='';
+     _EQ_K_01C.Manufacturer:='ООО "СКБ ТЕЛСИ"';
+     _EQ_K_01C.NameShort:='K-01C';
+     _EQ_K_01C.Name:='Кнопка вызова "K-01C"';
+     _EQ_K_01C.NameFull:='Кнопка вызова "K-01C"';
+     _EQ_K_01C.Description:='Стационарные кнопки вызова "K-01C"';
+     _EQ_K_01C.TreeCoord:='BP_HOSTCALL_K-01C|BC_Оборудование CС_HOSTCALL_K-01C';
+
+end.

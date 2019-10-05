856fce225f08477f66e9e0dbb6323ad1305c834a
zamtmn <zamtmn@yandex.ru>
2019-10-05 22:16:21 +0500

    Continue menus manager

--- cad_source/components/ztoolbars/uzmenusmanager.pas
+++ cad_source/components/ztoolbars/uzmenusmanager.pas
@@ -5,7 +5,7 @@ unit uzmenusmanager;
 interface
 
 uses
-  ugcontextchecker,uztoolbarsmanager,
+  ugcontextchecker,uztoolbarsmanager,uzmenusdefaults,
   ActnList,Laz2_XMLCfg,Laz2_DOM,Menus,Forms,
   sysutils,Generics.Collections;
 
@@ -23,12 +23,6 @@ type
     class function DefaultContexCheckState:TContextStateType;
   end;
   generic TCMContextChecker<T>=class (specialize TGCContextChecker<T,TMenuContextNameType,TContextStateType,TCMenuContextNameManipulator>)
-  end;
-  TMenuDefaults=class
-    class procedure CreateDefaultMenu(fmf:TForm;aName: string;aNode: TDomNode;actlist:TActionList;RootMenuItem:TMenuItem);
-    class procedure CreateDefaultMenuSeparator(fmf:TForm;aName: string;aNode: TDomNode;actlist:TActionList;RootMenuItem:TMenuItem);
-    class procedure DefaultSetMenu(fmf:TForm;aName: string;aNode: TDomNode;actlist:TActionList;RootMenuItem:TMenuItem);
-    //class procedure TMenuDefaults
   end;
 
   TMenusManager=class
@@ -155,55 +149,6 @@ begin
         RootMenuItem.Add(CreatedMenuItem)
       else
         TPopUpMenu(RootMenuItem).Items.Add(CreatedMenuItem);
-    end;
-end;
-
-class procedure TMenuDefaults.CreateDefaultMenu(fmf:TForm;aName: string;aNode: TDomNode;actlist:TActionList;RootMenuItem:TMenuItem);
-var
-  ppopupmenu:TMenuItem;
-  ts:String;
-  createdmenu:TMenu;
-  TBSubNode:TDomNode;
-begin
-  createdmenu:=TMainMenu.Create(application);
-  createdmenu.Images:=actlist.Images;
-  createdmenu.Name:=MenuNameModifier+uppercase(getAttrValue(aNode,'Name',''));
-
-  if assigned(aNode) then
-    TBSubNode:=aNode.FirstChild;
-  if assigned(TBSubNode) then
-    while assigned(TBSubNode)do
-    begin
-      ppopupmenu:=tmenuitem(application.FindComponent(MenuNameModifier+uppercase(TBSubNode.NodeName)));
-
-      if ppopupmenu<>nil then
-                                begin
-                                     createdmenu.items.Add(ppopupmenu);
-                                end;
-                            {else
-                                ZCMsgCallBackInterface.TextMessage(format(rsMenuNotFounf,[ts]),TMWOShowError);}
-
-      TBSubNode:=TBSubNode.NextSibling;
-    end;
-end;
-
-class procedure TMenuDefaults.DefaultSetMenu(fmf:TForm;aName: string;aNode: TDomNode;actlist:TActionList;RootMenuItem:TMenuItem);
-begin
-  fmf.Menu:=TMainMenu(application.FindComponent(MenuNameModifier+uppercase(getAttrValue(aNode,'Name',''))));
-end;
-
-
-class procedure TMenuDefaults.CreateDefaultMenuSeparator(fmf:TForm;aName: string;aNode: TDomNode;actlist:TActionList;RootMenuItem:TMenuItem);
-var
-  CreatedMenuItem:TMenuItem;
-begin
-  if RootMenuItem is TMenuItem then
-    RootMenuItem.AddSeparator
-  else
-    begin
-      CreatedMenuItem:=TMenuItem.Create(RootMenuItem);
-      CreatedMenuItem.Caption:='-';
-      TPopUpMenu(RootMenuItem).Items.Add(CreatedMenuItem);
     end;
 end;
 

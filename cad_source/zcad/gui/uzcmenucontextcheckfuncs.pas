a664012597b6d658b6c6b6f911fad14c288a2d02
zamtmn <zamtmn@yandex.ru>
2020-01-26 03:12:59 +0500

    Continue menus manager

--- cad_source/zcad/gui/uzcmenucontextcheckfuncs.pas
+++ cad_source/zcad/gui/uzcmenucontextcheckfuncs.pas
@@ -19,11 +19,40 @@
 unit uzcmenucontextcheckfuncs;
 {$INCLUDE def.inc}
 interface
+uses
+  LCLIntf,LCLType;
 function GMCCFTrue(const Context:TObject):boolean;
+function GMCCFFalse(const Context:TObject):boolean;
+function GMCCFDebugMode(const Context:TObject):boolean;
+
+function GMCCFCtrlPressed(const Context:TObject):boolean;
+function GMCCFShiftPressed(const Context:TObject):boolean;
+function GMCCFAltPressed(const Context:TObject):boolean;
 implementation
 function GMCCFTrue(const Context:TObject):boolean;
 begin
   result:=true;
 end;
+function GMCCFFalse(const Context:TObject):boolean;
+begin
+  result:=true;
+end;
+function GMCCFDebugMode(const Context:TObject):boolean;
+begin
+  {$IFDEF DEBUGBUILD}result:=true;{$ELSE}result:=false;{$ENDIF}
+end;
+function GMCCFCtrlPressed(const Context:TObject):boolean;
+begin
+  result:=(GetKeyState(VK_CONTROL) and $8000 <> 0);
+end;
+function GMCCFShiftPressed(const Context:TObject):boolean;
+begin
+  result:=(GetKeyState(VK_SHIFT) and $8000 <> 0);
+end;
+function GMCCFAltPressed(const Context:TObject):boolean;
+begin
+  result:=(GetKeyState(VK_MENU) and $8000 <> 0);
+end;
+
 begin
 end.

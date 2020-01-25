0f58b30fe68a6409f8ce718c15021bfcaa1ab2be
zamtmn <zamtmn@yandex.ru>
2020-01-25 17:28:35 +0500

    Continue menus manager

---
+++ cad_source/zcad/gui/uzcmenucontextcheckfuncs.pas
@@ -0,0 +1,29 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit uzcmenucontextcheckfuncs;
+{$INCLUDE def.inc}
+interface
+function GMCCFTrue(const Context:TObject):boolean;
+implementation
+function GMCCFTrue(const Context:TObject):boolean;
+begin
+  result:=true;
+end;
+begin
+end.

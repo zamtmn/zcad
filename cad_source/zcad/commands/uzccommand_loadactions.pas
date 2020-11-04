0356c2ec1323a20aac0c38302eb8c75fec5600cf
zamtmn <zamtmn@yandex.ru>
2020-11-05 04:49:48 +0500

    Extract loadpalettes, loadtoolbars, loadactions commands

---
+++ cad_source/zcad/commands/uzccommand_loadactions.pas
@@ -0,0 +1,47 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit uzccommand_loadactions;
+{$INCLUDE def.inc}
+
+interface
+uses
+ LCLProc,ComCtrls,Controls,
+ uzctreenode,uzbpaths,uzccommandsabstract,uzccommandsimpl,uzbtypes,uztoolbarsmanager;
+
+implementation
+procedure FixButtonCaption(_tb:TToolBar;_control:tcontrol);
+begin
+  if _control is TToolButton then
+    if assigned((_control as TToolButton).action) then
+       if ((_control as TToolButton).action)is TmyAction then
+         (_control as TToolButton).Caption:=(((_control as TToolButton).action)as TmyAction).imgstr;
+end;
+
+function LoadActions_com(operands:TCommandOperands):TCommandResult;
+begin
+  ToolBarsManager.LoadActions(ExpandPath(operands));
+  ToolBarsManager.IterateToolBarsContent(FixButtonCaption);
+  result:=cmd_ok;
+end;
+
+initialization
+  CreateCommandFastObjectPlugin(@LoadActions_com,'LoadActions',0,0);
+finalization
+  debugln('{I}[UnitsFinalization] Unit "',{$INCLUDE %FILE%},'" finalization');
+end.
---
+++ cad_source/zcad/commands/uzccommand_loadtoolbars.pas
@@ -0,0 +1,38 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit uzccommand_loadtoolbars;
+{$INCLUDE def.inc}
+
+interface
+uses
+ LCLProc,
+ uzbpaths,uzccommandsabstract,uzccommandsimpl,uzbtypes,uztoolbarsmanager;
+
+implementation
+function LoadToolbars_com(operands:TCommandOperands):TCommandResult;
+begin
+  ToolBarsManager.LoadToolBarsContent(ExpandPath(operands));
+  result:=cmd_ok;
+end;
+
+initialization
+  CreateCommandFastObjectPlugin(@LoadToolbars_com,'LoadToolbars',0,0);
+finalization
+  debugln('{I}[UnitsFinalization] Unit "',{$INCLUDE %FILE%},'" finalization');
+end.

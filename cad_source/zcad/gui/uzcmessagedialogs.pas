4e46f0266fa7ac854562b75dc56eb08c87b76361
zamtmn <zamtmn@yandex.ru>
2020-11-09 05:46:07 +0500

    Implement suppress messages

--- cad_source/zcad/gui/uzcmessagedialogs.pas
+++ cad_source/zcad/gui/uzcmessagedialogs.pas
@@ -20,19 +20,26 @@ unit uzcmessagedialogs;
 {$INCLUDE def.inc}
 interface
 uses
-    LCLTaskDialog,SysUtils,Forms,{$IFNDEF DELPHI}LCLtype,{$ELSE}windows,{$ENDIF}
-    uzcinterface,uzclog;
+    Controls,LCLTaskDialog,SysUtils,Forms,{$IFNDEF DELPHI}LCLtype,{$ELSE}windows,{$ENDIF}
+    uzcinterface,uzclog,uzelongprocesssupport,Generics.Collections;
 
 resourcestring
   rsMsgWndTitle='ZCAD';
   rsMsgNoShow='Do no show this next time';
 
 type
+
   TMsgDialogResult=record
     ModalResult:integer;
     RadioRes: integer;
     SelectionRes: integer;
     VerifyChecked: Boolean;
+  end;
+
+  TMessagesDictionary=TDictionary<string,TMsgDialogResult>;
+
+  TLPSSupporthelper=class
+    class procedure EndLongProcessHandler(LPHandle:TLPSHandle;TotalLPTime:TDateTime);
   end;
 
 procedure FatalError(errstr:String);
@@ -41,6 +48,16 @@ function zcMsgDlgWarning(ErrStr:String;NeedNoAsk:boolean=false):TMsgDialogResult
 function zcMsgDlgInformation(ErrStr:String;NeedNoAsk:boolean=false):TMsgDialogResult;
 
 implementation
+var
+  SuppressedMessages:TMessagesDictionary=nil;
+
+class procedure TLPSSupporthelper.EndLongProcessHandler(LPHandle:TLPSHandle;TotalLPTime:TDateTime);
+begin
+  if lps.ActiveProcessCount=1 then
+    if assigned(SuppressedMessages)then
+      SuppressedMessages.clear;
+end;
+
 procedure FatalError(errstr:String);
 var s:String;
 begin
@@ -53,10 +70,54 @@ begin
 
      halt(0);
 end;
-function zcMsgDlg(MsgStr:String;aDialogIcon: TTaskDialogIcon;NeedNoAsk:boolean=false):TMsgDialogResult;
+function getMsgID(MsgID:String):String;
 var
-  Task: TTaskDialog;
+  quoted:boolean;
+  count,i,j:integer;
 begin
+  quoted:=false;
+  count:=0;
+  for i:=1 to length(MsgID) do begin
+    if MsgID[i]='"' then quoted:=not quoted;
+    if not quoted then inc(count);
+  end;
+  setlength(result,count);
+  quoted:=false;
+  j:=1;
+  for i:=1 to length(MsgID) do begin
+    if MsgID[i]='"' then quoted:=not quoted;
+    if not quoted then begin
+      result[j]:=MsgID[i];
+      inc(j);
+    end;
+  end;
+end;
+function zcMsgDlg(MsgStr:String;aDialogIcon: TTaskDialogIcon;NeedNoAsk:boolean=false):TMsgDialogResult;
+  function isMsgSupressed(MsgID:String;var PrevResult:TMsgDialogResult):boolean;
+  begin
+    if assigned(SuppressedMessages) then
+      if SuppressedMessages.TryGetValue(MsgID,PrevResult) then
+        exit(true);
+    result:=false;
+  end;
+  function isSupressedMsgPresent:boolean;
+  begin
+    if assigned(SuppressedMessages) then
+      if SuppressedMessages.count>0 then
+        exit(true);
+    result:=false;
+  end;
+var
+  Task:TTaskDialog;
+  MsgID:String;
+begin
+  if isSupressedMsgPresent then begin
+    MsgID:=getMsgID(MsgStr);
+    if isMsgSupressed(MsgID,Result) then begin
+      exit;
+    end;
+  end else
+    MsgID:='';
   ZCMsgCallBackInterface.Do_BeforeShowModal(nil);
 
   Task.Title:=rsMsgWndTitle;
@@ -74,6 +135,14 @@ begin
   Result.VerifyChecked:=Task.VerifyChecked;
 
   ZCMsgCallBackInterface.Do_AfterShowModal(nil);
+
+  if Task.VerifyChecked then begin
+    if MsgID='' then
+      MsgID:=getMsgID(MsgStr);
+    if not assigned(SuppressedMessages) then
+      SuppressedMessages:=TMessagesDictionary.create;
+    SuppressedMessages.add(MsgID,Result);
+  end;
 end;
 function zcMsgDlgError(ErrStr:String;NeedNoAsk:boolean=false):TMsgDialogResult;
 var
@@ -93,5 +162,9 @@ var
 begin
   Result:=zcMsgDlg(ErrStr,tiInformation,NeedNoAsk);
 end;
-begin
+initialization
+  lps.AddOnLPEndHandler(TLPSSupporthelper.EndLongProcessHandler);
+finalization
+  if assigned(SuppressedMessages)then
+    freeandnil(SuppressedMessages);
 end.

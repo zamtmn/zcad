a3578603eb9c32a9c0a2ebc891abddde03343ae0
zamtmn <zamtmn@yandex.ru>
2020-11-18 16:44:26 +0500

    Separate SelObjChangeLayerToCurrent, SelObjChangeLWToCurrent, SelObjChangeColorToCurrent, SelObjChangeLTypeToCurrent, SelObjChangeTStyleToCurrent, SelObjChangeDimStyleToCurrent commands

---
+++ cad_source/zcad/commands/uzccommand_selobjchangeltypetocurrent.pas
@@ -0,0 +1,77 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
+}
+{$mode delphi}
+unit uzccommand_selobjchangeltypetocurrent;
+
+{$INCLUDE def.inc}
+
+interface
+uses
+  uzccommandsabstract,uzccommandsimpl,
+  uzeentity,
+  uzestyleslinetypes,
+  UGDBSelectedObjArray,
+  gzctnrvectortypes,
+  uzgldrawcontext,
+  uzcsysvars,
+  uzcdrawings,
+  uzcutils;
+
+implementation
+
+function SelObjChangeLTypeToCurrent_com(operands:TCommandOperands):TCommandResult;
+var pv:pGDBObjEntity;
+    psv:PSelectedObjDesc;
+    plt:PGDBLtypeProp;
+    ir:itrec;
+    DC:TDrawContext;
+begin
+  if (drawings.GetCurrentROOT.ObjArray.count = 0)or(drawings.GetCurrentDWG.wa.param.seldesc.Selectedobjcount=0) then exit;
+  plt:={drawings.GetCurrentDWG.LTypeStyleTable.getDataMutable}(SysVar.dwg.DWG_CLType^);
+  if plt=nil then
+                 exit;
+  dc:=drawings.GetCurrentDWG^.CreateDrawingRC;
+  pv:=drawings.GetCurrentROOT.ObjArray.beginiterate(ir);
+  if pv<>nil then
+  repeat
+    if pv^.Selected then
+                        begin
+                             pv^.vp.LineType:=plt;
+                             pv^.Formatentity(drawings.GetCurrentDWG^,dc);
+                        end;
+  pv:=drawings.GetCurrentROOT.ObjArray.iterate(ir);
+  until pv=nil;
+  psv:=drawings.GetCurrentDWG.SelObjArray.beginiterate(ir);
+  if psv<>nil then
+  begin
+       repeat
+             if psv.objaddr^.Selected then
+                                          begin
+                                               psv.objaddr^.vp.LineType:=plt;
+                                               psv.objaddr^.Formatentity(drawings.GetCurrentDWG^,dc);
+                                          end;
+       psv:=drawings.GetCurrentDWG.SelObjArray.iterate(ir);
+       until psv=nil;
+  end;
+  zcRedrawCurrentDrawing;
+  result:=cmd_ok;
+end;
+
+initialization
+  CreateCommandFastObjectPlugin(@SelObjChangeLTypeToCurrent_com,'SelObjChangeLTypeToCurrent',CADWG,0);
+end.
---
+++ cad_source/zcad/commands/uzccommand_selobjchangelwtocurrent.pas
@@ -0,0 +1,51 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
+}
+{$mode delphi}
+unit uzccommand_selobjchangelwtocurrent;
+
+{$INCLUDE def.inc}
+
+interface
+uses
+  uzccommandsabstract,uzccommandsimpl,
+  uzeentity,
+  gzctnrvectortypes,
+  uzcdrawings,
+  uzcutils,
+  uzcsysvars;
+
+implementation
+
+function SelObjChangeLWToCurrent_com(operands:TCommandOperands):TCommandResult;
+var pv:pGDBObjEntity;
+    ir:itrec;
+begin
+  if (drawings.GetCurrentROOT.ObjArray.count = 0)or(drawings.GetCurrentDWG.wa.param.seldesc.Selectedobjcount=0) then exit;
+  pv:=drawings.GetCurrentROOT.ObjArray.beginiterate(ir);
+  if pv<>nil then
+  repeat
+    if pv^.Selected then pv^.vp.LineWeight:=sysvar.dwg.DWG_CLinew^ ;
+  pv:=drawings.GetCurrentROOT.ObjArray.iterate(ir);
+  until pv=nil;
+  zcRedrawCurrentDrawing;
+  result:=cmd_ok;
+end;
+
+initialization
+  CreateCommandFastObjectPlugin(@SelObjChangeLWToCurrent_com,'SelObjChangeLWToCurrent',CADWG,0);
+end.

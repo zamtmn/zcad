28bd2378152e2e173fad65cd665e01d3b6c66855
zamtmn <zamtmn@yandex.ru>
2020-11-18 17:16:14 +0500

    Separate selectobjectbyaddres, selectonmouseobjects commands

---
+++ cad_source/zcad/commands/uzccommand_selectobjectbyaddres.pas
@@ -0,0 +1,48 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
+}
+{$mode delphi}
+unit uzccommand_selectobjectbyaddres;
+
+{$INCLUDE def.inc}
+
+interface
+uses
+  uzccommandsabstract,uzccommandsimpl,
+  uzeentity,
+  uzcutils,
+  uzcinterface,
+  uzcdrawings;
+
+implementation
+
+function SelectObjectByAddres_com(operands:TCommandOperands):TCommandResult;
+var
+  pp:PGDBObjEntity;
+  code:integer;
+begin
+  val(Operands,PtrUInt(pp),code);
+  if (code=0)and(assigned(pp))then
+    zcSelectEntity(pp);
+  ZCMsgCallBackInterface.Do_GUIaction(nil,ZMsgID_GUIActionRedraw);
+  ZCMsgCallBackInterface.Do_GUIaction(drawings.CurrentDWG.wa,ZMsgID_GUIActionSelectionChanged);
+  result:=cmd_ok;
+end;
+
+initialization
+  CreateCommandFastObjectPlugin(@SelectObjectByAddres_com,'SelectObjectByAddres',CADWG,0);
+end.

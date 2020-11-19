fa42d1453e8f6103b415ea120b08a49976810679
zamtmn <zamtmn@yandex.ru>
2020-11-19 07:06:25 +0500

    Rename uzccomdbgappexplorer.pas to uzccommand_dbgappexplorer.pas

---
+++ cad_source/zcad/commands/uzccommand_dbgappexplorer.pas
@@ -0,0 +1,34 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+}
+
+unit uzccommand_dbgappexplorer;
+{$INCLUDE def.inc}
+
+interface
+uses
+ uzccommandsimpl,uzccommandsabstract,uzbtypes,AppExploreFrm;
+implementation
+function DbgAppExplorer_com(operands:TCommandOperands):TCommandResult;
+begin
+  ShowAppExplorer;
+  result:=cmd_ok;
+end;
+
+begin
+CreateCommandFastObjectPlugin(@DbgAppExplorer_com,'DbgAppExplorer',0,0);
+end.

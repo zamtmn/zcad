e706686496ab731e3e89b9d1a8c6965ab6f6babf
zamtmn <zamtmn@yandex.ru>
2020-11-20 17:27:37 +0500

    Separate quit, units, layer commands

---
+++ cad_source/zcad/commands/uzccommand_layer.pas
@@ -0,0 +1,51 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
+}
+{$mode delphi}
+unit uzccommand_layer;
+
+{$INCLUDE def.inc}
+
+interface
+uses
+  SysUtils,
+  LazLogger,
+  uzcflayers,
+  uzctreenode,
+  uzcsysvars,
+  uzcinterface,
+  Varman,
+  //uzcdialogstypes,
+  uzccommandsabstract,uzccommandsimpl;
+
+implementation
+
+function layer_cmd(operands:TCommandOperands):TCommandResult;
+begin
+  LayersForm:=TLayersForm.Create(nil);
+  SetHeightControl(LayersForm,sysvar.INTF.INTF_DefaultControlHeight^);
+  ZCMsgCallBackInterface.DOShowModal(LayersForm);
+  Freeandnil(LayersForm);
+  result:=cmd_ok;
+end;
+
+initialization
+  debugln('{I}[UnitsInitialization] Unit "',{$INCLUDE %FILE%},'" initialization');
+  CreateCommandFastObjectPlugin(@layer_cmd,'Layer',CADWG,0);
+finalization
+  debugln('{I}[UnitsFinalization] Unit "',{$INCLUDE %FILE%},'" finalization');
+end.

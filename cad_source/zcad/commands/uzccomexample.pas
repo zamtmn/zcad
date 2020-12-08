d28a44a7dbc1a798683d709b466e675c2c590f13
zamtmn <zamtmn@yandex.ru>
2020-12-09 02:25:43 +0500

    Separate MatchProp command

--- cad_source/zcad/commands/uzccomexample.pas
+++ cad_source/zcad/commands/uzccomexample.pas
@@ -109,28 +109,6 @@ uses
 resourcestring
   rscmSelectEntityWithMainFunction='Select entity with main function';
   rscmSelectLinkedEntity='Select linked entity';
-type
-{EXPORT+}
-    //** Тип данных для отображения в инспекторе опций команды MatchProp о текстовых примитивах, составная часть TMatchPropParam
-    TMatchPropTextParam=packed record
-                       ProcessTextStyle:GDBBoolean;(*'Process style'*)
-                       ProcessTextSize:GDBBoolean;(*'Process size'*)
-                       ProcessTextOblique:GDBBoolean;(*'Process oblique'*)
-                       ProcessTextWFactor:GDBBoolean;(*'Process wfactor'*)
-                       ProcessTextJustify:GDBBoolean;(*'Process justify'*)
-                 end;
-    //** Тип данных для отображения в инспекторе опций команды MatchProp
-    TMatchPropParam=packed record
-                       ProcessLayer:GDBBoolean;(*'Process layer'*)
-                       ProcessLineWeight:GDBBoolean;(*'Process line weight'*)
-                       ProcessLineType:GDBBoolean;(*'Process line type'*)
-                       ProcessLineTypeScale:GDBBoolean;(*'Process line type scale'*)
-                       ProcessColor:GDBBoolean;(*'Process color'*)
-                       TextParams:TMatchPropTextParam;(*'Text params'*)
-                 end;
-{EXPORT-}
-var
-   MatchPropParam:TMatchPropParam; //**< Переменная содержащая опции команды MatchProp
 
 implementation
 //** блаблабла
@@ -455,163 +433,6 @@ begin
     result:=cmd_ok;
 end;
 
-function matchprop_com(operands:TCommandOperands):TCommandResult;
-var
-    ps,pd:PGDBObjEntity;
-    SourceObjType:TObjID;
-    isSourceObjText:boolean;
-    dc:TDrawContext;
-    UndoStartMarkerPlaced:boolean;
-    drawing:PTZCADDrawing;
-    EntChange:boolean;
-const
-    CommandName='MatchProp';
-function isTextEnt(ObjType:TObjID):boolean;
-begin
-     if (ObjType=GDBtextID)
-     or(ObjType=GDBMTextID)then
-                               result:=true
-                           else
-                               result:=false;
-end;
-
-begin
-    UndoStartMarkerPlaced:=false;
-    if commandmanager.getentity(rscmSelectSourceEntity,ps) then
-    begin
-         zcShowCommandParams(SysUnit^.TypeName2PTD('TMatchPropParam'),@MatchPropParam);
-         drawing:=PTZCADDrawing(drawings.GetCurrentDWG);
-         dc:=drawing^.CreateDrawingRC;
-         SourceObjType:=ps^.GetObjType;
-         isSourceObjText:=isTextEnt(SourceObjType);
-         while commandmanager.getentity(rscmSelectDestinationEntity,pd) do
-         begin
-              EntChange:=false;
-              if MatchPropParam.ProcessLayer then
-                if pd^.vp.Layer<>ps^.vp.Layer then
-                  begin
-                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.Layer)^ do
-                    begin
-                         pd^.vp.Layer:=ps^.vp.Layer;
-                         ComitFromObj;
-                    end;
-                    EntChange:=true;
-                  end;
-              if MatchPropParam.ProcessLineType then
-                if pd^.vp.LineType<>ps^.vp.LineType then
-                  begin
-                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.LineType)^ do
-                    begin
-                         pd^.vp.LineType:=ps^.vp.LineType;
-                         ComitFromObj;
-                    end;
-                    EntChange:=true;
-                  end;
-              if MatchPropParam.ProcessLineWeight then
-                if pd^.vp.LineWeight<>ps^.vp.LineWeight then
-                  begin
-                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.LineWeight)^ do
-                    begin
-                         pd^.vp.LineWeight:=ps^.vp.LineWeight;
-                         ComitFromObj;
-                    end;
-                    EntChange:=true;
-                  end;
-              if MatchPropParam.ProcessColor then
-                if pd^.vp.color<>ps^.vp.Color then
-                  begin
-                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.color)^ do
-                    begin
-                         pd^.vp.color:=ps^.vp.Color;
-                         ComitFromObj;
-                    end;
-                    EntChange:=true;
-                  end;
-              if MatchPropParam.ProcessLineTypeScale then
-                if pd^.vp.LineTypeScale<>ps^.vp.LineTypeScale then
-                  begin
-                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.LineTypeScale)^ do
-                    begin
-                         pd^.vp.LineTypeScale:=ps^.vp.LineTypeScale;
-                         ComitFromObj;
-                    end;
-                    EntChange:=true;
-                  end;
-              if (isSourceObjText)and(isTextEnt(pd^.GetObjType))then
-              begin
-                if MatchPropParam.TextParams.ProcessTextStyle then
-                  if PGDBObjText(pd)^.TXTStyleIndex<>PGDBObjText(ps)^.TXTStyleIndex then
-                    begin
-                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.TXTStyleIndex)^ do
-                      begin
-                           PGDBObjText(pd)^.TXTStyleIndex:=PGDBObjText(ps)^.TXTStyleIndex;
-                           ComitFromObj;
-                      end;
-                      EntChange:=true;
-                    end;
-                if MatchPropParam.TextParams.ProcessTextSize then
-                  if PGDBObjText(pd)^.textprop.size<>PGDBObjText(ps)^.textprop.size then
-                    begin
-                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.size)^ do
-                      begin
-                           PGDBObjText(pd)^.textprop.size:=PGDBObjText(ps)^.textprop.size;
-                           ComitFromObj;
-                      end;
-                      EntChange:=true;
-                    end;
-                if MatchPropParam.TextParams.ProcessTextOblique then
-                  if PGDBObjText(pd)^.textprop.Oblique<>PGDBObjText(ps)^.textprop.Oblique then
-                    begin
-                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.Oblique)^ do
-                      begin
-                           PGDBObjText(pd)^.textprop.Oblique:=PGDBObjText(ps)^.textprop.Oblique;
-                           ComitFromObj;
-                      end;
-                      EntChange:=true;
-                    end;
-                if MatchPropParam.TextParams.ProcessTextWFactor then
-                  if PGDBObjText(pd)^.textprop.wfactor<>PGDBObjText(ps)^.textprop.wfactor then
-                    begin
-                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.wfactor)^ do
-                      begin
-                           PGDBObjText(pd)^.textprop.wfactor:=PGDBObjText(ps)^.textprop.wfactor;
-                           ComitFromObj;
-                      end;
-                      EntChange:=true;
-                    end;
-                if MatchPropParam.TextParams.ProcessTextJustify then
-                  if PGDBObjText(pd)^.textprop.justify<>PGDBObjText(ps)^.textprop.justify then
-                    begin
-                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
-                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.justify)^ do
-                      begin
-                           PGDBObjText(pd)^.textprop.justify:=PGDBObjText(ps)^.textprop.justify;
-                           ComitFromObj;
-                      end;
-                      EntChange:=true;
-                    end;
-              end;
-              if EntChange then
-                begin
-                  pd^.FormatEntity(drawings.GetCurrentDWG^,dc);
-                  zcRedrawCurrentDrawing;
-                end;
-         end;
-         zcPlaceUndoEndMarkerIfNeed(UndoStartMarkerPlaced);
-         zcHideCommandParams;
-    end;
-    result:=cmd_ok;
-end;
-
 function InsertDevice_com(operands:TCommandOperands):TCommandResult;
 var
     pdev:PGDBObjDevice;
@@ -875,22 +696,9 @@ initialization
      CreateCommandFastObjectPlugin(@DrawDiametricDim_com,'DimDiameter',CADWG,0);
      CreateCommandFastObjectPlugin(@DrawRadialDim_com,   'DimRadius',  CADWG,0);
 
-     CreateCommandFastObjectPlugin(@matchprop_com,       'MatchProp',  CADWG,0);
-
      CreateCommandFastObjectPlugin(@InsertDevice_com,    'ID',   CADWG,0);
 
      CreateCommandFastObjectPlugin(@ExampleCreateLayer_com,'ExampleCreateLayer',   CADWG,0);
 
      CreateCommandFastObjectPlugin(@LinkDevices_com,'LD',   CADWG,0);
-
-     MatchPropParam.ProcessLayer:=true;
-     MatchPropParam.ProcessLineType:=true;
-     MatchPropParam.ProcessLineWeight:=true;
-     MatchPropParam.ProcessColor:=true;
-     MatchPropParam.ProcessLineTypeScale:=true;
-     MatchPropParam.TextParams.ProcessTextStyle:=true;
-     MatchPropParam.TextParams.ProcessTextSize:=true;
-     MatchPropParam.TextParams.ProcessTextOblique:=true;
-     MatchPropParam.TextParams.ProcessTextWFactor:=true;
-     MatchPropParam.TextParams.ProcessTextJustify:=true;
 end.
---
+++ cad_source/zcad/commands/uzccommand_matchprop.pas
@@ -0,0 +1,242 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.modifiedLGPL.txt, included in this distribution,    *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
+}
+{$mode delphi}
+unit uzccommand_matchprop;
+
+{$INCLUDE def.inc}
+
+interface
+uses
+  LazLogger,
+  SysUtils,
+  uzccommandsabstract,uzccommandsimpl,
+  uzccominteractivemanipulators,
+  uzcstrconsts,
+  uzbgeomtypes,
+  uzccommandsmanager,
+  uzeentlwpolyline,uzeentpolyline,uzeentityfactory,
+  uzcdrawings,
+  uzcutils,
+  uzbtypes,
+  uzegeometry,
+  uzeentity,uzeenttext,uzgldrawcontext,uzcdrawing,uzeconsts,zcchangeundocommand,
+  URecordDescriptor,typedescriptors,Varman;
+
+//type
+implementation
+
+type
+  //** Тип данных для отображения в инспекторе опций команды MatchProp о текстовых примитивах, составная часть TMatchPropParam
+  TMatchPropTextParam=packed record
+    ProcessTextStyle:Boolean;(*'Process style'*)
+    ProcessTextSize:Boolean;(*'Process size'*)
+    ProcessTextOblique:Boolean;(*'Process oblique'*)
+    ProcessTextWFactor:Boolean;(*'Process wfactor'*)
+    ProcessTextJustify:Boolean;(*'Process justify'*)
+  end;
+  //** Тип данных для отображения в инспекторе опций команды MatchProp
+  TMatchPropParam=packed record
+    ProcessLayer:Boolean;(*'Process layer'*)
+    ProcessLineWeight:Boolean;(*'Process line weight'*)
+    ProcessLineType:Boolean;(*'Process line type'*)
+    ProcessLineTypeScale:Boolean;(*'Process line type scale'*)
+    ProcessColor:Boolean;(*'Process color'*)
+    TextParams:TMatchPropTextParam;(*'Text params'*)
+  end;
+
+var
+   MatchPropParam:TMatchPropParam; //**< Переменная содержащая опции команды MatchProp
+
+function matchprop_com(operands:TCommandOperands):TCommandResult;
+var
+    ps,pd:PGDBObjEntity;
+    SourceObjType:TObjID;
+    isSourceObjText:boolean;
+    dc:TDrawContext;
+    UndoStartMarkerPlaced:boolean;
+    drawing:PTZCADDrawing;
+    EntChange:boolean;
+const
+    CommandName='MatchProp';
+function isTextEnt(ObjType:TObjID):boolean;
+begin
+     if (ObjType=GDBtextID)
+     or(ObjType=GDBMTextID)then
+                               result:=true
+                           else
+                               result:=false;
+end;
+
+begin
+    UndoStartMarkerPlaced:=false;
+    if commandmanager.getentity(rscmSelectSourceEntity,ps) then
+    begin
+         zcShowCommandParams(SysUnit^.TypeName2PTD('TMatchPropParam'),@MatchPropParam);
+         drawing:=PTZCADDrawing(drawings.GetCurrentDWG);
+         dc:=drawing^.CreateDrawingRC;
+         SourceObjType:=ps^.GetObjType;
+         isSourceObjText:=isTextEnt(SourceObjType);
+         while commandmanager.getentity(rscmSelectDestinationEntity,pd) do
+         begin
+              EntChange:=false;
+              if MatchPropParam.ProcessLayer then
+                if pd^.vp.Layer<>ps^.vp.Layer then
+                  begin
+                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.Layer)^ do
+                    begin
+                         pd^.vp.Layer:=ps^.vp.Layer;
+                         ComitFromObj;
+                    end;
+                    EntChange:=true;
+                  end;
+              if MatchPropParam.ProcessLineType then
+                if pd^.vp.LineType<>ps^.vp.LineType then
+                  begin
+                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.LineType)^ do
+                    begin
+                         pd^.vp.LineType:=ps^.vp.LineType;
+                         ComitFromObj;
+                    end;
+                    EntChange:=true;
+                  end;
+              if MatchPropParam.ProcessLineWeight then
+                if pd^.vp.LineWeight<>ps^.vp.LineWeight then
+                  begin
+                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.LineWeight)^ do
+                    begin
+                         pd^.vp.LineWeight:=ps^.vp.LineWeight;
+                         ComitFromObj;
+                    end;
+                    EntChange:=true;
+                  end;
+              if MatchPropParam.ProcessColor then
+                if pd^.vp.color<>ps^.vp.Color then
+                  begin
+                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.color)^ do
+                    begin
+                         pd^.vp.color:=ps^.vp.Color;
+                         ComitFromObj;
+                    end;
+                    EntChange:=true;
+                  end;
+              if MatchPropParam.ProcessLineTypeScale then
+                if pd^.vp.LineTypeScale<>ps^.vp.LineTypeScale then
+                  begin
+                    zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                    with PushCreateTGChangeCommand(drawing.UndoStack,pd^.vp.LineTypeScale)^ do
+                    begin
+                         pd^.vp.LineTypeScale:=ps^.vp.LineTypeScale;
+                         ComitFromObj;
+                    end;
+                    EntChange:=true;
+                  end;
+              if (isSourceObjText)and(isTextEnt(pd^.GetObjType))then
+              begin
+                if MatchPropParam.TextParams.ProcessTextStyle then
+                  if PGDBObjText(pd)^.TXTStyleIndex<>PGDBObjText(ps)^.TXTStyleIndex then
+                    begin
+                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.TXTStyleIndex)^ do
+                      begin
+                           PGDBObjText(pd)^.TXTStyleIndex:=PGDBObjText(ps)^.TXTStyleIndex;
+                           ComitFromObj;
+                      end;
+                      EntChange:=true;
+                    end;
+                if MatchPropParam.TextParams.ProcessTextSize then
+                  if PGDBObjText(pd)^.textprop.size<>PGDBObjText(ps)^.textprop.size then
+                    begin
+                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.size)^ do
+                      begin
+                           PGDBObjText(pd)^.textprop.size:=PGDBObjText(ps)^.textprop.size;
+                           ComitFromObj;
+                      end;
+                      EntChange:=true;
+                    end;
+                if MatchPropParam.TextParams.ProcessTextOblique then
+                  if PGDBObjText(pd)^.textprop.Oblique<>PGDBObjText(ps)^.textprop.Oblique then
+                    begin
+                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.Oblique)^ do
+                      begin
+                           PGDBObjText(pd)^.textprop.Oblique:=PGDBObjText(ps)^.textprop.Oblique;
+                           ComitFromObj;
+                      end;
+                      EntChange:=true;
+                    end;
+                if MatchPropParam.TextParams.ProcessTextWFactor then
+                  if PGDBObjText(pd)^.textprop.wfactor<>PGDBObjText(ps)^.textprop.wfactor then
+                    begin
+                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.wfactor)^ do
+                      begin
+                           PGDBObjText(pd)^.textprop.wfactor:=PGDBObjText(ps)^.textprop.wfactor;
+                           ComitFromObj;
+                      end;
+                      EntChange:=true;
+                    end;
+                if MatchPropParam.TextParams.ProcessTextJustify then
+                  if PGDBObjText(pd)^.textprop.justify<>PGDBObjText(ps)^.textprop.justify then
+                    begin
+                      zcPlaceUndoStartMarkerIfNeed(UndoStartMarkerPlaced,CommandName);
+                      with PushCreateTGChangeCommand(drawing.UndoStack,PGDBObjText(pd)^.textprop.justify)^ do
+                      begin
+                           PGDBObjText(pd)^.textprop.justify:=PGDBObjText(ps)^.textprop.justify;
+                           ComitFromObj;
+                      end;
+                      EntChange:=true;
+                    end;
+              end;
+              if EntChange then
+                begin
+                  pd^.FormatEntity(drawings.GetCurrentDWG^,dc);
+                  zcRedrawCurrentDrawing;
+                end;
+         end;
+         zcPlaceUndoEndMarkerIfNeed(UndoStartMarkerPlaced);
+         zcHideCommandParams;
+    end;
+    result:=cmd_ok;
+end;
+
+initialization
+  debugln('{I}[UnitsInitialization] Unit "',{$INCLUDE %FILE%},'" initialization');
+  MatchPropParam.ProcessLayer:=true;
+  MatchPropParam.ProcessLineType:=true;
+  MatchPropParam.ProcessLineWeight:=true;
+  MatchPropParam.ProcessColor:=true;
+  MatchPropParam.ProcessLineTypeScale:=true;
+  MatchPropParam.TextParams.ProcessTextStyle:=true;
+  MatchPropParam.TextParams.ProcessTextSize:=true;
+  MatchPropParam.TextParams.ProcessTextOblique:=true;
+  MatchPropParam.TextParams.ProcessTextWFactor:=true;
+  MatchPropParam.TextParams.ProcessTextJustify:=true;
+
+  SysUnit.RegisterType(TypeInfo(TMatchPropParam));//регистрируем тип данных в зкадном RTTI
+  SysUnit.SetTypeDesk(TypeInfo(TMatchPropParam),['Process layer','Process line weight','Process line type','Process line type scale','Process color','Text params'],[FNProgram]);//Даем програмные имена параметрам, по идее это должно быть в ртти, но ненашел
+  SysUnit.SetTypeDesk(TypeInfo(TMatchPropTextParam),['Process style','Process size','Process oblique','Process wfactor','Process justify'],[FNUser]);//Даем человечьи имена параметрам
+
+  CreateCommandFastObjectPlugin(@matchprop_com,'MatchProp',  CADWG,0);
+finalization
+  debugln('{I}[UnitsFinalization] Unit "',{$INCLUDE %FILE%},'" finalization');
+end.

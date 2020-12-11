74c559f74b7f8605aeb9e72f8d4391a607ba927a
zamtmn <zamtmn@yandex.ru>
2020-12-12 01:45:44 +0500

    Continue pkg installer

--- installpkgs.bat
+++ installpkgs.bat
@@ -1,4 +1,10 @@
+@echo off
 lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_attr.lpk
+if %errorlevel% neq 0 (
+   Echo Error when running lazbuild. Check lazbuild in %%PATCH%%
+   pause
+   exit
+)
 lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_graph.lpk
 lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_math.lpk
 lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk

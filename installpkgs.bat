7cd119da42c15bdc1ef3a274dfd8212fcf9bd2fc
zamtmn <zamtmn@yandex.ru>
2020-12-12 01:52:50 +0500

    Continue pkg installer

--- installpkgs.bat
+++ installpkgs.bat
@@ -10,4 +10,12 @@ lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_math.lpk
 lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
 lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
 lazbuild --add-package cad_source\components\zebase\zebase.lpk
- 
+lazbuild --add-package cad_source\components\zcontainers\zcontainers.lpk
+lazbuild --add-package cad_source\components\zcontrols\zcontrols.lpk
+lazbuild --add-package cad_source\components\zmacros\zmacros.lpk
+lazbuild --add-package cad_source\components\zmath\zmath.lpk
+lazbuild --add-package cad_source\components\zobjectinspector\zobjectinspector.lpk
+lazbuild --add-package cad_source\components\zscriptbase\zscriptbase.lpk
+lazbuild --add-package cad_source\components\zscript\zscript.lpk
+lazbuild --add-package cad_source\components\ztoolbars\ztoolbars.lpk
+lazbuild --add-package cad_source\components\zundostack\zundostack.lpk

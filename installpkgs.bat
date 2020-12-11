eb901b3bc50cd83d4bc89d08a5aaee6e9c1cf8a1
zamtmn <zamtmn@yandex.ru>
2020-12-12 01:39:01 +0500

    Start pkg installer

---
+++ installpkgs.bat
@@ -0,0 +1,7 @@
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_attr.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_graph.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_math.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
+lazbuild --add-package cad_source\components\zebase\zebase.lpk
+ 

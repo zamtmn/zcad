ff401d39328fc39a05b1967e91eaa218ac2f1110
zamtmn <zamtmn@yandex.ru>
2020-12-29 02:22:27 +0500

    Revert wrong changes

--- installpkgs.bat
+++ installpkgs.bat
@@ -1,24 +1,24 @@
 @echo off
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\AGraphLaz\lazarus\ag_attr.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_attr.lpk
 if %errorlevel% neq 0 (
    Echo Error when running lazbuild. Check lazbuild in %%PATCH%%
    pause
    exit
 )
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\AGraphLaz\lazarus\ag_graph.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\AGraphLaz\lazarus\ag_math.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\uniqueinstance\uniqueinstance_package.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\other\laz.virtualtreeview_package\laz.virtualtreeview_package.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zebase\zebase.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zcontainers\zcontainers.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zcontrols\zcontrols.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zmacros\zmacros.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zmath\zmath.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zobjectinspector\zobjectinspector.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zscriptbase\zscriptbase.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zscript\zscript.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\ztoolbars\ztoolbars.lpk
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg --add-package cad_source\components\zundostack\zundostack.lpk
-;E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg -B -r --build-ide=clean
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_graph.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_math.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
+lazbuild --add-package cad_source\other\AGraphLaz\lazarus\ag_vectors.lpk
+lazbuild --add-package cad_source\other\uniqueinstance\uniqueinstance_package.lpk
+lazbuild --add-package cad_source\other\laz.virtualtreeview_package\laz.virtualtreeview_package.lpk
+lazbuild --add-package cad_source\components\zebase\zebase.lpk
+lazbuild --add-package cad_source\components\zcontainers\zcontainers.lpk
+lazbuild --add-package cad_source\components\zcontrols\zcontrols.lpk
+lazbuild --add-package cad_source\components\zmacros\zmacros.lpk
+lazbuild --add-package cad_source\components\zmath\zmath.lpk
+lazbuild --add-package cad_source\components\zobjectinspector\zobjectinspector.lpk
+lazbuild --add-package cad_source\components\zscriptbase\zscriptbase.lpk
+lazbuild --add-package cad_source\components\zscript\zscript.lpk
+lazbuild --add-package cad_source\components\ztoolbars\ztoolbars.lpk
+lazbuild --add-package cad_source\components\zundostack\zundostack.lpk
+lazbuild -B -r --build-ide=clean
--- zcadelectrotech.bat
+++ zcadelectrotech.bat
@@ -1,5 +1,5 @@
 @echo off
-E:\lazarus2010\lazbuild --pcp=E:\lazarus2010\laz2010cfg cad_source\utils\typeexporter.lpi
+lazbuild cad_source\utils\typeexporter.lpi
 if %errorlevel% neq 0 (
    Echo Error when running lazbuild. Check lazbuild in %%PATCH%%
    pause

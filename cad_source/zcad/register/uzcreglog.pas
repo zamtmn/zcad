c5c05db34d2700c2ff97ce99e202cdb010d8dc54
zamtmn <zamtmn@yandex.ru>
2021-01-12 04:07:44 +0500

    Add LPSTIMING log module

--- cad_source/zcad/register/uzcreglog.pas
+++ cad_source/zcad/register/uzcreglog.pas
@@ -19,9 +19,29 @@
 unit uzcreglog;
 {$INCLUDE def.inc}
 interface
-uses uzclog,uzcinterface,uzcuidialogs,uzcuitypes,
+uses uzclog,uzcinterface,uzcuidialogs,uzcuitypes,uzelongprocesssupport,
      {$IFNDEF DELPHI}LCLtype,{$ELSE}windows,{$ENDIF}LCLProc,Forms;
 implementation
+
+type
+  TLogHelper=class
+    class procedure EndLongProcessHandler(LPHandle:TLPSHandle;TotalLPTime:TDateTime);
+  end;
+
+const
+  LPSTIMINGModuleName='LPSTIMING';
+
+var
+  LPSTIMINGModuleDeskIndex:TLogModuleDeskIndex;
+
+class procedure TLogHelper.EndLongProcessHandler(LPHandle:TLPSHandle;TotalLPTime:TDateTime);
+var
+   ts:string;
+begin
+  str((TotalLPTime*10e4):3:2,ts);
+  programlog.LogOutFormatStr('LongProcess "%s" finished: %s second',[lps.getLPName(LPHandle),ts],lp_OldPos,LM_Necessarily,LPSTIMINGModuleDeskIndex)
+end;
+
 
 procedure ShowMessageForLog(errstr:String);
 var
@@ -43,6 +63,10 @@ begin
 end;
 
 initialization
+
+  LPSTIMINGModuleDeskIndex:=programlog.registermodule(LPSTIMINGModuleName);
+
+  lps.AddOnLPEndHandler(TLogHelper.EndLongProcessHandler);
   uzclog.HistoryTextOut:=ZCMsgCallBackInterface.Do_HistoryOut();
   uzclog.MessageBoxTextOut:=@ShowMessageForLog;
   uzclog.WarningBoxTextOut:=@ShowWarningForLog;

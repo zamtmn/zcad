e0e9c335cc95fb9b596dec68ba69751ac7b67a98
vlsx <vl-sx@yandex.ru>
2021-06-10 19:14:08 +1000

    Добавлены:пожарные датчики с уменьшенным размером, опора освещения

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_PS_VL_DAT_HAND.pas
@@ -0,0 +1,10 @@
+unit DEVICE_PS_VL_DAT_HAND;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='0BTM0.0';
+   NMO_BaseName:='BTM';
+   DB_link:='HandDet';
+   BTY_TreeCoord:='PLAN_OPS_PSSENSORS_MSENSORS_Вариант 1';
+end.

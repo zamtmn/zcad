00eadc6b6d7f25180d173b1d6cb826718d629f59
vlsx <vl-sx@yandex.ru>
2021-07-04 15:01:22 +1000

    Добавлены элементы однолинейных схем

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_KM1.pas
@@ -0,0 +1,27 @@
+unit DEVICE_EL_VL_KM1;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:GDBString;(*'Тип'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Аппаратура';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='KM0';
+NMO_BaseName:='KM';
+NMO_Suffix:='??';
+
+T1:='??';
+
+end.
---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_SE1.pas
@@ -0,0 +1,37 @@
+unit DEVICE_EL_VL_SE1;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:GDBString;(*'Обозначение'*)
+T2:GDBString;(*'P активная'*)
+T3:GDBString;(*'Q реактивная'*)
+T4:GDBString;(*'I hасчетный'*)
+T5:GDBString;(*'Cos Ф'*)
+T6:GDBString;(*'Наименование'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Фидер';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='SE0';
+NMO_BaseName:='SE';
+NMO_Suffix:='??';
+
+T1:='??';
+T2:='??';
+T3:='??';
+T4:='??';
+T5:='??';
+T6:='??';
+
+end.

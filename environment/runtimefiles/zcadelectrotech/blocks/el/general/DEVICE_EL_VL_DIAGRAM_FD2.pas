632e545766a855bb44796481bc38ed85bb5d8dec
vlsx <vl-sx@yandex.ru>
2021-07-20 22:46:59 +1000

    Добавление элементов однолинейных схем

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_DIAGRAM_FD2.pas
@@ -0,0 +1,37 @@
+unit DEVICE_EL_VL_DIAGRAM_FD2;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:GDBString;(*'Обозначение'*)
+T2:GDBString;(*'P активная'*)
+T3:GDBString;(*'Q реактивная'*)
+T4:GDBString;(*'I hасчетный'*)
+T5:GDBString;(*'Cos Ф'*)
+T6:GDBString;(*'Наименование'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Фидер';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='F0';
+NMO_BaseName:='F';
+NMO_Suffix:='??';
+
+T1:='??';
+T2:='??';
+T3:='??';
+T4:='??';
+T5:='??';
+T6:='??';
+
+end.
---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_DIAGRAM_QF3.pas
@@ -0,0 +1,27 @@
+unit DEVICE_EL_VL_DIAGRAM_QF3;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:GDBString;(*'Тип'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Аппаратура';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='A0';
+NMO_BaseName:='A1.';
+NMO_Suffix:='??';
+
+T1:='??';
+
+end.

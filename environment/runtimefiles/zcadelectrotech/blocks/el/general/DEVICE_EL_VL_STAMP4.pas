766ce07b796c433014ce0390ca16fcec2989c274
vlsx <vl-sx@yandex.ru>
2021-07-30 12:08:53 +1000

    Добавлены ведомости узлов, рабочих чертежей, ссылочных документов

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_STAMP4.pas
@@ -0,0 +1,27 @@
+unit DEVICE_EL_VL_STAMP4;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T16:GDBString;(*'Гип'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Штамп';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='ШТ0';
+NMO_BaseName:='ШТ';
+NMO_Suffix:='??';
+
+T16:='??';
+
+end.

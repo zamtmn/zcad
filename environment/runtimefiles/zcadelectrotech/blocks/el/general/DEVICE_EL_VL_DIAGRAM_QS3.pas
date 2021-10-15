d633ff7d7fc1d2a46a0ff1a9e8948f17b4d9317c
vlsx <vl-sx@yandex.ru>
2021-10-15 14:58:36 +1000

    Добавлены элементы ВРУ по схеме крест

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_DIAGRAM_QS3.pas
@@ -0,0 +1,32 @@
+unit DEVICE_EL_VL_DIAGRAM_QS3;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:GDBString;(*'Обозначение'*)
+T2:GDBString;(*'Марка'*)
+T3:GDBString;(*'Параметры'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Аппаратура';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='А0';
+NMO_BaseName:='А';
+NMO_Suffix:='??';
+NMO_Affix:='.1';
+
+T1:='??';
+T2:='??';
+T3:='??';
+
+end.

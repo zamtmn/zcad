efb772ed039a43f166c84d3ba403f38de96c9649
vlsx <vl-sx@yandex.ru>
2022-07-06 09:19:38 +1000

    Элементы планов освещения

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_SWITH3_1P_3.pas
@@ -0,0 +1,36 @@
+unit DEVICE_EL_VL_SWITH3_1P_3;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname_eo;
+usescopy objgroup;
+usescopy addtocable;
+
+var
+
+VL_Type:String;(*'Выключатель'*)
+VL_Room:String;(*'Помещение'*)
+VL_Group:String;(*'Группа'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Выключатель';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='Гр0';
+NMO_BaseName:='Гр';
+NMO_Suffix:='??';
+
+SerialConnection:=1;
+GC_HeadDevice:='ЩО??';
+GC_HDShortName:='??';
+GC_HDGroup:=0;
+
+VL_Room:='.';
+
+end.

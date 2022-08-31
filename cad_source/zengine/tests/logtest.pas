75e76f427975968a8c91d32440afa97aca68e4ae
zamtmn <zamtmn@yandex.ru>
2022-09-01 01:54:37 +0500

    Fix log test

--- cad_source/zengine/tests/logtest.pas
+++ cad_source/zengine/tests/logtest.pas
@@ -19,23 +19,25 @@ implementation
 
 Procedure TLogTest.WithoutLog;
 var
-  i,j:Integer;
+  i,j,a:Integer;
 begin
-  for i:=0 to 10000 do
+  for i:=0 to 10000 do begin
     for j:=0 to 10000 do
-    ;
+      a:=j;
+  end;
 end;
 
 Procedure TLogTest.WithLog;
 var
-  i,j:Integer;
+  i,j,a:Integer;
   log:tlog;
 begin
   log.init();
-  for i:=0 to 10000 do
+  for i:=0 to 10000 do begin
     for j:=0 to 10000 do
-    log.LogOutStr('test')
-    ;
+      a:=j;
+    log.LogOutStr('test');
+  end;
   log.done;
 end;
 

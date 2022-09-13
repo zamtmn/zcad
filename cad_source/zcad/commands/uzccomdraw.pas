49111252e3498fae786b12320871361f2d6510a6
zamtmn <zamtmn@yandex.ru>
2022-09-13 13:43:45 +0500

    Separate CopyBase command

--- cad_source/zcad/commands/uzccomdraw.pas
+++ cad_source/zcad/commands/uzccomdraw.pas
@@ -128,11 +128,6 @@ type
   ptpcoavector=^tpcoavector;
   tpcoavector={-}specialize{//}
               GZVector{-}<TCopyObjectDesc>{//};
-  {REGISTEROBJECTTYPE copybase_com}
-  copybase_com =  object(CommandRTEdObject)
-    procedure CommandStart(Operands:TCommandOperands); virtual;
-    function BeforeClick(wc: GDBvertex; mc: GDBvertex2DI; var button: Byte;osp:pos_record): Integer; virtual;
-  end;
   {REGISTEROBJECTTYPE PasteClip_com}
   PasteClip_com =  object(FloatInsert_com)
     procedure Command(Operands:TCommandOperands); virtual;
@@ -225,7 +220,6 @@ var
    pold:PGDBObjEntity;
    p3dpl:pgdbobjpolyline;
    p3dplold:PGDBObjEntity;
-   copybase:copybase_com;
    PasteClip:PasteClip_com;
 
    InsertTestTable:ITT_com;
@@ -1216,96 +1210,6 @@ begin
   end else
     ZCMsgCallBackInterface.TextMessage(rsClipboardIsEmpty,TMWOHistoryOut);
 end;
-procedure copybase_com.CommandStart(Operands:TCommandOperands);
-var //i: Integer;
-  {tv,}pobj: pGDBObjEntity;
-      ir:itrec;
-      counter:integer;
-      //tcd:TCopyObjectDesc;
-begin
-  inherited;
-
-  counter:=0;
-
-  pobj:=drawings.GetCurrentROOT^.ObjArray.beginiterate(ir);
-  if pobj<>nil then
-  repeat
-    if pobj^.selected then
-    inc(counter);
-  pobj:=drawings.GetCurrentROOT^.ObjArray.iterate(ir);
-  until pobj=nil;
-
-
-  if counter>0 then
-  begin
-  drawings.GetCurrentDWG^.wa.SetMouseMode((MGet3DPoint) or (MMoveCamera) or (MRotateCamera));
-  ZCMsgCallBackInterface.TextMessage(rscmBasePoint,TMWOHistoryOut);
-  end
-  else
-  begin
-    ZCMsgCallBackInterface.TextMessage(rscmSelEntBeforeComm,TMWOHistoryOut);
-    Commandmanager.executecommandend;
-  end;
-end;
-function copybase_com.BeforeClick(wc: GDBvertex; mc: GDBvertex2DI; var button: Byte;osp:pos_record): Integer;
-var
-    dist:gdbvertex;
-    dispmatr:DMatrix4D;
-    ir:itrec;
-    //pcd:PTCopyObjectDesc;
-
-    //pbuf:pchar;
-    //hgBuffer:HGLOBAL;
-
-    //s,suni:String;
-    //I:Integer;
-      tv,pobj: pGDBObjEntity;
-      DC:TDrawContext;
-      NeedReCreateClipboardDWG:boolean;
-begin
-
-      //drawings.GetCurrentDWG^.ConstructObjRoot.ObjMatrix:=dispmatr;
-  NeedReCreateClipboardDWG:=true;
-  if (button and MZW_LBUTTON)<>0 then
-  begin
-      ClipboardDWG^.pObjRoot^.ObjArray.free;
-      dist.x := -wc.x;
-      dist.y := -wc.y;
-      dist.z := -wc.z;
-
-      dispmatr:=onematrix;
-      PGDBVertex(@dispmatr[3])^:=dist;
-
-   dc:=drawings.GetCurrentDWG^.CreateDrawingRC;
-   pobj:=drawings.GetCurrentROOT^.ObjArray.beginiterate(ir);
-   if pobj<>nil then
-   repeat
-          begin
-              if pobj^.selected then
-              begin
-                if NeedReCreateClipboardDWG then
-                                                 begin
-                                                      ReCreateClipboardDWG;
-                                                      NeedReCreateClipboardDWG:=false;
-                                                 end;
-                tv:=drawings.CopyEnt(drawings.GetCurrentDWG,ClipboardDWG,pobj);
-                if tv^.IsHaveLCS then
-                                    PGDBObjWithLocalCS(tv)^.CalcObjMatrix;
-                tv^.transform(dispmatr);
-                tv^.FormatEntity(ClipboardDWG^,dc);
-              end;
-          end;
-          pobj:=drawings.GetCurrentROOT^.ObjArray.iterate(ir);
-   until pobj=nil;
-
-   CopyToClipboard;
-
-   drawings.GetCurrentDWG^.ConstructObjRoot.ObjMatrix:=onematrix;
-   commandend;
-   commandmanager.executecommandend;
-  end;
-  result:=cmd_ok;
-end;
 function Insert_com_CommandStart(operands:TCommandOperands):Integer;
 var pb:PGDBObjBlockdef;
     //ir:itrec;
@@ -1881,7 +1785,6 @@ begin
 
   CreateCommandRTEdObjectPlugin(@Insert_com_CommandStart,@Insert_com_CommandEnd,nil,nil,@Insert_com_BeforeClick,@Insert_com_BeforeClick,nil,nil,'Insert',0,0);
 
-  copybase.init('CopyBase',CADWG or CASelEnts,0);
   PasteClip.init('PasteClip',0,0);
 
   BlockReplace.init('BlockReplace',0,0);

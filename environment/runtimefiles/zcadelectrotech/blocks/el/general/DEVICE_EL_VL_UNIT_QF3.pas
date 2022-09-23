f23eab827395a46feebfb1b790a2039b4dd480a5
vlsx <vl-sx@yandex.ru>
2022-09-23 23:05:53 +1000

    Добавлены элементы вторичных схем

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_UNIT_QF3.pas
@@ -0,0 +1,28 @@
+unit DEVICE_EL_VL_UNIT_QF3;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:String;(*'Обозначение'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Аппаратура';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='QF0';
+NMO_BaseName:='QF';
+NMO_Suffix:='??';
+NMO_Affix:='';
+
+T1:='';
+
+end.

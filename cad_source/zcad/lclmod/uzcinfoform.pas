6d956a12d6620758f625afdb8b39b9f50ae9b23e
zamtmn <zamtmn@yandex.ru>
2023-01-04 22:00:28 +0500

    Refactor

--- cad_source/zcad/lclmod/uzcinfoform.pas
+++ cad_source/zcad/lclmod/uzcinfoform.pas
@@ -21,65 +21,58 @@ unit uzcinfoform;
 interface
 
 uses
-  uzcsysparams,uzcinterface,ExtCtrls,
-  {$IFNDEF DELPHI}lclproc,{$ENDIF}
-  Graphics,ActnList,ComCtrls,StdCtrls,Controls,Classes,menus,Forms,{$IFDEF FPC}lcltype,fileutil,ButtonPanel,{$ENDIF}Buttons,
-  uzclog,{$IFNDEF DELPHI}uzctranslations,{$ENDIF}sysutils,uzbstrproc,varmandef,Varman,UBaseTypeDescriptor,uzcsysinfo,uzctnrVectorBytes;
+  uzcsysparams,uzcinterface,uzclog,
+  Forms,ButtonPanel,StdCtrls,ComCtrls,Controls;
 type
   TButtonMethod=procedure({Sender:pointer;}pdata:{Pointer}PtrInt)of object;
   TButtonProc=procedure(pdata:Pointer);
   TDialogForm=class(tform)
-                         {$IFNDEF DELPHI}
-                         DialogPanel: TButtonPanel;
-                         {$ENDIF}
-                         public
-                         procedure AfterConstruction;override;
-                    end;
+      DialogPanel: TButtonPanel;
+    public
+      procedure AfterConstruction;override;
+  end;
   TInfoForm=class(TDialogForm)
-                         Memo: TMemo;
-                         public
-                         procedure AfterConstruction; override;
-                    end;
+      Memo: TMemo;
+    public
+      procedure AfterConstruction; override;
+  end;
   TmyProcToolButton=class({Tmy}TToolButton)
-                public
-                FProc:TButtonProc;
-                FMethod:TButtonMethod;
-                PPata:Pointer;
-                procedure Click; override;
-                end;
+    public
+      FProc:TButtonProc;
+      FMethod:TButtonMethod;
+      PPata:Pointer;
+      procedure Click; override;
+  end;
 implementation
 procedure TDialogForm.AfterConstruction;
 begin
-     inherited;
-     self.Width:=sysparam.notsaved.screenx div 2;
-     self.Height:=sysparam.notsaved.screeny div 2;
-     self.Position:=poScreenCenter;
-     self.BorderStyle:=bsSizeToolWin;
-     {$IFNDEF DELPHI}
-     DialogPanel:=TButtonPanel.create(self);
-     DialogPanel.ShowButtons:=[pbOK, pbCancel];
-     DialogPanel.Align:=alBottom;
-     DialogPanel.Parent:=self;
-     {$ENDIF}
+  inherited;
+  self.Width:=sysparam.notsaved.screenx div 2;
+  self.Height:=sysparam.notsaved.screeny div 2;
+  self.Position:=poScreenCenter;
+  self.BorderStyle:=bsSizeToolWin;
+
+  DialogPanel:=TButtonPanel.create(self);
+  DialogPanel.ShowButtons:=[pbOK, pbCancel];
+  DialogPanel.Align:=alBottom;
+  DialogPanel.Parent:=self;
 end;
 procedure TInfoForm.AfterConstruction;
 begin
-     inherited;
-     self.Position:=poDesigned;
-     Memo:=TMemo.create(self);
-     {$IFNDEF DELPHI}Memo.ScrollBars:=ssAutoBoth;{$ENDIF}
-     Memo.Align:=alClient;
-     Memo.Parent:=self;
+  inherited;
+  self.Position:=poDesigned;
+  Memo:=TMemo.create(self);
+  Memo.ScrollBars:=ssAutoBoth;
+  Memo.Align:=alClient;
+  Memo.Parent:=self;
 end;
 procedure TmyProcToolButton.Click;
 begin
-     ZCMsgCallBackInterface.Do_GUIaction(self,ZMsgID_GUIStoreAndFreeEditorProc);
-     {$IFNDEF DELPHI}
-     if assigned(FProc) then
-                            FProc(PPata);
-     if assigned(FMethod) then
-                            Application.QueueAsyncCall(FMethod,PtrInt(PPata));
-     {$ENDIF}
+  ZCMsgCallBackInterface.Do_GUIaction(self,ZMsgID_GUIStoreAndFreeEditorProc);
+  if assigned(FProc) then
+    FProc(PPata);
+  if assigned(FMethod) then
+    Application.QueueAsyncCall(FMethod,PtrInt(PPata));
 end;
 initialization
 finalization

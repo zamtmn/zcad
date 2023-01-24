686fd17814d3ee2022260e2c18f8692a62823937
zamtmn <zamtmn@yandex.ru>
2023-01-24 14:36:03 +0500

    Improve ExampleConstructToModalSpace cmd

--- cad_source/zcad/commands/uzccommand_exampleconstructtomodalspace.pas
+++ cad_source/zcad/commands/uzccommand_exampleconstructtomodalspace.pas
@@ -33,63 +33,64 @@ uses
   uzeentabstracttext,
   uzeentpolyline,uzeconsts;
 
+resourcestring
+  RSHelloWorld='HELLO WORLD!';
+
+
 implementation
 
 function ExampleConstructToModalSpace_com(operands:TCommandOperands):TCommandResult;
 var
-    pe:T3PointPentity;
-const
-    createMText='HELLO WORLD!';
-
+  pp:PGDBObjPolyLine;
+  pt:PGDBObjMText;
+  v:GDBvertex;
 //Визуализация многострочный текст
-procedure drawMText(pt:GDBVertex;color:integer;rotate:double);
-var
-    pmtext:PGDBObjMText;
+function drawMText(pt:GDBVertex;color:integer;rotate:double):PGDBObjMText;
 begin
-    pmtext := GDBObjMText.CreateInstance;
-    zcSetEntPropFromCurrentDrawingProp(pmtext); //добавляем дефаултные свойства
-    pmtext^.TXTStyleIndex:=drawings.GetCurrentDWG^.GetCurrentTextStyle; //добавляет тип стиля текста, дефаултные свойства его не добавляют
-
-    pmtext^.Local.P_insert:=pt;                                         //координата вставки
-    pmtext^.textprop.justify:=jsmc;                                     //выравнивание текста
-    pmtext^.Template:=TDXFEntsInternalStringType(createMText);               //Текст незнаю чем они отличаются
-    pmtext^.Content:=TDXFEntsInternalStringType(createMText);                //Текст незнаю чем они отличаются
-    pmtext^.vp.LineWeight:=LnWt100;                                     //Толщина линии
-    pmtext^.linespacef:=1;                                              //Межстрочный интервал
-    rotate:=(rotate*pi)/180;                                            //получаем угол из градуса
-    pmtext^.Local.basis.ox.x:=cos(rotate);                                    //поворот по оси икс, почему их два незнаю что то там связоно с отрисовкой
-    pmtext^.Local.basis.ox.y:=sin(rotate);                                    //поворот по оси игрик, почему их два незнаю что то там связоно с отрисовкой
-    pmtext^.vp.Color:=color;                                            //Цвет текста
-    pmtext^.textprop.size:=2.5;                                         //Высота текста
-    //zcAddEntToCurrentDrawingConstructRoot(pmtext);                    //добавляем в конструкторскую область
-    zcAddEntToCurrentDrawingWithUndo(pmtext);                           //добавляем текст с ундо в пространство модели
+  Result:=GDBObjMText.CreateInstance;
+  zcSetEntPropFromCurrentDrawingProp(Result);                        //добавляем дефаултные свойства
+  Result^.TXTStyleIndex:=drawings.GetCurrentDWG^.GetCurrentTextStyle;//добавляет тип стиля текста, дефаултные свойства его не добавляют
+  Result^.Local.P_insert:=pt;                                        //координата вставки
+  Result^.textprop.justify:=jsmc;                                    //выравнивание текста
+  Result^.Template:=TDXFEntsInternalStringType(RSHelloWorld);         //Текст незнаю чем они отличаются
+  Result^.Content:=TDXFEntsInternalStringType(RSHelloWorld);          //Текст незнаю чем они отличаются
+  Result^.vp.LineWeight:=LnWt100;                                    //Толщина линии
+  Result^.linespacef:=1;                                             //Межстрочный интервал
+  rotate:=(rotate*pi)/180;                                           //получаем угол из градуса
+  Result^.Local.basis.ox.x:=cos(rotate);                             //поворот по оси икс, почему их два незнаю что то там связоно с отрисовкой
+  Result^.Local.basis.ox.y:=sin(rotate);                             //поворот по оси игрик, почему их два незнаю что то там связоно с отрисовкой
+  Result^.vp.Color:=color;                                           //Цвет текста
+  Result^.textprop.size:=2.5;                                        //Высота текста
+  zcAddEntToCurrentDrawingConstructRoot(Result);                     //добавляем в конструкторскую область
+  //zcAddEntToCurrentDrawingWithUndo(Result);                        //добавляем текст с ундо в пространство модели
 end;
 
-//Визуализация полилинии
-procedure drawPolyline(pt1,pt2:GDBVertex;color:integer);
-var
-    polyObj:PGDBObjPolyLine;
+function drawPolyline(pt1,pt2:GDBVertex;color:integer):PGDBObjPolyLine;
 begin
-     polyObj:=GDBObjPolyline.CreateInstance;
-     zcSetEntPropFromCurrentDrawingProp(polyObj);                         //добавляем дефаултные свойства
-     polyObj^.Closed:=true;                                               //полилиния замкнута
-     polyObj^.vp.Color:=color;                                            //Цвет линии
-     polyObj^.vp.LineWeight:=LnWt050;                                     //Толщина линии
-     polyObj^.VertexArrayInOCS.PushBackData(pt1);
-     polyObj^.VertexArrayInOCS.PushBackData(uzegeometry.CreateVertex(pt2.x,pt1.y,0));
-     polyObj^.VertexArrayInOCS.PushBackData(pt2);
-     polyObj^.VertexArrayInOCS.PushBackData(uzegeometry.CreateVertex(pt1.x,pt2.y,0));
-     //zcAddEntToCurrentDrawingConstructRoot(polyObj);                    //добавляем в конструкторскую область
-     zcAddEntToCurrentDrawingWithUndo(polyObj);                           //добавляем полилинию с ундо в пространство модели
+  Result:=GDBObjPolyline.CreateInstance;
+  zcSetEntPropFromCurrentDrawingProp(Result);                      //добавляем дефаултные свойства
+  Result^.Closed:=true;                                            //полилиния замкнута
+  Result^.vp.Color:=color;                                         //Цвет линии
+  Result^.vp.LineWeight:=LnWt050;                                  //Толщина линии
+  Result^.VertexArrayInOCS.PushBackData(pt1);
+  Result^.VertexArrayInOCS.PushBackData(uzegeometry.CreateVertex(pt2.x,pt1.y,0));
+  Result^.VertexArrayInOCS.PushBackData(pt2);
+  Result^.VertexArrayInOCS.PushBackData(uzegeometry.CreateVertex(pt1.x,pt2.y,0));
+  zcAddEntToCurrentDrawingConstructRoot(Result);                   //добавляем в конструкторскую область
+  //zcAddEntToCurrentDrawingWithUndo(Result);                      //добавляем полилинию с ундо в пространство модели
 end;
 
 begin
-    if commandmanager.get3dpoint(rscmSpecifyFirstPoint,pe.p1)=GRNormal then   //Получаем координату ЛКМ-клик
-    begin
-       drawMText(pe.p1,2,0);                                                  //Рисуем текст
-       drawPolyline(uzegeometry.CreateVertex(pe.p1.x-20,pe.p1.y+5,0),uzegeometry.CreateVertex(pe.p1.x+20,pe.p1.y-5,0),3)
-    end;
-    result:=cmd_ok;
+  pt:=drawMText(NulVertex,2,0);                                        //Рисуем текст
+  pp:=drawPolyline(uzegeometry.CreateVertex(-20,+5,0),uzegeometry.CreateVertex(+20,-5,0),3);
+  if commandmanager.Get3DPointInteractive(rscmSpecifyFirstPoint,v,@InteractiveConstructRootManipulator,nil)=GRNormal then begin   //Получаем координату ЛКМ-кли
+    zcClearCurrentDrawingConstructRoot;
+    zcStartUndoCommand('ExampleConstructToModalSpace');
+    zcAddEntToCurrentDrawingWithUndo(pt);
+    zcAddEntToCurrentDrawingWithUndo(pp);
+    zcEndUndoCommand;
+  end;
+  result:=cmd_ok;
 end;
 
 

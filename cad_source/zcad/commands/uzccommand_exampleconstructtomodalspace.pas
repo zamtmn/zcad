7ed0838084b9c747c21ff7ad561db2bd58fae508
zamtmn <zamtmn@yandex.ru>
2023-02-01 15:26:59 +0500

    Improve ExampleConstructToModalSpace cmd

--- cad_source/zcad/commands/uzccommand_exampleconstructtomodalspace.pas
+++ cad_source/zcad/commands/uzccommand_exampleconstructtomodalspace.pas
@@ -31,13 +31,21 @@ uses
   uzegeometry,
   uzcutils,
   uzeentabstracttext,
-  uzeentpolyline,uzeconsts;
+  uzeentpolyline,uzeconsts,
+  Varman;
 
 resourcestring
   RSHelloWorld='HELLO WORLD!';
 
 
 implementation
+type
+  TCmdProp=record
+   props:TObjectUnit;
+  end;
+
+var
+  CmdProp:TCmdProp;
 
 function ExampleConstructToModalSpace_com(operands:TCommandOperands):TCommandResult;
 //Визуализация многострочный текст
@@ -45,16 +53,16 @@ function drawMText(pt:GDBVertex;color:integer;rotate:double):PGDBObjMText;
 begin
   Result:=GDBObjMText.CreateInstance;
   zcSetEntPropFromCurrentDrawingProp(Result);                        //добавляем дефаултные свойства
-  Result^.TXTStyleIndex:=drawings.GetCurrentDWG^.GetCurrentTextStyle;//добавляет тип стиля текста, дефаултные свойства его не добавляют
+  //Result^.TXTStyleIndex:=drawings.GetCurrentDWG^.GetCurrentTextStyle;//добавляет тип стиля текста, дефаултные свойства его не добавляют
   Result^.Local.P_insert:=pt;                                        //координата вставки
   Result^.textprop.justify:=jsmc;                                    //выравнивание текста
   Result^.Template:=TDXFEntsInternalStringType(RSHelloWorld);         //Текст незнаю чем они отличаются
-  Result^.Content:=TDXFEntsInternalStringType(RSHelloWorld);          //Текст незнаю чем они отличаются
-  Result^.vp.LineWeight:=LnWt100;                                    //Толщина линии
-  Result^.linespacef:=1;                                             //Межстрочный интервал
-  rotate:=(rotate*pi)/180;                                           //получаем угол из градуса
-  Result^.Local.basis.ox.x:=cos(rotate);                             //поворот по оси икс, почему их два незнаю что то там связоно с отрисовкой
-  Result^.Local.basis.ox.y:=sin(rotate);                             //поворот по оси игрик, почему их два незнаю что то там связоно с отрисовкой
+  //Result^.Content:=TDXFEntsInternalStringType(RSHelloWorld);          //Текст незнаю чем они отличаются
+  //Result^.vp.LineWeight:=LnWt100;                                    //Толщина линии
+  //Result^.linespacef:=1;                                             //Межстрочный интервал
+  //rotate:=(rotate*pi)/180;                                           //получаем угол из градуса
+  //Result^.Local.basis.ox.x:=cos(rotate);                             //поворот по оси икс, почему их два незнаю что то там связоно с отрисовкой
+  //Result^.Local.basis.ox.y:=sin(rotate);                             //поворот по оси игрик, почему их два незнаю что то там связоно с отрисовкой
   Result^.vp.Color:=color;                                           //Цвет текста
   Result^.textprop.size:=2.5;                                        //Высота текста
   zcAddEntToCurrentDrawingConstructRoot(Result);                     //добавляем в конструкторскую область
@@ -77,6 +85,17 @@ begin
 end;
 
 begin
+  CmdProp.props.Free;
+  CmdProp.props.InterfaceUses.PushBackIfNotPresent(sysunit);
+  with CmdProp.props.CreateVariable('test1','boolean') do begin
+    username:='тест1';
+    pboolean(data.Addr.GetInstance)^:=true;
+  end;
+  with CmdProp.props.CreateVariable('test2','integer') do begin
+    username:='тест2';
+    pinteger(data.Addr.GetInstance)^:=123;
+  end;
+  zcShowCommandParams(SysUnit^.TypeName2PTD('TCmdProp'),@CmdProp);
   {pt:=}drawMText(NulVertex,2,0);                                  //Рисуем текст, и полилинию
   {pp:=}drawPolyline(uzegeometry.CreateVertex(-20,+5,0),uzegeometry.CreateVertex(+20,-5,0),3);
   if commandmanager.MoveConstructRootTo(rscmSpecifyFirstPoint)=GRNormal then //двигаем их
@@ -87,7 +106,12 @@ end;
 
 initialization
   programlog.LogOutFormatStr('Unit "%s" initialization',[{$INCLUDE %FILE%}],LM_Info,UnitsInitializeLMId);
+  SysUnit^.RegisterType(TypeInfo(TCmdProp));
+  SysUnit^.SetTypeDesk(TypeInfo(TCmdProp),['параметры']);
+  CmdProp.props.init('test');
   CreateCommandFastObjectPlugin(@ExampleConstructToModalSpace_com,'ExampleConstructToModalSpace',CADWG,0);
 finalization
   ProgramLog.LogOutFormatStr('Unit "%s" finalization',[{$INCLUDE %FILE%}],LM_Info,UnitsFinalizeLMId);
+  CmdProp.props.free;
+  CmdProp.props.done;
 end.

d87d063e4768994f19c84ec946f894062bcdbd5a
zamtmn <zamtmn@yandex.ru>
2023-02-08 03:14:01 +0500

    Continue tstCmdLinePromt command

--- cad_source/zcad/commands/uzccommand_tstcmdlineprompt.pas
+++ cad_source/zcad/commands/uzccommand_tstcmdlineprompt.pas
@@ -24,9 +24,10 @@ unit uzccommand_tstCmdLinePrompt;
 
 interface
 uses
+  SysUtils,
   uzcLog,
   uzccommandsabstract,uzccommandsimpl,uzccommandsmanager,
-  uzeparsercmdprompt,
+  uzeparsercmdprompt,uzegeometrytypes,
   uzcinterface,uzcdialogsfiles;
 
 {resourcestring}//чтоб не засирать локализацию просто const
@@ -40,26 +41,29 @@ var
 
 function CmdLinePrompt_com(operands:TCommandOperands):TCommandResult;
 var
-  inpt:String;
+  //inpt:String;
   gr:TGetResult;
   filename:string;
+  p:GDBVertex;
 begin
   if clFileParam=nil then
     clFileParam:=CMDLinePromptParser.GetTokens(RSCLParam);
   commandmanager.ChangeInputMode([IPEmpty,IPShortCuts],[]);
   commandmanager.SetPrompt(clFileParam);
   repeat
-    gr:=commandmanager.GetInput('',inpt);
+    //gr:=commandmanager.GetInput('',inpt);
+    gr:=commandmanager.Get3DPoint('',p);
     case gr of
       GRId:case commandmanager.GetLastId of
-             CLPIdUser1:ZCMsgCallBackInterface.TextMessage('GRId:CLPIdUser1',TMWOHistoryOut);
+             CLPIdUser1:ZCMsgCallBackInterface.TextMessage('GRId: CLPIdUser1',TMWOHistoryOut);
              CLPIdFileDialog:begin
-               ZCMsgCallBackInterface.TextMessage('GRId:CLPIdFileDialog',TMWOHistoryOut);
+               ZCMsgCallBackInterface.TextMessage('GRId: CLPIdFileDialog',TMWOHistoryOut);
                if SaveFileDialog(filename,'CSV',CSVFileFilter,'','Export data...') then begin
                  system.break;
                end;
              end;
           end;
+  GRNormal:ZCMsgCallBackInterface.TextMessage(format('GRNormal: %g,%g,%g',[p.x,p.y,p.z]),TMWOHistoryOut);
     end;
   until gr=GRCancel;
   result:=cmd_ok;
@@ -67,7 +71,7 @@ end;
 
 initialization
   programlog.LogOutFormatStr('Unit "%s" initialization',[{$INCLUDE %FILE%}],LM_Info,UnitsInitializeLMId);
-  CreateCommandFastObjectPlugin(@CmdLinePrompt_com,'tstCmdLinePrompt',0,0);
+  CreateCommandFastObjectPlugin(@CmdLinePrompt_com,'tstCmdLinePrompt',CADWG,0);
 finalization
   ProgramLog.LogOutFormatStr('Unit "%s" finalization',[{$INCLUDE %FILE%}],LM_Info,UnitsFinalizeLMId);
   if clFileParam<>nil then

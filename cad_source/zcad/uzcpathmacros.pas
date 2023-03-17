4670ad14cf7820680517a57f76a6e21fadff1469
zamtmn <zamtmn@yandex.ru>
2023-03-18 02:37:49 +0500

    Добавлены $Env(EnvVarName), $(UserDir), $(LocalConfigDir), $(GlobalConfigDir), $(AppName), $(DirectorySeparator) макросы

--- cad_source/zcad/uzcpathmacros.pas
+++ cad_source/zcad/uzcpathmacros.pas
@@ -21,7 +21,7 @@ unit uzcPathMacros;
 interface
 uses
   MacroDefIntf,uzmacros,uzcsysparams,
-  uzclog,uzblog,uzbpaths,Forms,
+  uzclog,uzblog,uzbpaths,Forms,uzcstrconsts,
   {$IFDEF WINDOWS}ShlObj,{$ENDIF}{$IFNDEF DELPHI}LazUTF8,{$ENDIF}sysutils,uzcsysvars;
 type
   TZCADPathsMacroMethods=class
@@ -35,6 +35,18 @@ type
                                               var {%H-}Abort: boolean): string;
     class function MacroFuncsUserFontsPath (const {%H-}Param: string; const Data: PtrInt;
                                               var {%H-}Abort: boolean): string;
+    class function MacroFuncEnv(const Param: string; const {%H-}Data: PtrInt;
+                                var {%H-}Abort: boolean): string;
+    class function MacroFuncUserDir(const Param: string; const {%H-}Data: PtrInt;
+                                    var {%H-}Abort: boolean): string;
+    class function MacroFuncLocalConfigDir(const Param: string; const {%H-}Data: PtrInt;
+                                           var {%H-}Abort: boolean): string;
+    class function MacroFuncGlobalConfigDir(const Param: string; const {%H-}Data: PtrInt;
+                                            var {%H-}Abort: boolean): string;
+    class function MacroFuncAppName(const Param: string; const {%H-}Data: PtrInt;
+                                    var {%H-}Abort: boolean): string;
+    class function MacroFuncDirectorySeparator(const Param: string; const {%H-}Data: PtrInt;
+                                               var {%H-}Abort: boolean): string;
   end;
 implementation
 class function TZCADPathsMacroMethods.MacroFuncZCADPath(const {%H-}Param: string; const Data: PtrInt;var {%H-}Abort: boolean): string;
@@ -81,6 +93,31 @@ begin
    Result:='/todo/';
 end;
 {$ENDIF}
+class function TZCADPathsMacroMethods.MacroFuncEnv(const Param: string; const Data: PtrInt;var Abort: boolean): string;
+begin
+  Result:=GetEnvironmentVariableUTF8(Param);
+end;
+class function TZCADPathsMacroMethods.MacroFuncUserDir(const Param: string; const {%H-}Data: PtrInt;var {%H-}Abort: boolean): string;
+begin
+  Result:=GetUserDir;
+end;
+class function TZCADPathsMacroMethods.MacroFuncLocalConfigDir(const Param: string; const {%H-}Data: PtrInt;var {%H-}Abort: boolean): string;
+begin
+  Result:=GetAppConfigDir(false);
+end;
+class function TZCADPathsMacroMethods.MacroFuncGlobalConfigDir(const Param: string; const {%H-}Data: PtrInt; var {%H-}Abort: boolean): string;
+begin
+  Result:=GetAppConfigDir(true);
+end;
+class function TZCADPathsMacroMethods.MacroFuncAppName(const Param: string; const {%H-}Data: PtrInt;var {%H-}Abort: boolean): string;
+begin
+  Result:=programname;
+end;
+class function TZCADPathsMacroMethods.MacroFuncDirectorySeparator(const Param: string; const {%H-}Data: PtrInt;var {%H-}Abort: boolean): string;
+begin
+  Result:=DirectorySeparator;
+end;
+
 initialization
 DefaultMacros.AddMacro(TTransferMacro.Create('ZCADPath','',
                        'Path to ZCAD',TZCADPathsMacroMethods.MacroFuncZCADPath,[]));
@@ -92,4 +129,16 @@ DefaultMacros.AddMacro(TTransferMacro.Create('SystemFontsPath','',
                        'System fonts path',TZCADPathsMacroMethods.MacroFuncSystemFontsPath(),[]));
 DefaultMacros.AddMacro(TTransferMacro.Create('UserFontsPath','',
                        'User fonts path',TZCADPathsMacroMethods.MacroFuncsUserFontsPath(),[]));
+DefaultMacros.AddMacro(TTransferMacro.Create('Env','',
+                       'Environment variable, name as parameter',TZCADPathsMacroMethods.MacroFuncEnv,[]));
+DefaultMacros.AddMacro(TTransferMacro.Create('UserDir','',
+                       'User directory',TZCADPathsMacroMethods.MacroFuncUserDir,[]));
+DefaultMacros.AddMacro(TTransferMacro.Create('LocalConfigDir','',
+                       'Local config dir',TZCADPathsMacroMethods.MacroFuncLocalConfigDir,[]));
+DefaultMacros.AddMacro(TTransferMacro.Create('GlobalConfigDir','',
+                       'Global config dir',TZCADPathsMacroMethods.MacroFuncGlobalConfigDir,[]));
+DefaultMacros.AddMacro(TTransferMacro.Create('AppName','',
+                       'Application name',TZCADPathsMacroMethods.MacroFuncAppName,[]));
+DefaultMacros.AddMacro(TTransferMacro.Create('DirectorySeparator','',
+                       'Directory separator',TZCADPathsMacroMethods.MacroFuncDirectorySeparator,[]));
 end.

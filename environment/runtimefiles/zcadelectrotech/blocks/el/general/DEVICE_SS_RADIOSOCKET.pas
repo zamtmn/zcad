ec7064210a644afdd56d971709f2276ca2e9f2bc
veb86 <veb86@mail.ru>
2023-06-21 01:46:26 +0300

    Добавил радиорозетку Подправил слаботочные блоки

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_SS_RADIOSOCKET.pas
@@ -0,0 +1,10 @@
+unit DEVICE_SS_RADIOSOCKET;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='Р0';
+   NMO_BaseName:='Р';
+   DB_link:='Розетка радио';
+   BTY_TreeCoord:='PLAN_SS_SSSOCKETS_Розетка радио';
+end.

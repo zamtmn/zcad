f36ca355e8de3987576919690606098678c8967c
veb86 <veb86@mail.ru>
2023-06-21 15:53:52 +0300

    Добавил УГО на слаботочку

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_SS_RADIODEVICE.pas
@@ -0,0 +1,10 @@
+unit DEVICE_SS_RADIODEVICE;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='Р0';
+   NMO_BaseName:='Р';
+   DB_link:='Радиоприемник';
+   BTY_TreeCoord:='PLAN_SS_SSSOCKETS_Радиоприемник';
+end.

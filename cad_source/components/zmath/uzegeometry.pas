7633ede250e26d042ec139a34c8ca82f8ae361f4
zamtmn <zamtmn@yandex.ru>
2023-09-02 01:56:00 +0500

    Загрубил определение пересечения ббоксов

--- cad_source/components/zmath/uzegeometry.pas
+++ cad_source/components/zmath/uzegeometry.pas
@@ -2327,26 +2327,30 @@ else if IsPointInBB(CreateVertex(bb2.rtf.x,bb2.lbn.y,bb2.rtf.z),bb1) then begin 
 end;}
 function boundingintersect(const bb1,bb2:TBoundingBox):Boolean;
 var
-   b1,b2,b1c,b2c,dist:gdbvertex;
-   //dist:Double;
+  b1,b2,b1c,b2c,dist:gdbvertex;
 begin
-     b1.x:=(bb1.RTF.x-bb1.LBN.x)/2;
-     b1.y:=(bb1.RTF.y-bb1.LBN.y)/2;
-     b1.z:=(bb1.RTF.z-bb1.LBN.z)/2;
-     b2.x:=(bb2.RTF.x-bb2.LBN.x)/2;
-     b2.y:=(bb2.RTF.y-bb2.LBN.y)/2;
-     b2.z:=(bb2.RTF.z-bb2.LBN.z)/2;
-     b1c:=VertexAdd(bb1.LBN,b1);
-     b2c:=VertexAdd(bb2.LBN,b2);
-     dist:=VertexSub(b1c,b2c);
-     dist.x:=abs(dist.x);
-     dist.y:=abs(dist.y);
-     dist.z:=abs(dist.z);
-     result:=false;
-     if (((b1.x+b2.x)-dist.x)>-eps)
-      and(((b1.y+b2.y)-dist.y)>-eps)
-      and(((b1.z+b2.z)-dist.z)>-eps) then
-                                 result:=true
+  //половина диагонали первого бокса
+  b1.x:=(bb1.RTF.x-bb1.LBN.x)/2;
+  b1.y:=(bb1.RTF.y-bb1.LBN.y)/2;
+  b1.z:=(bb1.RTF.z-bb1.LBN.z)/2;
+  //половина диагонали второго бокса
+  b2.x:=(bb2.RTF.x-bb2.LBN.x)/2;
+  b2.y:=(bb2.RTF.y-bb2.LBN.y)/2;
+  b2.z:=(bb2.RTF.z-bb2.LBN.z)/2;
+  //центры боксов
+  b1c:=VertexAdd(bb1.LBN,b1);
+  b2c:=VertexAdd(bb2.LBN,b2);
+  //расстояние между центрами
+  dist:=VertexSub(b1c,b2c);
+  dist.x:=abs(dist.x);
+  dist.y:=abs(dist.y);
+  dist.z:=abs(dist.z);
+  //пересечение боксов
+  result:=false;
+  if (((b1.x+b2.x)-dist.x)>-bigeps)
+     and(((b1.y+b2.y)-dist.y)>-bigeps)
+     and(((b1.z+b2.z)-dist.z)>-bigeps) then
+    result:=true
 end;
 function CreateMatrixFromBasis(const ox,oy,oz:GDBVertex):DMatrix4D;
 begin

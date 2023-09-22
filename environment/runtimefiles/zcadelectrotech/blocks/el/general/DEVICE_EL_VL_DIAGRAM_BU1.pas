b5fb1d47621c61fa912fbdcd2e8fc33faff791ee
vlsx <vl-sx@yandex.ru>
2023-09-22 21:41:09 +1000

    Добавлен элемент "Блок" в однолинейную схему

---
+++ environment/runtimefiles/zcadelectrotech/blocks/el/general/DEVICE_EL_VL_DIAGRAM_BU1.pas
@@ -0,0 +1,36 @@
+unit DEVICE_EL_VL_DIAGRAM_BU1;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname;
+
+var
+
+T1:String;(*'Обозначение'*)
+T2:String;(*'Марка'*)
+T3:String;(*'Параметры'*)
+
+T11:Integer;(*'123'*)
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_EM_Аппаратура';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_Name:='А0';
+NMO_BaseName:='А';
+NMO_Suffix:='??';
+NMO_Affix:='.2';
+
+T1:='??';
+T2:='??';
+T3:='??';
+
+T11:=3;
+
+end.

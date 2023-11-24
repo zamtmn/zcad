e2a9ccd4a8f5e1b2e4d7d9528314b99237697013
veb86 <veb86@mail.ru>
2023-11-24 16:04:51 +0300

    Добавил УГО

---
+++ environment/runtimefiles/zcadelectrotech/preload/lowcurrentnetworks/networkequipment/DEVICE_SS_IPCAMIN.pas
@@ -0,0 +1,11 @@
+unit DEVICE_SS_IPCAMIN;
+interface
+usescopy firesensor;
+implementation
+begin
+   NMO_Name:='BK0.0';
+   NMO_BaseName:='BK';
+   DB_link:='Камера внутренняя круглая';
+   BTY_TreeCoord:='PLAN_SS_Камера внутренняя круглая';
+   NMO_Template:='@@[NMO_BaseName]@@[GC_HDShortName].@@[GC_HDGroup]';
+end.

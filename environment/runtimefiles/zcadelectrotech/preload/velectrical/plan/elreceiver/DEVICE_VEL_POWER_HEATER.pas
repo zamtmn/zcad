cf5ed953450512c3c994a2af5379de17e76d9e60
veb86 <veb86@mail.ru>
2024-03-18 17:00:26 +0300

    bim электрика продолжение

---
+++ environment/runtimefiles/zcadelectrotech/preload/velectrical/plan/elreceiver/DEVICE_VEL_POWER_HEATER.pas
@@ -0,0 +1,38 @@
+unit DEVICE_VEL_POWER_HEATER;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname_eo;
+usescopy objgroup;
+usescopy addtocable;
+usescopy elreceivers;
+usescopy vlocation;
+usescopy vinfopersonaluse;
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_VEL_Электроприемники_Электрообогреватель';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_BaseName:='Об';
+NMO_Suffix:='(??)';
+NMO_Template:='@@[NMO_BaseName]@@[NMO_Suffix]\P@@[Power]';
+
+GC_NameGroupTemplate:='@@[GC_HeadDevice].@@[GC_HDGroup]';
+
+realnamedev:='Электрообогреватель';
+Power:=1.5;
+CosPHI:=0.9;
+Voltage:=_AC_220V_50Hz;
+Phase:=_A;
+
+INFOPERSONALUSE_TextTemplate:='';
+
+SerialConnection:=1;
+
+end.
--- environment/runtimefiles/zcadelectrotech/preload/velectrical/velectrical.cmd
+++ environment/runtimefiles/zcadelectrotech/preload/velectrical/velectrical.cmd
@@ -2,4 +2,5 @@ MergeBlocks($(CurrentMacrosPath)schemes$(DirectorySeparator)_vschemes.dxf)
 MergeBlocks($(CurrentMacrosPath)schemes$(DirectorySeparator)_vschemesugo.dxf)
 MergeBlocks($(CurrentMacrosPath)plan$(DirectorySeparator)sockets$(DirectorySeparator)_vsockets.dxf)
 MergeBlocks($(CurrentMacrosPath)plan$(DirectorySeparator)shields$(DirectorySeparator)_vshields.dxf)
+MergeBlocks($(CurrentMacrosPath)plan$(DirectorySeparator)elreceiver$(DirectorySeparator)_elreceiver.dxf)
 

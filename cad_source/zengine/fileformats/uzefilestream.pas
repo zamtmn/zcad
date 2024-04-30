b1460409dd00fc2bf141039773386a2f5bb0d6c6
zamtmn <zamtmn@yandex.ru>
2024-04-30 19:21:31 +0500

    Эксперименты3

--- cad_source/zengine/fileformats/uzefilestream.pas
+++ cad_source/zengine/fileformats/uzefilestream.pas
@@ -22,7 +22,7 @@ unit uzeFileStream;
 {$ModeSwitch advancedrecords}
 {$PointerMath ON}
 {$Interfaces CORBA}
-{$Inline off}
+{Inline off}
 interface
 uses
   SysUtils,
@@ -323,9 +323,22 @@ end;
 
 
 function TZInMemoryReader.FindEOL:int64;
+const
+  CR_XOR_MASK4=$0d0d0d0d;
+  LF_XOR_MASK4=$0a0a0a0a;
+  SUB_MASK4=Integer(-$01010101);
+  OVERFLOW_MASK4=Integer($80808080);
+
+  CR_XOR_MASK8=$0d0d0d0d0d0d0d0d;
+  LF_XOR_MASK8=$0a0a0a0a0a0a0a0a;
+  SUB_MASK8=Int64(-$0101010101010101);
+  OVERFLOW_MASK8=Int64($8080808080808080);
 var
   InMemPos:int64;
   pch:pchar;
+  i,n,optin,nn:NativeInt;
+  X4,T4,V4:Integer;
+  X8,T8,V8:NativeInt;
 begin
   inc(FindEOLcount);
 
@@ -333,16 +346,118 @@ begin
 
   InMemPos:=fInMemPosition;
   pch:=@fMemory[InMemPos];
+  n:=fCurrentViewSize-InMemPos;
 
-  while InMemPos<fCurrentViewSize do begin
+  //проверяем по 8 байт
+  optin:=n div sizeof(qword);
+  if optin>0 then begin
+    for i:=optin-1 downto 0 do begin
+      X8:=pqword(pch)^;
+      T8 := (X8 xor CR_XOR_MASK8);
+      X8 := (X8 xor LF_XOR_MASK8);
+      V8 := T8 + SUB_MASK8;
+      T8 := not T8;
+      T8 := T8 and V8;
+      V8 := X8 + SUB_MASK8;
+      X8 := (not X8);
+      V8 := V8 and X8;
+      T8 := T8 or V8;
+      if (T8 and OVERFLOW_MASK8<>0) then begin
+        nn := Byte(Byte(T8 and $80 = 0) + Byte(T8 and $8080 = 0) + Byte(T8 and $808080 = 0) + Byte(T8 and $80808080 = 0) + Byte(T8 and $8080808080 = 0) + Byte(T8 and $808080808080 = 0) + Byte(T8 and $80808080808080 = 0));
+        FNeedScipEOL:=True;
+        inc(InMemPos,(optin-i)*sizeof(qword)-(8-nn));
+        exit(InMemPos);
+
+        T4:=lo(T8) and OVERFLOW_MASK4;
+        if ((T4 and OVERFLOW_MASK4)<>0) then begin
+          nn := Byte(Byte(T4 and $80 = 0) + Byte(T4 and $8080 = 0) + Byte(T4 and $808080 = 0));
+          FNeedScipEOL:=True;
+          inc(InMemPos,(optin-i)*sizeof(qword)-(4-nn));
+          exit(InMemPos);
+        end;
+
+        T4:=hi(T8) and OVERFLOW_MASK4;
+        if (T4 and OVERFLOW_MASK4)<>0 then begin
+          nn := Byte(Byte(T4 and $80 = 0) + Byte(T4 and $8080 = 0) + Byte(T4 and $808080 = 0));
+          FNeedScipEOL:=True;
+          inc(InMemPos,(optin-i)*sizeof(qword)-(4-nn));
+          exit(InMemPos);
+        end;
+      end;
+      inc(pqword(pch));
+    end;
+    inc(InMemPos,optin*sizeof(qword));
+    n:=n mod sizeof(qword);
+  end;
+
+  //проверяем по 4 байта
+  optin:=n div sizeof(dword);
+  if optin>0 then begin
+    for i:=optin-1 downto 0 do begin
+      x4:=pdword(pch)^;
+      T4 := (X4 xor CR_XOR_MASK4);
+      X4 := (X4 xor LF_XOR_MASK4);
+      V4 := T4 + SUB_MASK4;
+      T4 := not T4;
+      T4 := T4 and V4;
+      V4 := X4 + SUB_MASK4;
+      X4 := (not X4);
+      V4 := V4 and X4;
+      T4 := T4 or V4;
+      T4 := (T4 and OVERFLOW_MASK4);
+      if (T4 and OVERFLOW_MASK4)<>0 then begin
+        nn := Byte(Byte(T4 and $80 = 0) + Byte(T4 and $8080 = 0) + Byte(T4 and $808080 = 0));
+        FNeedScipEOL:=True;
+        inc(InMemPos,(optin-i)*sizeof(dword)-(4-nn));
+        exit(InMemPos);
+      end;
+      inc(pdword(pch));
+    end;
+    inc(InMemPos,optin*sizeof(dword));
+    n:=n mod sizeof(dword);
+  end;
+
+  //проверяем по 2 байта
+  optin:=n div sizeof(word);
+  if optin>0 then begin
+    for i:=optin-1 downto 0 do begin
+      x4:=pword(pch)^;
+      T4 := (X4 xor CR_XOR_MASK4);
+      X4 := (X4 xor LF_XOR_MASK4);
+      V4 := T4 + SUB_MASK4;
+      T4 := not T4;
+      T4 := T4 and V4;
+      V4 := X4 + SUB_MASK4;
+      X4 := (not X4);
+      V4 := V4 and X4;
+      T4 := T4 or V4;
+      T4 := (T4 and OVERFLOW_MASK4);
+
+      if T4<>0 then begin
+        nn := Byte(Byte(T8 and $80 = 0));
+        FNeedScipEOL:=True;
+        inc(InMemPos,(optin-i)*sizeof(qword)-(8-nn));
+        exit(InMemPos);
+      end;
+      inc(pword(pch));
+    end;
+    inc(InMemPos,optin*sizeof(word));
+    n:=n mod sizeof(word);
+  end;
+
+  //остатки проверяем по байту
+  for i:=n-1 downto 0 do begin
     if byte(pch^)<14 then
       if (pch^=ChLF)or(pch^=ChCR) then begin  //pch^ in CLFCR медленней в 2 раза
         FNeedScipEOL:=True;
-        exit(InMemPos)
+        inc(InMemPos,n-i-1);
+        exit(InMemPos);
       end;
-    inc(InMemPos);
     inc(pch);
   end;
+  inc(InMemPos,n);
+
+
   if InMemPos=fSize then
     result:=InMemPos
   else
@@ -361,16 +476,16 @@ begin
   if PEOL=fInMemPosition then
     exit('')
   else if PEOL=CNotInThisPage then begin
-    l:=fCurrentViewSize-fInMemPosition;
-    //SetLength(Result,l);
-    //Move(fMemory[fInMemPosition],Result[1],l);
+    {//}l:=fCurrentViewSize-fInMemPosition;
+    {//}SetLength(Result,l);
+    {//}Move(fMemory[fInMemPosition],Result[1],l);
     setFromTMemViewInfo(fIS.MoveMemViewProc(fCurrentViewOffset+fCurrentViewSize));
-    {ts:=}ParseString();
-    //result:=result+ts;
+    ts:=ParseString();
+    {//}result:=result+ts;
   end else begin
-    l:=PEOL-fInMemPosition;
-    //SetLength(Result,l);
-    //Move(fMemory[fInMemPosition],Result[1],l);
+    {//}l:=PEOL-fInMemPosition;
+    {//}SetLength(Result,l);
+    {//}Move(fMemory[fInMemPosition],Result[1],l);
     fInMemPosition:=PEOL;
   end;
 end;

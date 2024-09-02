2fbccd0268ed39d80ed086fbc42aea8deb3874c7
zamtmn <zamtmn@yandex.ru>
2024-09-02 20:40:51 +0500

    Подсветка синтаксиса в synform

--- cad_source/zcad/lclmod/uzcsynform.pas
+++ cad_source/zcad/lclmod/uzcsynform.pas
@@ -23,7 +23,8 @@ interface
 uses
 
   uzcsysparams,uzcinterface,uzclog,uzcinfoform,
-  SynEdit,Forms,StdCtrls,Controls;
+  SynEdit,SynHighlighterPas,SynEditHighlighter,
+  Forms,StdCtrls,Controls,Graphics;
 
 type
   TSynForm=class(TDialogForm)
@@ -33,13 +34,24 @@ type
                     end;
 implementation
 procedure TSynForm.AfterConstruction;
+var
+  shlist:TSynHighlighterList;
+  phli:integer;
 begin
-     inherited;
-     self.Position:=poDesigned;
-     Memo:={TMemo}TSynEdit.create(self);
-     {$IFNDEF DELPHI}Memo.ScrollBars:=ssAutoBoth;{$ENDIF}
-     Memo.Align:=alClient;
-     Memo.Parent:=self;
+  inherited;
+  self.Position:=poDesigned;
+  Memo:={TMemo}TSynEdit.create(self);
+  {$IFNDEF DELPHI}Memo.ScrollBars:=ssAutoBoth;{$ENDIF}
+  Memo.Align:=alClient;
+  Memo.Parent:=self;
+  Memo.Font.Quality:=fqCleartype;
+  Memo.Font.Size:=8;
+  shlist:=GetPlaceableHighlighters;
+  if shlist<>nil then begin
+    phli:=shlist.FindByName('ObjectPascal');
+    if phli<>-1 then
+      Memo.Highlighter:=shlist[phli].Create(Memo);
+  end;
 end;
 initialization
 finalization

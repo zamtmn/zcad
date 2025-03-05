0295de27853beecfb39e97eab4e8f17077af0253
zamtmn <zamtmn@yandex.ru>
2025-03-05 16:43:34 +0500

    Более глубокое разбиение простпанства

--- environment/runtimefiles/common/cfg/configs/sysvar.pas
+++ environment/runtimefiles/common/cfg/configs/sysvar.pas
@@ -148,8 +148,8 @@ begin
   DISP_RemoveSystemCursorFromWorkArea:=True;
   DSGN_SelNew:=False;
   DWG_EditInSubEntry:=False;
-  RD_SpatialNodeCount:=2000;
-  RD_SpatialNodesDepth:=8;
+  RD_SpatialNodeCount:=8;
+  RD_SpatialNodesDepth:=10;
   DWG_RotateTextInLT:=True;
   RD_MaxLTPatternsInEntity:=10000;
   RD_PanObjectDegradation:=False;

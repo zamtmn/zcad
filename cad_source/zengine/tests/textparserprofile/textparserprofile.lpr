d5c34fd7f14ab9253cb225b07e19be06c329299d
zamtmn <zamtmn@yandex.ru>
2025-04-28 18:50:19 +0500

    Тест парсеров

---
+++ cad_source/zengine/tests/textparserprofile/textparserprofile.lpr
@@ -0,0 +1,62 @@
+program textparserprofile;
+{$Codepage UTF8}
+{$Mode delphi}
+uses
+  SysUtils,StrUtils,Math,
+  Interfaces,
+  uzbtypes,
+  uzetextpreprocessor,uzctextpreprocessorimpl,
+  velecparser;
+
+type
+  TTestFunc=function(AData:TDXFEntsInternalStringType):TDXFEntsInternalStringType;
+
+function CalcCheckCRC(AData:TDXFEntsInternalStringType):Word;
+var
+  uch:UnicodeChar;
+begin
+  result:=0;
+  for uch in AData do
+    result:=result+ord(uch);
+end;
+
+function ZCADTextFormatTest(AData:TDXFEntsInternalStringType):TDXFEntsInternalStringType;
+begin
+  Result:=textformat(AData,nil);
+end;
+
+function DoTest(ATestFunc:TTestFunc;AData:TDXFEntsInternalStringType;ATestName:string):TDXFEntsInternalStringType;
+const
+  maxsyms=20;//количество отображаемых символов результата
+var
+  LPTime:Tdatetime;
+  l:integer;
+begin
+  //имя теста
+  writeln(ATestName,':');
+  LPTime:=now();
+  Result:=ATestFunc(AData);
+  LPTime:=now()-LPTime;
+  //выводим первые maxsyms символов результата
+  l:=min(maxsyms,Length(Result));
+  writeln(format('  First result %d chars = %s',[l,copy(Result,1,l)]));
+  //на случай если результат длинный его долину и CRC для проверки
+  writeln('  Result length  = ',Length(Result));
+  writeln('  Result CRC  = ',CalcCheckCRC(Result));
+  //время теста
+  writeln('  Time elapsed = '+inttostr(round(lptime*10e7))+'ms');
+end;
+var
+  VTestData:TDXFEntsInternalStringType;
+  TestResult:TDXFEntsInternalStringType;
+begin
+  VTestData:=DupeString('йцукqwer',1000000);
+  TestResult:='';
+  TestResult:=DoTest(@ZCADTextFormatTest,VTestData,'ZCADTextFormatTest(DupeString(''йцукqwer'',1000))');
+  TestResult:='';
+  TestResult:=DoTest(@velecParseMText,VTestData,'velecParseMText(DupeString(''йцукqwer'',1000))');
+  readln;
+end.
+
+
+
---
+++ cad_source/zengine/tests/textparserprofile/velecparser.pas
@@ -0,0 +1,141 @@
+unit velecparser;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls,Dialogs, StdCtrls,
+  uzbtypes;
+
+type
+  TTextStyle = record
+    FontName: string;
+    Bold: Boolean;
+    Italic: Boolean;
+    CharSet: Integer;
+    Pitch: Integer;
+  end;
+  PTextStyle = ^TTextStyle;
+
+  TTextFragment = record
+    FragmentText: string;
+    Style: TTextStyle;
+  end;
+  PTextFragment = ^TTextFragment;
+
+function velecParseMText(const Input: TDXFEntsInternalStringType):TDXFEntsInternalStringType;
+
+implementation
+
+function UnicodeToText(const S: TDXFEntsInternalStringType): TDXFEntsInternalStringType;
+var
+  i: Integer;
+  Code: TDXFEntsInternalStringType;
+  CharCode: Integer;
+begin
+  Result := '';
+  i := 1;
+  while i <= Length(S) do
+  begin
+    if (S[i] = '\') and (i + 6 <= Length(S)) and (S[i+1] = 'U') and (S[i+2] = '+') then
+    begin
+      Code := Copy(S, i+3, 4);
+      if TryStrToInt('$' + Code, CharCode) then
+        Result := Result + UTF8Encode(WideChar(CharCode))
+      else
+        Result := Result + '?';
+      Inc(i, 7);
+    end
+    else
+    begin
+      Result := Result + S[i];
+      Inc(i);
+    end;
+  end;
+end;
+
+function velecParseMText(const Input: TDXFEntsInternalStringType):TDXFEntsInternalStringType;
+var
+  i: Integer;
+  c: Char;
+  Buffer, Code, UnicodeBuffer: string;
+  InControl: Boolean;
+  InBraces: Integer;
+  CharCode: Integer;
+begin
+  result:='';
+  Buffer := '';
+  Code := '';
+  InControl := False;
+  InBraces := 0;
+  i := 1;
+  while i <= Length(Input) do
+  begin
+    c := Input[i];
+    if InControl then
+    begin
+      if (c = ';') then
+      begin
+        //ApplyCode(Code);
+        Code := '';
+        InControl := False;
+      end
+      else
+        Code := Code + c;
+    end
+    else if (c = '\') and (i + 6 <= Length(Input)) and (Input[i+1] = 'U') and (Input[i+2] = '+') then
+    begin
+      // \U+XXXX обработка юникода напрямую
+      UnicodeBuffer := Copy(Input, i+3, 4);
+      if TryStrToInt('$' + UnicodeBuffer, CharCode) then
+        Buffer := Buffer + UTF8Encode(WideChar(CharCode))
+      else
+        Buffer := Buffer + '?';
+      Inc(i, 6); // пропускаем \U+XXXX
+    end
+    else if c = '\' then
+    begin
+      // управляющая последовательность начинается
+      if Buffer <> '' then
+      begin
+        //AddFragment(Buffer);
+        Buffer := '';
+      end;
+      InControl := True;
+      Code := '\';
+    end
+    else if c = '{' then
+    begin
+      if Buffer <> '' then
+      begin
+        //AddFragment(Buffer);
+        Buffer := '';
+      end;
+      //PushStyle;
+      Inc(InBraces);
+    end
+    else if c = '}' then
+    begin
+      if Buffer <> '' then
+      begin
+        //AddFragment(Buffer);
+        Buffer := '';
+      end;
+      //PopStyle;
+      Dec(InBraces);
+      if InBraces = 0 then
+        ;//CurrentStyle := DefaultStyle;
+    end
+    else
+      Buffer := Buffer + c;
+    Inc(i);
+  end;
+  if Buffer <> '' then
+    ;//AddFragment(Buffer);
+end;
+
+
+
+end.
+

58d7ef6081370760d29aefb8f2ea9c380fe090cb
zamtmn <zamtmn@yandex.ru>
2025-05-06 14:35:04 +0500

    * Починен переход на последний найденый элемент при уже показаном первом для команды FindPrev

--- cad_source/zcad/commands/uzccommand_find.pas
+++ cad_source/zcad/commands/uzccommand_find.pas
@@ -357,7 +357,7 @@ begin
     if fe.Finded.Count>0 then begin
       dec(fe.Current);
       if (fe.Current<0)or(fe.Current>=fe.Finded.Count) then
-        fe.Current:=0;
+        fe.Current:=fe.Finded.Count-1;
       showentity(fe);
     end;
   result:=cmd_ok;

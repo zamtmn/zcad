18a37be1127ca4dc9c8249eeba9464d594992c0e
zamtmn <zamtmn@yandex.ru>
2025-08-14 12:56:43 +0500

    + FTP 5e 19C-F5-23WT-B305

---
+++ environment/runtimefiles/AllCPU-AllOS/zcadelectrotech/data/programdb/eurolan/eurolan.pas
@@ -0,0 +1,32 @@
+subunit devicebase;
+interface
+uses system;
+type
+    T19C_F5_23WT_B305=packed object(CableDeviceBaseObject);
+                   end;
+var
+   _EQ_19C_F5_23WT_B305:T19C_F5_23WT_B305;
+implementation
+begin
+     _EQ_19C_F5_23WT_B305.initnul;
+
+     _EQ_19C_F5_23WT_B305.Category:=_kables;
+     _EQ_19C_F5_23WT_B305.Group:=_cables_sv;
+     _EQ_19C_F5_23WT_B305.EdIzm:=_m;
+     _EQ_19C_F5_23WT_B305.ID:='19C_F5_23WT_B305';
+     _EQ_19C_F5_23WT_B305.Standard:='';
+     _EQ_19C_F5_23WT_B305.OKP:='';
+     _EQ_19C_F5_23WT_B305.Manufacturer:='Eurolan';
+     _EQ_19C_F5_23WT_B305.Description:='Кабели предназначены для присоединения к стационарным электрическим приборам, аппаратам, устройствам с номинальным переменным напряжением до 500 В переменного тока частоты до 400 Гц или постоянным напряжением до 750 В. Применяются для подключения различных устройств промышленной автоматики, датчиков, контроллеров, исполнительных механизмов и других устройств. Также монтажный кабель используют в сисемах телемеханики, прочих системах где, необходим контроль и управление отдельными элементами оборудования';
+
+     _EQ_19C_F5_23WT_B305.NameShortTemplate:='19C-F5-23WT-B305';
+     _EQ_19C_F5_23WT_B305.NameTemplate:='Кабель экранированный FTP 4 пары, категория 5e';
+     _EQ_19C_F5_23WT_B305.UIDTemplate:='%%[ID]';
+     _EQ_19C_F5_23WT_B305.NameFullTemplate:='Кабель экранированный внутренней прокладки FTP 4 пары, категория 5e, LSZH, белый';
+
+     _EQ_19C_F5_23WT_B305.Wire_Count_Section_DESC:=_02_75;
+
+     _EQ_19C_F5_23WT_B305.TreeCoord:='BP_Eurolan_FTP-5e_19C-F5-23WT-B305|BC_Кабельная продукция_FTP-5e_19C-F5-23WT-B305(Eurolan)';
+
+     _EQ_19C_F5_23WT_B305.format;
+end.

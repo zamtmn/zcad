650ae999eb82895d6fae13061c5c136fc5b176fb
veb86 <veb86@mail.ru>
2025-08-19 12:54:39 +0300

    Для диспетчера подключений

---
+++ cad_source/zcad/velec/connectmanager/uzvmcdbconsts.pas
@@ -0,0 +1,31 @@
+{
+*****************************************************************************
+*                                                                           *
+*  This file is part of the ZCAD                                            *
+*                                                                           *
+*  See the file COPYING.txt, included in this distribution,                 *
+*  for details about the copyright.                                         *
+*                                                                           *
+*  This program is distributed in the hope that it will be useful,          *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                     *
+*                                                                           *
+*****************************************************************************
+}
+{
+@author(Vladimir Bobrov)
+}
+
+unit uzvmcdbconsts;
+{$INCLUDE zengineconfig.inc}
+
+interface
+const
+  //osm_inspoint=1;
+
+  vcalctempdbfilename='mytempcalcdb.db3';   //имя временного файла необходимого для выполнения расчетов с использованием БД
+
+
+implementation
+
+end.

3b394987349428608005461f3ad7f8644f85c91d
konard <drakonard@gmail.com>
2025-10-07 08:47:13 +0300

    Implement ConvertControlArrayToFitArray function for spline fit points
    - Add function ConvertControlArrayToFitArray that converts control points to points on the spline curve
    - Implement B-spline evaluation using Cox-de Boor recursion
    - Generate uniform clamped knot vectors for B-splines
    - Evaluate curve at 20 uniform parameter points from 0 to 1
    - Fixes issue #200: implement uzccommand_spline.ConvertControlArrayToFitArray

--- cad_source/zcad/commands/uzccommand_spline.pas
+++ cad_source/zcad/commands/uzccommand_spline.pas
@@ -23,15 +23,18 @@ unit uzccommand_spline;
 interface
 
 uses
-  uzcLog,
-  uzccommandsabstract,uzccommandsimpl,
-  uzeconsts,uzcstrconsts,
-  uzegeometrytypes,
-  uzccommandsmanager,
-  uzeentspline,uzeentity,uzeentityfactory,
-  uzcutils,
-  uzcdrawings,
-  UGDBPoint3DArray;
+   uzcLog,
+   uzccommandsabstract,uzccommandsimpl,
+   uzeconsts,uzcstrconsts,
+   uzegeometrytypes,
+   uzccommandsmanager,
+   uzeentspline,uzeentity,uzeentityfactory,
+   uzcutils,
+   uzcdrawings,
+   UGDBPoint3DArray,
+   Math;
+
+function ConvertControlArrayToFitArray(const AControlPoints: GDBPoint3dArray; Degree: integer): GDBPoint3dArray;
 
 type
   PSplineInteractiveData=^TSplineInteractiveData;
@@ -41,6 +44,102 @@ type
   end;
 
 implementation
+
+// Generate uniform clamped knot vector for B-spline
+procedure GenerateUniformClampedKnotVector(n: integer; degree: integer; var knots: array of single);
+var
+  i: integer;
+  numKnots: integer;
+  internalKnots: integer;
+begin
+  numKnots := n + degree + 2;
+  SetLength(knots, numKnots);
+  internalKnots := n - degree;
+
+  // Start with degree+1 zeros
+  for i := 0 to degree do
+    knots[i] := 0.0;
+
+  // Internal knots
+  if internalKnots > 0 then
+    for i := 1 to internalKnots do
+      knots[degree + i] := i / (internalKnots + 1);
+
+  // End with degree+1 ones
+  for i := 0 to degree do
+    knots[numKnots - 1 - i] := 1.0;
+end;
+
+// Cox-de Boor recursion for B-spline basis function
+function BSplineBasis(i, degree: integer; u: single; const knots: array of single): single;
+var
+  left, right: single;
+begin
+  if degree = 0 then begin
+    if (knots[i] <= u) and (u < knots[i+1]) then
+      Result := 1.0
+    else
+      Result := 0.0;
+    exit;
+  end;
+
+  // Left term
+  left := 0.0;
+  if knots[i+degree] <> knots[i] then
+    left := ((u - knots[i]) / (knots[i+degree] - knots[i])) * BSplineBasis(i, degree-1, u, knots);
+
+  // Right term
+  right := 0.0;
+  if knots[i+degree+1] <> knots[i+1] then
+    right := ((knots[i+degree+1] - u) / (knots[i+degree+1] - knots[i+1])) * BSplineBasis(i+1, degree-1, u, knots);
+
+  Result := left + right;
+end;
+
+// Evaluate B-spline curve at parameter u
+function EvaluateBSplineCurve(const controlPoints: GDBPoint3dArray; degree: integer; u: single; const knots: array of single): GDBVertex;
+var
+  i: integer;
+  basis: single;
+  point: GDBVertex;
+begin
+  point := NulVertex;
+  for i := 0 to controlPoints.Count - 1 do begin
+    basis := BSplineBasis(i, degree, u, knots);
+    point.x := point.x + controlPoints.getData(i).x * basis;
+    point.y := point.y + controlPoints.getData(i).y * basis;
+    point.z := point.z + controlPoints.getData(i).z * basis;
+  end;
+  Result := point;
+end;
+
+function ConvertControlArrayToFitArray(const AControlPoints: GDBPoint3dArray; Degree: integer): GDBPoint3dArray;
+const
+  NUM_FIT_POINTS = 20; // Number of points to evaluate on the curve
+var
+  knots: array of single;
+  i: integer;
+  u: single;
+  fitPoint: GDBVertex;
+begin
+  Result.init(NUM_FIT_POINTS);
+
+  if AControlPoints.Count < Degree + 1 then begin
+    // Not enough control points for the degree
+    exit;
+  end;
+
+  // Generate knot vector
+  SetLength(knots, AControlPoints.Count + Degree + 1);
+  GenerateUniformClampedKnotVector(AControlPoints.Count - 1, Degree, knots);
+
+  // Evaluate curve at uniform parameter intervals
+  for i := 0 to NUM_FIT_POINTS - 1 do begin
+    u := i / (NUM_FIT_POINTS - 1); // from 0 to 1
+    fitPoint := EvaluateBSplineCurve(AControlPoints, Degree, u, knots);
+    Result.PushBackData(fitPoint);
+  end;
+end;
 
 procedure InteractiveSplineManipulator(
   const PInteractiveData:PSplineInteractiveData;

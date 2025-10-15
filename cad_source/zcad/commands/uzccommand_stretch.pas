aa9a03a1f49ad02bdb8ff3b6cb5310c5106a4f93
zamtmn <zamtmn@yandex.ru>
2025-10-15 13:26:45 +0500

    + (ии)Возможность предвыбора точек растяжки

--- cad_source/zcad/commands/uzccommand_stretch.pas
+++ cad_source/zcad/commands/uzccommand_stretch.pas
@@ -13,7 +13,7 @@
 *****************************************************************************
 }
 {
-@author(Andrey Zubarev <zamtmn@yandex.ru>) 
+@author(Andrey Zubarev <zamtmn@yandex.ru>)
 }
 
 unit uzccommand_stretch;
@@ -36,20 +36,129 @@ uses
   uzclog,
   uzegeometrytypes,
   uzegeometry,
-  uzccommand_selectframe,uzccommand_ondrawinged;
+  uzccommand_selectframe,uzccommand_ondrawinged,
+  UGDBSelectedObjArray,
+  UGDBControlPointArray;
 
 implementation
 
 type
-  TStretchComMode=(SM_GetEnts,SM_FirstPoint,SM_SecondPoint);
+  // Режимы работы команды Stretch
+  // Modes of the Stretch command operation
+  TStretchComMode=(
+    SM_GetEnts,      // Режим выбора объектов / Selection mode
+    SM_FirstPoint,   // Режим выбора первой точки / First point selection mode
+    SM_SecondPoint   // Режим выбора второй точки / Second point selection mode
+  );
 
 var
   StretchComMode:TStretchComMode;
 
-procedure Stretch_com_CommandStart(const Context:TZCADCommandContext;Operands:pansichar);
+// Создание вершины 2D с целочисленными координатами
+// Creating a 2D vertex with integer coordinates
+function CreateVertex2DI(x, y: Integer): GDBvertex2DI;
 begin
-  StretchComMode:=SM_GetEnts;
-  FrameEdit_com_CommandStart(Context,Operands);
+  Result.x := x;
+  Result.y := y;
+end;
+
+// Проверка наличия выбранных контрольных точек в массиве выделенных объектов
+// Checking for the presence of selected control points in the selected objects array
+function HasSelectedControlPoints:Boolean;
+var
+  i:integer;
+  tdesc:pselectedobjdesc;
+begin
+  Result:=False;
+  if drawings.GetCurrentDWG.GetSelObjArray.Count>0 then begin
+    tdesc:=drawings.GetCurrentDWG.GetSelObjArray.GetParrayAsPointer;
+    for i:=0 to drawings.GetCurrentDWG.GetSelObjArray.Count-1 do begin
+      if tdesc^.pcontrolpoint<>nil then
+        if tdesc^.pcontrolpoint^.SelectedCount<>0 then begin
+          Result:=True;
+          Exit;
+        end;
+      Inc(tdesc);
+    end;
+  end;
+end;
+
+// Проверка наличия выбранных объектов (примитивов) в чертеже
+// Checking for the presence of selected objects (primitives) in the drawing
+function HasSelectedObjects:Boolean;
+begin
+  Result:=drawings.GetCurrentDWG.wa.param.SelDesc.Selectedobjcount>0;
+end;
+
+// Создание контрольных точек для всех выбранных объектов
+// Creating control points for all selected objects
+procedure CreateControlPointsForSelectedObjects;
+var
+  i:integer;
+  tdesc:pselectedobjdesc;
+begin
+  if drawings.GetCurrentDWG.GetSelObjArray.Count>0 then begin
+    tdesc:=drawings.GetCurrentDWG.GetSelObjArray.GetParrayAsPointer;
+    for i:=0 to drawings.GetCurrentDWG.GetSelObjArray.Count-1 do begin
+      // Если у объекта еще нет контрольных точек, создаем их
+      // If the object doesn't have control points yet, create them
+      if tdesc^.pcontrolpoint=nil then begin
+        if tdesc^.objaddr^.IsHaveGRIPS then begin
+          Getmem(Pointer(tdesc^.pcontrolpoint),sizeof(GDBControlPointArray));
+          tdesc^.objaddr^.addcontrolpoints(tdesc);
+        end;
+      end;
+      Inc(tdesc);
+    end;
+  end;
+end;
+
+procedure Stretch_com_CommandStart(const Context:TZCADCommandContext;Operands:pansichar);
+var
+  DC:TDrawContext;
+begin
+  // Проверяем наличие предварительно выбранных контрольных точек
+  // Check if there are already selected control points from a crossing window selection
+  if HasSelectedControlPoints then begin
+    // Пропускаем фазу выделения и сразу переходим к режиму первой точки
+    // Skip the selection phase and go directly to the first point mode
+    StretchComMode:=SM_FirstPoint;
+    drawings.GetCurrentDWG.wa.SetMouseMode(MGet3DPoint or MMoveCamera or MRotateCamera);
+    // Перемещаем точки для правильного отображения (красные ручки)
+    // Remap points to ensure they're properly displayed (red grips)
+    dc:=drawings.GetCurrentDWG.wa.CreateRC;
+    drawings.GetCurrentDWG.GetSelObjArray.remappoints(
+      drawings.GetCurrentDWG.GetPcamera.POSCOUNT,drawings.GetCurrentDWG.wa.param.scrollmode,
+      drawings.GetCurrentDWG.GetPcamera^,drawings.GetCurrentDWG^.myGluProject2,dc);
+    zcUI.Do_GUIaction(nil,zcMsgUIActionRedrawContent);
+  end else if HasSelectedObjects then begin
+    // Если есть выбранные объекты, но нет контрольных точек
+    // If there are selected objects but no control points yet
+    // Создаем контрольные точки для всех выбранных объектов
+    // Create control points for all selected objects
+    CreateControlPointsForSelectedObjects;
+    // Перемещаем точки для правильного отображения
+    // Remap points to ensure they're properly displayed
+    dc:=drawings.GetCurrentDWG.wa.CreateRC;
+    drawings.GetCurrentDWG.GetSelObjArray.remappoints(
+      drawings.GetCurrentDWG.GetPcamera.POSCOUNT,drawings.GetCurrentDWG.wa.param.scrollmode,
+      drawings.GetCurrentDWG.GetPcamera^,drawings.GetCurrentDWG^.myGluProject2,dc);
+    // Выбираем только контрольные точки, попавшие в секущую рамку (подсвечиваем их красным)
+    // Select only control points that fell within the crossing frame (highlight them in red)
+    drawings.GetCurrentDWG.GetSelObjArray.selectcontrolpointinframe(
+      drawings.GetCurrentDWG.wa.param.seldesc.Frame1,
+      drawings.GetCurrentDWG.wa.param.seldesc.Frame2);
+    // Переходим сразу к режиму первой точки
+    // Go directly to the first point mode
+    StretchComMode:=SM_FirstPoint;
+    drawings.GetCurrentDWG.wa.SetMouseMode(MGet3DPoint or MMoveCamera or MRotateCamera);
+    zcUI.Do_GUIaction(nil,zcMsgUIActionRedrawContent);
+  end else begin
+    // Нет предварительно выбранных объектов, запускаем обычный процесс выбора
+    // No pre-selected objects, start with normal selection
+    StretchComMode:=SM_GetEnts;
+    FrameEdit_com_CommandStart(Context,Operands);
+  end;
 end;
 
 function Stretch_com_BeforeClick(const Context:TZCADCommandContext;wc:GDBvertex;
@@ -76,14 +185,22 @@ begin
   end;
 end;
 
+// Выбор контрольных точек в рамке после завершения выделения секущей рамкой
+// Selection of control points in the frame after crossing window selection is complete
 procedure selectpoints;
 var
   DC:TDrawContext;
 begin
+  // Создаем контекст отрисовки
+  // Create drawing context
   dc:=drawings.GetCurrentDWG.wa.CreateRC;
+  // Перемещаем контрольные точки для правильного отображения на экране
+  // Remap control points for proper display on screen
   drawings.GetCurrentDWG.GetSelObjArray.remappoints(
     drawings.GetCurrentDWG.GetPcamera.POSCOUNT,drawings.GetCurrentDWG.wa.param.scrollmode,
     drawings.GetCurrentDWG.GetPcamera^,drawings.GetCurrentDWG^.myGluProject2,dc);
+  // Выбираем контрольные точки, попавшие в секущую рамку
+  // Select control points that fall within the crossing frame
   drawings.GetCurrentDWG.GetSelObjArray.selectcontrolpointinframe(
     drawings.GetCurrentDWG.wa.param.seldesc.Frame1,
     drawings.GetCurrentDWG.wa.param.seldesc.Frame2);
@@ -137,3 +254,4 @@ finalization
   ProgramLog.LogOutFormatStr('Unit "%s" finalization',[{$INCLUDE %FILE%}],
     LM_Info,UnitsFinalizeLMId);
 end.
+

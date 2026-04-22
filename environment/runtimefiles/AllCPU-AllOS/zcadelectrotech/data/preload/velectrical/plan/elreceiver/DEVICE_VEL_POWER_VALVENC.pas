442c31014645a556614b3d7903e32508bd46c435
veb86 <veb86@mail.ru>
2026-04-22 14:21:00 +0300

    добавил новые УГО

---
+++ environment/runtimefiles/AllCPU-AllOS/zcadelectrotech/data/preload/velectrical/plan/elreceiver/DEVICE_VEL_POWER_VALVENC.pas
@@ -0,0 +1,44 @@
+unit DEVICE_VEL_POWER_VALVENC;
+
+interface
+
+uses system,devices;
+usescopy blocktype;
+usescopy objname_eo;
+usescopy objgroup;
+usescopy addtocable;
+usescopy elreceivers;
+usescopy vlocation;
+usescopy vtextpointer;
+usescopy vinfopersonaluse;
+
+implementation
+
+begin
+
+BTY_TreeCoord:='PLAN_VEL_Электроприемники_Клапан нормально закрытый';
+Device_Type:=TDT_SilaPotr;
+Device_Class:=TDC_Shell;
+
+NMO_BaseName:='КДУ.';
+NMO_Suffix:='(??)';
+NMO_Template:='@@[NMO_BaseName]@@[NMO_Suffix]';
+
+GC_NameGroupTemplate:='@@[GC_HeadDevice].@@[GC_HDGroup]';
+
+realnamedev:='Клапан НЗ';
+Power:=0.01;
+CosPHI:=0.8;
+Voltage:=_AC_220V_50Hz;
+Phase:=_A;
+
+INFOPERSONALUSE_TextTemplate:='';
+
+INFOTEXTPOINTER_Tp1Up.format:='@@[NMO_BaseName]@@[NMO_Suffix]';
+INFOTEXTPOINTER_Tp1Bottom.format:='@@[Power]';
+INFOTEXTPOINTER_Tp2Up.format:='@@[GC_HeadDevice].@@[GC_HDGroup]';
+INFOTEXTPOINTER_Tp2Bottom.format:=' ';
+
+SerialConnection:=1;
+
+end.

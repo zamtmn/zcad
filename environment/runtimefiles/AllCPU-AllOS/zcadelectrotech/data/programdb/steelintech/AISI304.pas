e03c7336711f382eeb5d8ffb28e6e3ab16d1d314
zamtmn <zamtmn@yandex.ru>
2026-08-11 00:15:51 +0500

    + AISI304(08Х18Н10) 15х1,5х6м

---
+++ environment/runtimefiles/AllCPU-AllOS/zcadelectrotech/data/programdb/steelintech/AISI304.pas
@@ -0,0 +1,24 @@
+subunit devicebase;
+interface
+uses system;
+var
+  _EQAISI304_15_1_5:DbBaseObject;
+implementation
+begin
+     _EQAISI304_15_1_5.initnul;
+
+     _EQAISI304_15_1_5.Group:=_materials;
+     _EQAISI304_15_1_5..Category:=_materials;
+     _EQAISI304_15_1_5.EdIzm:=_sht;
+     _EQAISI304_15_1_5.ID:='_EQAISI304_15_1_5';
+     _EQAISI304_15_1_5.Standard:='AISI304(08Х18Н10)';
+     _EQAISI304_15_1_5.OKP:='';
+     _EQAISI304_15_1_5.Manufacturer:='ООО"СтальИнтех" г.Москва';
+
+     _EQAISI304_15_1_5.NameShort:='Труба нержавеющая 15х1,5х6м';
+     _EQAISI304_15_1_5.Name:='Труба нержавеющая 15х1,5х6м. AISI304(08Х18Н10)';
+     _EQAISI304_15_1_5.NameFull:='Труба нержавеющая 15х1,5х6м. AISI304(08Х18Н10) электросварная, круглая, матовая';
+
+     _EQAISI304_15_1_5.TreeCoord:='BP_СтальИнтех_Трубы_AISI304(08Х18Н10)15х1,5|BC_Материалы_Трубы_AISI304(08Х18Н10)15х1,5(СтальИнтех)';
+
+end.
